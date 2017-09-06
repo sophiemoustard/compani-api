0279c27fdab8234b0dd6e40951e589c73931161f
Jean-Christophe TREBALAG <jean-christophetrebalag@MacBook-Pro-de-Jean-Christophe.local>
2017-09-06 15:46:57 +0200

    Add CORS middleware

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 const app = express();
 
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  // intercept OPTIONS method
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
 // Db connection
 require('./config/mongoose');
 
@@ -33,7 +45,7 @@ if (process.env.NODE_ENV == 'development') {
   app.use(morgan('dev'));
   // app.use(cors());
 }
-
+app.use(allowCrossDomain);
 // Maybe a future use...
 app.use(cookieParser());
 
