fb51814368befcaf8ba90407572f36eb17fea475
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-10-27 12:17:21 +0200

    Redid botController functions with async / await

--- controllers/botController.js
+++ controllers/botController.js
@@ -8,7 +8,7 @@ const User = require('../models/User');
 const language = translate.language;
 
 module.exports = {
-  authorize: (req, res) => {
+  authorize: async (req, res) => {
     if (!req.query.email || !req.query.password) {
       return res.status(400).send({ success: false, message: `Erreur: ${translate[language].missingParameters}` });
       // return response.error(res, 400, translate[language].missingParameters);
@@ -18,61 +18,61 @@ module.exports = {
       // return response.error(res, 400, translate[language].missingParameters);
     }
     // Get by local email
-    User.findOne({ 'local.email': req.query.email }, (err, user) => {
-      if (err) {
-        return res.status(500).send({ success: false, message: `Erreur: ${translate[language].unexpectedBehavior}` });
-        // return response.error(res, 500, translate[language].unexpectedBehavior);
-      }
+    let user = {};
+    try {
+      user = await User.findOne({ 'local.email': req.query.email });
       if (!user) {
         return res.status(404).send({ success: false, message: `Erreur: ${translate[language].userAuthNotFound}` });
         // return response.error(res, 404, translate[language].userAuthNotFound);
       }
       // check if password matches
-      bcrypt.compare(req.query.password, user.local.password, (error, isMatch) => {
-        if (error || !isMatch) {
-          return res.status(401).send({ success: false, message: `Erreur: ${translate[language].userAuthFailed}` });
-          // return response.error(res, 401, translate[language].userAuthFailed);
-        }
-        const payload = {
-          firstname: user.firstname,
-          lastname: user.lastname,
-          _id: user.id,
-          'local.email': user.local.email,
-          role: user.role,
-          customer_id: user.customer_id,
-          employee_id: user.employee_id,
-          sector: user.sector
-        };
-        const newPayload = _.pickBy(payload);
-        const token = tokenProcess.encode(newPayload);
-        console.log(`${req.query.email} connected`);
-        // return the information including token as JSON
-        console.log('REDIRECT_URI =');
-        console.log(req.query.redirect_uri);
-        const redirectUri = `${req.query.redirect_uri}&authorization_code=${token}`;
-        return res.redirect(302, redirectUri);
-        // return response.success(res, translate[language].userAuthentified, { user: user, token: token } );
-      });
-    });
-  },
-  getUserByParamId: (req, res) => {
-    User.findOne({ _id: req.params._id }, (err, user) => {
-      if (err || !user) {
-        res.status(404).send({ success: false, message: translate[language].userNotFound });
-      } else {
-        const payload = {
-          firstname: user.firstname,
-          lastname: user.lastname,
-          _id: user.id,
-          'local.email': user.local.email,
-          role: user.role,
-          customer_id: user.customer_id,
-          employee_id: user.employee_id,
-          sector: user.sector
-        };
-        const newPayload = _.pickBy(payload);
-        res.status(200).send({ success: true, message: translate[language].userFound, data: { user: newPayload } });
+      if (!await bcrypt.compare(req.query.password, user.local.password)) {
+        return res.status(401).json({ success: false, message: `Erreur: ${translate[language].userAuthFailed}` });
       }
-    });
+      const payload = {
+        firstname: user.firstname,
+        lastname: user.lastname,
+        _id: user.id,
+        'local.email': user.local.email,
+        role: user.role,
+        customer_id: user.customer_id,
+        employee_id: user.employee_id,
+        sector: user.sector
+      };
+      const newPayload = _.pickBy(payload);
+      const token = tokenProcess.encode(newPayload);
+      console.log(`${req.query.email} connected`);
+      // return the information including token as JSON
+      console.log('REDIRECT_URI =');
+      console.log(req.query.redirect_uri);
+      const redirectUri = `${req.query.redirect_uri}&authorization_code=${token}`;
+      return res.redirect(302, redirectUri);
+      // return response.success(res, translate[language].userAuthentified, { user: user, token: token } );
+    } catch (e) {
+      return res.status(500).send({ success: false, message: `Erreur: ${translate[language].unexpectedBehavior}` });
+      // return response.error(res, 500, translate[language].unexpectedBehavior);
+    }
+  },
+  getUserByParamId: async (req, res) => {
+    try {
+      const user = await User.findOne({ _id: req.params._id });
+      if (!user) {
+        return res.status(404).send({ success: false, message: translate[language].userNotFound });
+      }
+      const payload = {
+        firstname: user.firstname,
+        lastname: user.lastname,
+        _id: user.id,
+        'local.email': user.local.email,
+        role: user.role,
+        customer_id: user.customer_id,
+        employee_id: user.employee_id,
+        sector: user.sector
+      };
+      const newPayload = _.pickBy(payload);
+      res.status(200).send({ success: true, message: translate[language].userFound, data: { user: newPayload } });
+    } catch (e) {
+      return res.status(404).send({ success: false, message: translate[language].userNotFound });
+    }
   }
 };
