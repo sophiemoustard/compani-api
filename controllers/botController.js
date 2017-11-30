7d977257be7302ee182145644e2c68d6df90e44e
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-11-30 18:02:50 +0100

    Change authorize for bot controller and route is now post

--- controllers/botController.js
+++ controllers/botController.js
@@ -9,24 +9,20 @@ const language = translate.language;
 
 module.exports = {
   authorize: async (req, res) => {
-    if (!req.query.email || !req.query.password) {
-      return res.status(400).send({ success: false, message: `Erreur: ${translate[language].missingParameters}` });
-      // return response.error(res, 400, translate[language].missingParameters);
-    }
-    if (!req.query && !req.query.redirect_uri) {
+    if (!req.body.email || !req.body.password) {
       return res.status(400).send({ success: false, message: `Erreur: ${translate[language].missingParameters}` });
       // return response.error(res, 400, translate[language].missingParameters);
     }
     // Get by local email
     let user = {};
     try {
-      user = await User.findOne({ 'local.email': req.query.email });
+      user = await User.findOne({ 'local.email': req.body.email });
       if (!user) {
         return res.status(404).send({ success: false, message: `Erreur: ${translate[language].userAuthNotFound}` });
         // return response.error(res, 404, translate[language].userAuthNotFound);
       }
       // check if password matches
-      if (!await bcrypt.compare(req.query.password, user.local.password)) {
+      if (!await bcrypt.compare(req.body.password, user.local.password)) {
         return res.status(401).json({ success: false, message: `Erreur: ${translate[language].userAuthFailed}` });
       }
       const payload = {
@@ -41,18 +37,60 @@ module.exports = {
       };
       const newPayload = _.pickBy(payload);
       const token = tokenProcess.encode(newPayload);
-      console.log(`${req.query.email} connected`);
+      console.log(`${req.body.email} connected`);
       // return the information including token as JSON
-      console.log('REDIRECT_URI =');
-      console.log(req.query.redirect_uri);
-      const redirectUri = `${req.query.redirect_uri}&authorization_code=${token}`;
-      return res.redirect(302, redirectUri);
+      return res.status(200).json({ success: true, message: translate[language].userAuthentified, data: { token, user } });
       // return response.success(res, translate[language].userAuthentified, { user: user, token: token } );
     } catch (e) {
       return res.status(500).send({ success: false, message: `Erreur: ${translate[language].unexpectedBehavior}` });
       // return response.error(res, 500, translate[language].unexpectedBehavior);
     }
   },
+  // authorize: async (req, res) => {
+  //   if (!req.query.email || !req.query.password) {
+  //     return res.status(400).send({ success: false, message: `Erreur: ${translate[language].missingParameters}` });
+  //     // return response.error(res, 400, translate[language].missingParameters);
+  //   }
+  //   if (!req.query && !req.query.redirect_uri) {
+  //     return res.status(400).send({ success: false, message: `Erreur: ${translate[language].missingParameters}` });
+  //     // return response.error(res, 400, translate[language].missingParameters);
+  //   }
+  //   // Get by local email
+  //   let user = {};
+  //   try {
+  //     user = await User.findOne({ 'local.email': req.query.email });
+  //     if (!user) {
+  //       return res.status(404).send({ success: false, message: `Erreur: ${translate[language].userAuthNotFound}` });
+  //       // return response.error(res, 404, translate[language].userAuthNotFound);
+  //     }
+  //     // check if password matches
+  //     if (!await bcrypt.compare(req.query.password, user.local.password)) {
+  //       return res.status(401).json({ success: false, message: `Erreur: ${translate[language].userAuthFailed}` });
+  //     }
+  //     const payload = {
+  //       firstname: user.firstname,
+  //       lastname: user.lastname,
+  //       _id: user.id,
+  //       'local.email': user.local.email,
+  //       role: user.role,
+  //       customer_id: user.customer_id,
+  //       employee_id: user.employee_id,
+  //       sector: user.sector
+  //     };
+  //     const newPayload = _.pickBy(payload);
+  //     const token = tokenProcess.encode(newPayload);
+  //     console.log(`${req.query.email} connected`);
+  //     // return the information including token as JSON
+  //     console.log('REDIRECT_URI =');
+  //     console.log(req.query.redirect_uri);
+  //     const redirectUri = `${req.query.redirect_uri}&authorization_code=${token}`;
+  //     return res.redirect(302, redirectUri);
+  //     // return response.success(res, translate[language].userAuthentified, { user: user, token: token } );
+  //   } catch (e) {
+  //     return res.status(500).send({ success: false, message: `Erreur: ${translate[language].unexpectedBehavior}` });
+  //     // return response.error(res, 500, translate[language].unexpectedBehavior);
+  //   }
+  // },
   getUserByParamId: async (req, res) => {
     try {
       const user = await User.findOne({ _id: req.params._id });
--- routes/api/bot.js
+++ routes/api/bot.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const botController = require('../../controllers/botController');
 
-router.get('/authorize', botController.authorize);
+router.post('/authorize', botController.authorize);
 router.get('/user/:_id', botController.getUserByParamId); // not protected because we don't want user to re-enter password everytime from the bot
 
 module.exports = router;
