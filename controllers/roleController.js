4010fce6d87b0c8c094af1eb00980bd33c5d622f
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-12-08 16:48:54 +0100

    Add remove + create with default features + format show and showAll in role controller

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -1,22 +1,50 @@
+const _ = require('lodash');
+
 const translate = require('../helpers/translate');
 const dot = require('dot-object');
 
 const language = translate.language;
 
 const Role = require('../models/Role');
+const Feature = require('../models/Feature');
 
 const create = async (req, res) => {
   try {
-    if (!req.body.role && !req.body.features) {
+    if (!req.body.name && !req.body.features) {
       return res.status(400).json({ success: false, message: translate[language].missingParameters });
     }
-    const role = new Role(req.body);
+    const createPayload = { name: req.body.name, features: [] };
+    const features = await Feature.find();
+    if (features.length === 0) {
+      return res.status(404).json({ success: false, message: translate[language].featuresDoNotExist });
+    }
+    for (let i = 0, l = features.length; i < l; i++) {
+      if (_.has(req.body.features, features[i].name)) {
+        createPayload.features.push({
+          feature_id: features[i]._id,
+          permission_level: req.body.features[features[i].name]
+        });
+      } else {
+        createPayload.features.push({
+          feature_id: features[i]._id,
+          permission_level: 0
+        });
+      }
+    }
+    const role = new Role(createPayload);
     await role.save();
+    const populatedRole = await Role.findOne({ _id: role._id }).populate('features.feature_id');
     const payload = {
-      _id: role._id,
-      name: role.name,
-      features: role.features
+      _id: populatedRole._id,
+      name: populatedRole.name,
+      features: []
     };
+    for (let i = 0, l = populatedRole.features.length; i < l; i++) {
+      payload.features.push({
+        _id: populatedRole.features[i].feature_id._id,
+        [populatedRole.features[i].feature_id.name]: populatedRole.features[i].permission_level
+      });
+    }
     return res.status(200).json({ success: true, message: translate[language].roleCreated, data: { role: payload } });
   } catch (e) {
     console.error(e);
@@ -51,10 +79,26 @@ const update = async (req, res) => {
 const showAll = async (req, res) => {
   // No security here to restrict access
   try {
-    const roles = await Role.find(req.query);
-    if (roles.length === 0) {
+    const rolesRaw = await Role.find(req.query, { 'features._id': 0, updatedAt: 0, createdAt: 0, __v: 0 }).populate({ path: 'features.feature_id', select: 'name _id' });
+    if (rolesRaw.length === 0) {
       return res.status(404).json({ success: false, message: translate[language].rolesShowAllNotFound });
     }
+    const roles = [];
+    rolesRaw.forEach((role) => {
+      const features = [];
+      role.features.forEach((feature) => {
+        features.push({
+          _id: feature.feature_id._id,
+          name: feature.feature_id.name,
+          permission_level: feature.permission_level
+        });
+      });
+      roles.push({
+        _id: role._id,
+        name: role.name,
+        features
+      });
+    });
     return res.status(200).json({ success: true, message: translate[language].rolesShowAllFound, data: { roles } });
   } catch (e) {
     console.error(e);
@@ -65,10 +109,22 @@ const showAll = async (req, res) => {
 // Find an role by Id
 const show = async (req, res) => {
   try {
-    const role = await Role.findOne({ _id: req.params._id });
-    if (!role) {
+    const roleRaw = await Role.findOne({ _id: req.params._id }, { 'features._id': 0, updatedAt: 0, createdAt: 0, __v: 0 }).populate({ path: 'features.feature_id', select: 'name _id' });
+    if (!roleRaw) {
       return res.status(404).json({ success: false, message: translate[language].roleNotFound });
     }
+    const role = {
+      _id: roleRaw._id,
+      name: roleRaw.name,
+      features: []
+    };
+    roleRaw.features.forEach((feature) => {
+      role.features.push({
+        _id: feature.feature_id._id,
+        name: feature.feature_id.name,
+        permission_level: feature.permission_level
+      });
+    });
     return res.status(200).json({ success: true, message: translate[language].roleFound, data: { role } });
   } catch (e) {
     console.error(e);
@@ -76,9 +132,23 @@ const show = async (req, res) => {
   }
 };
 
+const remove = async (req, res) => {
+  try {
+    const roleDeleted = await Role.findByIdAndRemove({ _id: req.params._id });
+    if (!roleDeleted) {
+      return res.status(404).json({ success: false, message: translate[language].roleNotFound });
+    }
+    return res.status(200).json({ success: true, message: translate[language].roleRemoved, data: { roleDeleted } });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ success: false, message: translate[language].unexpectedBehavior });
+  }
+};
+
 module.exports = {
   create,
   update,
   showAll,
-  show
+  show,
+  remove
 };
