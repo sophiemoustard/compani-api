cd5e62ce0c39dbbedb8d246fee80596064e06d81
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-12-19 17:24:32 +0100

    Remove comment and add condition to prevent errors

--- helpers/populateRole.js
+++ helpers/populateRole.js
@@ -1,16 +1,13 @@
 exports.populateRole = (features) => {
-  // const roleToAdd = {
-  //   _id: populatedRole._id,
-  //   name: populatedRole.name,
-  //   features: []
-  // };
   const formattedFeatures = [];
   features.forEach((feature) => {
-    formattedFeatures.push({
-      _id: feature.feature_id._id,
-      name: feature.feature_id.name,
-      permission_level: feature.permission_level
-    });
+    if (feature.feature_id && feature.feature_id._id && feature.feature_id.name) {
+      formattedFeatures.push({
+        _id: feature.feature_id._id,
+        name: feature.feature_id.name,
+        permission_level: feature.permission_level
+      });
+    }
   });
   return formattedFeatures;
 };
