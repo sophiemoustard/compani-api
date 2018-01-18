d18a3c0a814305116e066ecd54177b542a374b21
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-01-18 16:23:28 +0100

    Handle case where year is not specified

--- controllers/Ogust/customerController.js
+++ controllers/Ogust/customerController.js
@@ -186,7 +186,7 @@ const getCustomerInvoices = async (req, res) => {
     const params = {
       token: req.headers['x-ogust-token'],
       id_customer: req.params.id,
-      end_of_period: `@between|${req.query.year}0101|${req.query.year}1231`,
+      end_of_period: req.query.year ? `@between|${req.query.year}0101|${req.query.year}1231` : '',
       nbperpage: req.query.nbPerPage || '50',
       pagenum: req.query.pageNum || '1'
     };
