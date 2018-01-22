20c86560118514c00350856f2d0a3d4bf5255383
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-01-22 16:32:13 +0100

    No need of requesting month for getCustomerFiscalAttests

--- controllers/Ogust/customerController.js
+++ controllers/Ogust/customerController.js
@@ -160,7 +160,7 @@ const getCustomerFiscalAttests = async (req, res) => {
       token: req.headers['x-ogust-token'],
       id_customer: req.params.id,
       status: 'E',
-      period_end: req.query.year && req.query.month ? `@between|${req.query.year}${req.query.month}01|${req.query.year}${req.query.month}31` : '',
+      period_end: req.query.year ? `@between|${req.query.year}0101|${req.query.year}1231` : '',
       nbperpage: req.query.nbPerPage || '24',
       pagenum: req.query.pageNum || '1'
     };
