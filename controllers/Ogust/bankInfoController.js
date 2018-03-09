927b1376ed7c72bff3e9c00d4826f57cce6b977b
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-03-09 17:31:12 +0100

    Fix some names

--- controllers/Ogust/bankInfoController.js
+++ controllers/Ogust/bankInfoController.js
@@ -13,15 +13,15 @@ const updateByEmployeeId = async (req, res) => {
     const params = {
       token: req.headers['x-ogust-token'],
       id_tiers: req.body.id_tiers,
-      iban_number: req.body.iban || '',
-      bic_number: req.body.bic || ''
+      iban_number: req.body.iban_number || '',
+      bic_number: req.body.bic_number || ''
     };
     const newParams = _.pickBy(params);
     const updatedBankInfo = await bankInfo.setBankInfoByEmployeeId(newParams);
     if (updatedBankInfo.body.status == 'KO') {
       return res.status(400).json({ success: false, message: updatedBankInfo.body.message });
     }
-    return res.status(200).json({ success: true, message: translate[language].serviceUpdated, data: { updatedService: updatedBankInfo.body } });
+    return res.status(200).json({ success: true, message: translate[language].bankInfoUpdated, data: { updatedBankInfo: updatedBankInfo.body } });
   } catch (e) {
     console.error(e);
     res.status(500).json({ success: false, message: translate[language].unexpectedBehavior });
