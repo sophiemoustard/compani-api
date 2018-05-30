040dd733ec0beaabebb509910d6b4708f9be2366
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-05-30 10:45:52 +0200

    Fix mail typos

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -18,11 +18,10 @@ const sendWelcome = async (req, res) => {
              <p>Vous pouvez désormais accéder à votre espace Alenvi dans lequel vous trouverez les éléments suivants :<p>
              <ul>
               <li>Vos factures et attestations fiscales,</li>
-              <li>Le planning d’interventions,</li>
+              <li>Le planning des interventions,</li>
               <li>Le blog Alenvi avec des informations utiles pour les aidants familiaux.</li>
              </ul>
-             <p>Le lien :</p>
-             <a href="${process.env.WEBSITE_HOSTNAME}/login">${process.env.WEBSITE_HOSTNAME}/login</a>
+             <p>Le lien :<a href="${process.env.WEBSITE_HOSTNAME}/login">${process.env.WEBSITE_HOSTNAME}/login</a></p>
              <p>Vos identifiants pour y accéder:</p>
              <ul>
               <li>login : ${req.body.receiver.email}</li>
