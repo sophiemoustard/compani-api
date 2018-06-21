0c9708f81ccf1c3b73ee7915c95e9cee04831528
Pierre HOFMAN <pierre.hofman@epitech.eu>
2018-06-21 11:31:05 +0200

    Fix comma

--- plugins/index.js
+++ plugins/index.js
@@ -2,26 +2,26 @@ let reporters = {};
 if (process.env.NODE_ENV !== 'test') {
   reporters = {
     console: [{
-        module: 'good-squeeze',
-        name: 'Squeeze',
-        args: [{
-          log: '*',
-          response: '*'
-        }]
-      },
-      {
-        module: 'good-console'
-      },
-      'stdout'
+      module: 'good-squeeze',
+      name: 'Squeeze',
+      args: [{
+        log: '*',
+        response: '*'
+      }]
+    },
+    {
+      module: 'good-console'
+    },
+    'stdout'
     ]
-  }
+  };
 }
 exports.plugins = [
   {
     plugin: require('good'),
     options: {
       reporters
-      }
-  }
+    }
+  },
   { plugin: require('hapi-auth-jwt2') },
 ];
