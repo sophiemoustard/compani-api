c0196636a1e3b5d85f42faed4845fc2ebda5a8f3
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-06-21 11:29:53 +0200

    Remove good reporters on test env

--- plugins/index.js
+++ plugins/index.js
@@ -1,23 +1,27 @@
+let reporters = {};
+if (process.env.NODE_ENV !== 'test') {
+  reporters = {
+    console: [{
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{
+          log: '*',
+          response: '*'
+        }]
+      },
+      {
+        module: 'good-console'
+      },
+      'stdout'
+    ]
+  }
+}
 exports.plugins = [
   {
     plugin: require('good'),
     options: {
-      reporters: {
-        console: [{
-          module: 'good-squeeze',
-          name: 'Squeeze',
-          args: [{
-            log: '*',
-            response: '*'
-          }]
-        },
-        {
-          module: 'good-console'
-        },
-        'stdout'
-        ]
+      reporters
       }
-    }
-  },
+  }
   { plugin: require('hapi-auth-jwt2') },
 ];
