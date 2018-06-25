824000675b787b0147e1db49f7d858d41c042703
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-06-25 14:29:10 +0200

    Using beforeEach for testing + test PUT /role/{_id} route

--- tests/roles.test.js
+++ tests/roles.test.js
@@ -2,14 +2,14 @@ const expect = require('expect');
 
 const app = require('../server');
 const { getToken, populateUsers } = require('./seed/usersSeed');
-const { populateRoles, rolePayload } = require('./seed/rolesSeed');
+const { populateRoles, rolePayload, wrongRolePayload, rolesList } = require('./seed/rolesSeed');
 const Role = require('../models/Role');
 
 describe('ROLES ROUTES', () => {
   let token = null;
-  before(populateRoles);
-  before(populateUsers);
-  before(async () => {
+  beforeEach(populateRoles);
+  beforeEach(populateUsers);
+  beforeEach(async () => {
     token = await getToken();
   });
   describe('POST /roles', () => {
@@ -55,6 +55,71 @@ describe('ROLES ROUTES', () => {
           permission_level: rolePayload.features[2].permission_level
         })
       ]));
+    });
+
+    it('should return a 400 error if missing payload', async () => {
+      const res = await app.inject({
+        method: 'POST',
+        url: '/roles',
+        headers: { 'x-access-token': token }
+      });
+      expect(res.statusCode).toBe(400);
+    });
+
+    it('should return a 400 error if features permission level are wrong', async () => {
+      const res = await app.inject({
+        method: 'POST',
+        url: '/roles',
+        payload: wrongRolePayload,
+        headers: { 'x-access-token': token }
+      });
+      expect(res.statusCode).toBe(400);
+    });
+
+    it('should return a 409 error if role already exists', async () => {
+      const payload = {
+        name: rolesList[0].name,
+        features: rolesList[0].features
+      };
+      const res = await app.inject({
+        method: 'POST',
+        url: '/roles',
+        payload,
+        headers: { 'x-access-token': token }
+      });
+      expect(res.statusCode).toBe(409);
+    });
+  });
+
+  describe('PUT /roles/{_id}', () => {
+    it('should update role', async () => {
+      const payload = {
+        name: 'Kokonut',
+        features: [
+          {
+            _id: rolesList[0].features[0].feature_id,
+            permission_level: 0
+          },
+
+        ]
+      };
+      const res = await app.inject({
+        method: 'PUT',
+        url: `/roles/${rolesList[0]._id}`,
+        payload,
+        headers: { 'x-access-token': token }
+      });
+      expect(res.statusCode).toBe(200);
+      expect(res.result.data.role).toEqual(expect.objectContaining({
+        _id: rolesList[0]._id,
+        name: payload.name,
+        features: expect.arrayContaining([
+          expect.objectContaining({
+            _id: payload.features[0]._id,
+            permission_level: payload.features[0].permission_level
+          })
+        ])
+      }));
     });
   });
 });
