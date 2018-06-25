45b884b468b0016bfe326ae4255a620461d259f8
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-06-25 18:17:44 +0200

    Add logging for all routes by default

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const { plugins } = require('./plugins/index');
 
 const server = Hapi.server({
   port: process.env.NODE_ENV === 'test' ? 3002 : 3003,
-  host: 'localhost'
+  host: 'localhost',
+  routes: {
+    log: {
+      collect: true
+    }
+  }
 });
 
 
