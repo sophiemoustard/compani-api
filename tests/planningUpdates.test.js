d0effd3d242c4aeb480a11657f72b60e59ca561a
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-07-02 11:39:19 +0200

    Add seed + testing errors (GET /planningUpdates)

--- tests/planningUpdates.test.js
+++ tests/planningUpdates.test.js
@@ -2,7 +2,7 @@ const expect = require('expect');
 const { ObjectID } = require('mongodb');
 
 const app = require('../server');
-const { getToken, populateUsers } = require('./seed/usersSeed');
+const { getToken, populateUsers, userList } = require('./seed/usersSeed');
 const { populateRoles } = require('./seed/rolesSeed');
 const { populatePlanningUpdates } = require('./seed/planningUpdatesSeed');
 
@@ -23,7 +23,26 @@ describe('PLANNING UPDATES ROUTES', () => {
         headers: { 'x-access-token': token }
       });
       expect(res.statusCode).toBe(200);
+      expect(res.result.data.modifPlanning.length).toBe(2);
+    });
+
+    it('should return list with userId query param', async () => {
+      const res = await app.inject({
+        method: 'GET',
+        url: `/planningUpdates?userId=${userList[3]._id}`,
+        headers: { 'x-access-token': token }
+      });
+      expect(res.statusCode).toBe(200);
       expect(res.result.data.modifPlanning.length).toBe(1);
+    });
+
+    it('should return a 400 error if query param is invalid', async () => {
+      const res = await app.inject({
+        method: 'GET',
+        url: '/planningUpdates?toto=test',
+        headers: { 'x-access-token': token }
+      });
+      expect(res.statusCode).toBe(400);
     });
   });
 });
--- tests/seed/planningUpdatesSeed.js
+++ tests/seed/planningUpdatesSeed.js
@@ -3,7 +3,7 @@ const { ObjectID } = require('mongodb');
 const { userList } = require('./usersSeed');
 const User = require('../../models/User');
 
-const planningUpdate = {
+const planningUpdatesList = [{
   _id: new ObjectID(),
   content: 'Test',
   involved: 'M. Toto',
@@ -14,14 +14,26 @@ const planningUpdate = {
     checkBy: null,
     checkedAt: null
   }
-};
+}, {
+  _id: new ObjectID(),
+  content: 'Test2',
+  involved: 'M. Tata',
+  createdAt: Date.now(),
+  modificationType: 'Modification Planning',
+  check: {
+    isChecked: false,
+    checkBy: null,
+    checkedAt: null
+  }
+}];
 
 const populatePlanningUpdates = async () => {
   await User.update({}, { $unset: { planningModification: '' } }, { multi: true });
-  await User.findByIdAndUpdate(userList[3]._id, { $push: { planningModification: planningUpdate } });
+  await User.findByIdAndUpdate(userList[3]._id, { $push: { planningModification: planningUpdatesList[0] } });
+  await User.findByIdAndUpdate(userList[2]._id, { $push: { planningModification: planningUpdatesList[1] } });
 };
 
 module.exports = {
-  planningUpdate,
+  planningUpdatesList,
   populatePlanningUpdates
 };
