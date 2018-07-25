9b2e9ba2887ea776b36cc65874be9aeb160517c3
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-07-25 20:35:16 +0400

    Add empty string if name doesnt exist

--- helpers/populateRole.js
+++ helpers/populateRole.js
@@ -2,7 +2,7 @@ const processingRights = rights => rights.map((right) => {
   if (right.right_id && right.right_id._id && (right.right_id.name || right.right_id.permission)) {
     return {
       right_id: right.right_id._id,
-      name: right.right_id.name,
+      name: right.right_id.name || '',
       permission: right.right_id.permission,
       description: right.right_id.description,
       rolesConcerned: right.rolesConcerned,
