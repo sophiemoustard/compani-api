42b7d8246c9c1c7b699b2941c46381c97ef386c1
sophiemoustard <sophie.moustard@gmail.com>
2018-12-11 16:43:07 +0100

    [73] Add tests on CRUD company

--- tests/companies.test.js
+++ tests/companies.test.js
@@ -17,6 +17,103 @@ describe('COMPANIES ROUTES', () => {
     authToken = await getToken();
   });
 
+  describe('GET /companies/:id', () => {
+    it('should return company', async () => {
+      const company = companiesList[0];
+      const response = await app.inject({
+        method: 'GET',
+        url: `/companies/${company._id.toHexString()}`,
+        headers: { 'x-access-token': authToken },
+      });
+
+      expect(response.statusCode).toBe(200);
+      expect(response.result.data.company).toBeDefined();
+    });
+
+    it('should return 404 if no company found', async () => {
+      const invalidId = new ObjectID().toHexString();
+      const response = await app.inject({
+        method: 'GET',
+        url: `/companies/${invalidId}`,
+        headers: { 'x-access-token': authToken },
+      });
+
+      expect(response.statusCode).toBe(404);
+    });
+  });
+
+  describe('POST /companies', () => {
+    it('should create a new service', async () => {
+      const payload = {
+        name: 'Alenvi',
+      };
+      const response = await app.inject({
+        method: 'POST',
+        url: `/companies`,
+        headers: { 'x-access-token': authToken },
+        payload,
+      });
+
+      expect(response.statusCode).toBe(200);
+      expect(response.result.data.company.name).toBe('Alenvi');
+    });
+  });
+
+  describe('PUT /companies/:id', () => {
+    it('should update company service', async () => {
+      const company = companiesList[0];
+      const payload = {
+        name: 'Alenvi Alenvi',
+      };
+      const response = await app.inject({
+        method: 'PUT',
+        url: `/companies/${company._id.toHexString()}`,
+        headers: { 'x-access-token': authToken },
+        payload,
+      });
+
+      expect(response.statusCode).toBe(200);
+      expect(response.result.data.company.name).toEqual(payload.name);
+    });
+
+    it('should return 404 if no company found', async () => {
+      const invalidId = new ObjectID().toHexString();
+      const payload = {
+        name: 'Alenvi Alenvi',
+      };
+      const response = await app.inject({
+        method: 'PUT',
+        url: `/companies/${invalidId}`,
+        headers: { 'x-access-token': authToken },
+        payload,
+      });
+
+      expect(response.statusCode).toBe(404);
+    });
+  });
+
+  describe('DELETE /companies/:id', () => {
+    it('should delete company service', async () => {
+      const company = companiesList[0];
+
+      const response = await app.inject({
+        method: 'DELETE',
+        url: `/companies/${company._id.toHexString()}`,
+        headers: { 'x-access-token': authToken },
+      });
+
+      expect(response.statusCode).toBe(200);
+    });
+  });
+});
+
+describe('COMPANIES ROUTES', () => {
+  let authToken = null;
+  before(populateCompanies);
+  beforeEach(async () => {
+    authToken = await getToken();
+  });
+
   describe('POST /companies/:id/services', () => {
     it('should create a new service', async () => {
       const company = companiesList[0];
@@ -123,16 +220,6 @@ describe('COMPANIES ROUTES', () => {
       });
 
       expect(response.statusCode).toBe(200);
-    });
-    
-    it('should return 404 if no company found', async () => {
-      const response = await app.inject({
-        method: 'DELETE',
-        url: `/companies/1234/services`,
-        headers: { 'x-access-token': authToken },
-      });
-  
-      expect(response.statusCode).toBe(404);
     });
   });
 });
