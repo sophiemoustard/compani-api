033a99206424b7e94b17f9c09c51b80528552f06
sophiemoustard <sophie.moustard@gmail.com>
2018-12-13 17:03:42 +0100

    [64] Retours JC

--- helpers/populateServices.js
+++ helpers/populateServices.js
@@ -1,20 +1,20 @@
 const Company = require('../models/Company');
 
 const populateServices = async (subscriptions) => {
-  if (subscriptions.length === 0) return [];
+  if (!subscriptions || subscriptions.length === 0) return [];
 
   const company = await Company.findOne({ 'customersConfig.services._id': subscriptions[0].service });
 
   return subscriptions.map((subscription) => {
     const serviceId = subscription.service;
-    const service = company.customersConfig.services.find(service => service._id.toHexString() == serviceId);
+    const subscribedService = company.customersConfig.services.find(service => service._id.toHexString() == serviceId);
 
     return {
       ...subscription,
-      service,
+      service: subscribedService,
     };
   });
-}
+};
 
 module.exports = {
   populateServices,
