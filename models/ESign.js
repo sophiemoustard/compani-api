d8a2674ae00a429f9b7142d5bfb972ac75936f16
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-12-14 17:46:12 +0100

    Add docx generation and base64 conversion to customer signature generation

--- models/ESign.js
+++ models/ESign.js
@@ -7,3 +7,10 @@ exports.createDocument = async data => axios.post('https://api.eversign.com/api/
   }
 });
 
+exports.uploadFile = async (file, headers) => axios.post('https://api.eversign.com/api/file', file, {
+  params: {
+    access_key: process.env.EVERSIGN_API_KEY,
+    business_id: process.env.EVERSIGN_BUSINESS_ID
+  },
+  headers
+});
