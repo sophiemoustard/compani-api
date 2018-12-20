15abd9c7c704431f21a57b5d1189542bbd8f46eb
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-12-20 18:42:38 +0100

    Remove unnecessary variable

--- helpers/createAndReadFile.js
+++ helpers/createAndReadFile.js
@@ -4,7 +4,6 @@ exports.createAndReadFile = async (stream, outputPath) => new Promise((resolve) 
   const tmpFile = fs.createWriteStream(outputPath);
   stream.pipe(tmpFile);
   tmpFile.on('finish', () => {
-    const file = fs.createReadStream(outputPath);
-    resolve(file);
+    resolve(fs.createReadStream(outputPath));
   });
 });
