3b5242ddb95118890111c3ccdaabb6053e7fd5a9
sophiemoustard <sophie.moustard@gmail.com>
2019-01-14 12:03:18 +0100

    Add unit test on populateEventsListSubscription

--- tests/unit/helpers/events.test.js
+++ tests/unit/helpers/events.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 const { ObjectID } = require('mongodb');
-const { populateEventSubscription } = require('../../../helpers/events');
+const { populateEventSubscription, populateEventsListSubscription } = require('../../../helpers/events');
 
 describe('populateEventSubscription', () => {
   it('should populate subscription as event is an intervention', () => {
@@ -79,5 +79,66 @@ describe('populateEventSubscription', () => {
     } catch (e) {
       expect(e.output.statusCode).toEqual(409);
     }
+  });
+});
+
+describe('populateEventsListSubscription', () => {
+  it('should populate subscription as event is an intervention', () => {
+    const events = [
+      {
+        type: 'intervention',
+        customer: {
+          subscriptions: [
+            {
+              createdAt: '2019-01-11T08:38:18.653Z',
+              _id: new ObjectID('5c3855fa12d1370abdda0b8f'),
+              service: '5c35cdc2bd5e3e7360b853fa',
+              unitTTCRate: 25,
+              estimatedWeeklyVolume: 12,
+              sundays: 2,
+            },
+            {
+              createdAt: '2019-01-21T09:38:18.653Z',
+              _id: new ObjectID('5c35b5eb1a6fb00997363eeb'),
+              service: '5c35cdc2bd5e3e7360b853fa',
+              unitTTCRate: 25,
+              estimatedWeeklyVolume: 12,
+              sundays: 2,
+            }
+          ],
+        },
+        subscription: new ObjectID('5c3855fa12d1370abdda0b8f'),
+      },
+      {
+        type: 'intervention',
+        customer: {
+          subscriptions: [
+            {
+              createdAt: '2019-01-12T08:38:18.653Z',
+              _id: new ObjectID('5a3bc0315e421400147d5ecd'),
+              service: '5ad8c41659769000142589f7',
+              unitTTCRate: 25,
+              estimatedWeeklyVolume: 12,
+              sundays: 2,
+            },
+            {
+              createdAt: '2019-01-22T09:38:18.653Z',
+              _id: new ObjectID('5a3bc0005e421400147d5ec4'),
+              service: '5a5735cb1f2a1f0014d48e14',
+              unitTTCRate: 25,
+              estimatedWeeklyVolume: 12,
+              sundays: 2,
+            }
+          ],
+        },
+        subscription: new ObjectID('5a3bc0315e421400147d5ecd'),
+      }
+    ];
+
+    const result = populateEventsListSubscription(events);
+    expect(result[0].subscription).toBeDefined();
+    expect(result[1].subscription).toBeDefined();
+    expect(result[0].subscription._id).toEqual(events[0].subscription);
+    expect(result[1].subscription._id).toEqual(events[1].subscription);
   });
 });
