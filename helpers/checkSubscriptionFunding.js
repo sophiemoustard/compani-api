748a07373eb389c8506a37c0432e1ee5358f0bba
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2019-01-22 14:45:40 +0100

    Add populate + check subscriptions to create customer fundings method

---
+++ helpers/checkSubscriptionFunding.js
@@ -0,0 +1,24 @@
+const Boom = require('boom');
+const moment = require('moment');
+
+const Customer = require('../models/Customer');
+const {
+  getLastVersion
+} = require('./getLastVersion');
+
+const checkSubscriptionFunding = async (customerId, payloadVersion) => {
+  const customer = await Customer.findById(customerId).lean();
+  if (!customer) return Boom.notFound('Error while checking subscription funding: customer not found.');
+
+  const lastVersions = customer.fundings.map(funding => getLastVersion(funding.versions, 'createdAt'));
+
+  return lastVersions
+    .filter(version => version.subscriptions.some(sub => payloadVersion.subscriptions.includes(sub.toHexString())))
+    .every((el) => {
+      return el.endDate ? moment(el.endDate).isBefore(payloadVersion.startDate, 'day') : false;
+    });
+};
+
+module.exports = {
+  checkSubscriptionFunding
+};
