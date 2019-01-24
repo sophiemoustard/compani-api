da11be3a3081bec81e3a2186bfc66dfc35020b74
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2019-01-24 14:14:36 +0100

    Retours Sophie

--- helpers/getLastVersion.js
+++
@@ -1,6 +0,0 @@
-exports.getLastVersion = (versions, dateKey) => {
-  if (!Array.isArray(versions)) throw new Error('versions must be an array !');
-  if (typeof dateKey !== 'string') throw new Error('sortKey must be a string !');
-  if (versions.length === 0) return null;
-  return versions.sort((a, b) => new Date(b[dateKey]) - new Date(a[dateKey]))[0];
-};
