3e2938aced2141393e31733a783ba76a350094f0
jctrebalag <jctrebalag@gmail.com>
2019-01-25 11:35:10 +0100

    Add check of third party payers usage in fundings (#70)

---
+++ helpers/thirdPartyPayers.js
@@ -0,0 +1,12 @@
+const Customer = require('../models/Customer');
+
+const isUsedInFundings = async (thirdPartyPayers) => {
+  if (!thirdPartyPayers || !Array.isArray(thirdPartyPayers)) return false;
+  for (const tpp of thirdPartyPayers) {
+    const customers = await Customer.find({ fundings: { $exists: true }, 'fundings.versions.thirdPartyPayer': tpp._id });
+    tpp.isUsedInFundings = customers.length > 0;
+  }
+  return thirdPartyPayers;
+};
+
+module.exports = { isUsedInFundings };
