66c29cb42c43a77c73b182f7f709e2771553fbab
Pierre HOFMAN <pierre.hofman@epitech.eu>
2019-01-30 17:53:47 +0100

    Delete old files, add DUE automatization with Ogust after validating a contract version (#76)

--- models/Ogust/Employment.js
+++
@@ -1,5 +0,0 @@
-const { Ogust } = require('../../config/config');
-const axios = require('axios');
-
-exports.getEmploymentContracts = async payload => axios.post(`${Ogust.API_LINK}searchEmployment`, payload);
-exports.createEmploymentContract = async payload => axios.post(`${Ogust.API_LINK}setEmployment`, payload);
