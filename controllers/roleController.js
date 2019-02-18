d45a1baa1d79c4b04823a202f179f6a6610e3047
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2019-02-18 18:09:12 +0100

    Remove joi alternative for name query

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -65,7 +65,6 @@ const update = async (req) => {
 
 const showAll = async (req) => {
   try {
-    if (req.query.name && req.query.name.match(/\[.+\]/)) req.query.name = JSON.parse(req.query.name);
     let roles = await Role.find(req.query);
     if (roles.length === 0) return Boom.notFound(translate[language].rolesShowAllNotFound);
 
