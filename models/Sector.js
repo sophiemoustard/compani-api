8b08ae607937c466afe669933ee03170f59a08e1
jctrebalag <jctrebalag@gmail.com>
2019-03-07 11:58:47 +0100

    Add numAuxiliary virtual populate to sector model (#125)
    * Add numAuxiliary virtual populate to sector model
    
    * Retours Sophie et Pierre

--- models/Sector.js
+++ models/Sector.js
@@ -1,8 +1,27 @@
 const mongoose = require('mongoose');
 
-const SectorsSchema = mongoose.Schema({
+const SectorSchema = mongoose.Schema({
   name: String,
   company: mongoose.Schema.Types.ObjectId
-}, { timestamps: true });
+}, {
+  timestamps: true,
+  toObject: { virtuals: true },
+  toJSON: { virtuals: true }
+});
 
-module.exports = mongoose.model('Sector', SectorsSchema);
+SectorSchema.virtual('auxiliaryCount', {
+  ref: 'User',
+  localField: '_id',
+  foreignField: 'sector',
+  count: true
+});
+
+const populateAuxiliaryCount = async (docs) => {
+  for (const doc of docs) {
+    await doc.populate('auxiliaryCount').execPopulate();
+  }
+};
+
+SectorSchema.post('find', populateAuxiliaryCount);
+
+module.exports = mongoose.model('Sector', SectorSchema);
