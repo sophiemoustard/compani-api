df27d5e033635fc1c3152acd77d01d2f7b801880
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2019-04-16 19:37:55 +0200

    Add computePayments balance helper

--- helpers/balances.js
+++ helpers/balances.js
@@ -1,10 +1,37 @@
 const { getLastVersion } = require('./utils');
+const { PAYMENT } = require('./constants');
 
 const canBeWithdrawn = (bill) => {
   if (!bill) throw new Error('Bill must be provided');
-  return !!(bill.balance < 0 && !bill._id.tpp && bill.customer.payment && bill.customer.payment.mandates.length > 0 && getLastVersion(bill.customer.payment.mandates, 'createdAt').signedAt);
+  return (
+    bill.balance < 0 &&
+    !bill._id.tpp &&
+    bill.customer.payment &&
+    bill.customer.payment.mandates.length > 0 &&
+    getLastVersion(bill.customer.payment.mandates, 'createdAt').signedAt
+  );
+};
+
+const computeTotal = (nature, total, netInclTaxes) => {
+  if (nature === PAYMENT) return total + netInclTaxes;
+  return total - netInclTaxes;
+};
+
+const reducePayments = ids => (total, next) => {
+  if (ids.tpp && next.client && ids.tpp.toHexString() === next.client.toHexString() && ids.customer.toHexString() === next.customer.toHexString()) {
+    return computeTotal(next.nature, total, next.netInclTaxes);
+  } else if (!ids.tpp && !next.client && ids.customer.toHexString() === next.customer.toHexString()) {
+    return computeTotal(next.nature, total, next.netInclTaxes);
+  }
+  return total;
+};
+
+const computePayments = (payments, ids) => {
+  if (!payments || !Array.isArray(payments) || payments.length === 0) throw new Error('Invalid payments array !');
+  return payments.reduce(reducePayments(ids), 0);
 };
 
 module.exports = {
-  canBeWithdrawn
+  canBeWithdrawn,
+  computePayments,
 };
