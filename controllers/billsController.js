1d5ef7503140c7fa455c1d384ae0937cae38e456
Sophie Moustard <sophie.moustard@gmail.com>
2019-04-17 11:03:46 +0200

    [220] Add cancellation options on draft bill request

--- controllers/billsController.js
+++ controllers/billsController.js
@@ -2,11 +2,10 @@ const Boom = require('boom');
 const { ObjectID } = require('mongodb');
 const moment = require('moment');
 
-const Event = require('../models/Event');
 const BillNumber = require('../models/BillNumber');
 const Bill = require('../models/Bill');
 const translate = require('../helpers/translate');
-const { INTERVENTION } = require('../helpers/constants');
+const { INTERVENTION, INVOICED_AND_NOT_PAYED, INVOICED_AND_PAYED } = require('../helpers/constants');
 const { getDraftBillsList } = require('../helpers/draftBills');
 const { formatAndCreateBills } = require('../helpers/bills');
 const { getDateQuery } = require('../helpers/utils');
@@ -18,7 +17,7 @@ const draftBillsList = async (req) => {
     const rules = [
       { endDate: { $lt: req.query.endDate } },
       { $or: [{ isBilled: false }, { isBilled: { $exists: false } }] },
-      { $or: [{ isCancelled: false }, { isCancelled: { $exists: false } }] },
+      { $or: [{ isCancelled: false }, { isCancelled: { $exists: false } }, { 'cancel.condition': INVOICED_AND_PAYED }, { 'cancel.condition': INVOICED_AND_NOT_PAYED }] },
       { type: INTERVENTION },
     ];
     if (req.query.startDate) rules.push({ startDate: { $gte: req.query.startDate } });
