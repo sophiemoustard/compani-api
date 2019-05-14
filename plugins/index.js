91df3037e8b15928d04d9022f280d82db6236434
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2019-05-14 14:42:40 +0200

    Fix registering of sentry plugin

--- plugins/index.js
+++ plugins/index.js
@@ -1,15 +1,20 @@
 const good = require('./good');
 const hapiSentry = require('./hapiSentry');
 
-exports.plugins = [
+const plugins = [
   {
     plugin: require('good'),
     options: { reporters: good.reporters }
   },
   { plugin: require('hapi-auth-jwt2') },
-  { plugin: require('inert') },
-  {
-    plugin: require('hapi-sentry'),
-    options: hapiSentry.options,
-  },
+  { plugin: require('inert') }
 ];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push({
+    plugin: require('hapi-sentry'),
+    options: hapiSentry.options
+  });
+}
+
+exports.plugins = plugins;
