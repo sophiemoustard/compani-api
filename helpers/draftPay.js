fff3a3d75113a196aa729ab6c6e83ebee5dfc1f9
Sophie Moustard <sophie.moustard@gmail.com>
2019-05-20 14:04:01 +0200

    [276] Populate sector

--- helpers/draftPay.js
+++ helpers/draftPay.js
@@ -4,6 +4,25 @@ exports.getDraftPay = async (rules) => {
   const events = await Event.aggregate([
     { $match: { $and: rules } },
     { $group: { _id: '$auxiliary', events: { $push: '$$ROOT' } } },
+    {
+      $lookup: {
+        from: 'users',
+        localField: '_id',
+        foreignField: '_id',
+        as: 'auxiliary',
+      },
+    },
+    { $unwind: { path: '$auxiliary' } },
+    {
+      $lookup: {
+        from: 'sectors',
+        localField: 'auxiliary.sector',
+        foreignField: '_id',
+        as: 'auxiliary.sector',
+      },
+    },
+    { $unwind: { path: '$auxiliary.sector' } },
+    { $project: { auxiliary: { _id: 1, identity: 1, sector: 1 }, events: 1 } },
   ]);
 
   return events;
