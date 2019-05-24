52d9e54a20940dd846aec16c34d80048f8dad1de
Camille Alemany <camillealemany23@gmail.com>
2019-05-24 17:22:07 +0200

    Add unit tests for distanceMatrix helpers (#221)
    * Add unit tests for distanceMatrix helpers
    
    * Norme units tests for distanceMatrix helper
    
    * Add asserts for units tests of distanceMatrix helper
    
    * Move variables declaration in unit tests of distanceMatrix helper

---
+++ tests/unit/helpers/distanceMatrix.test.js
@@ -0,0 +1,110 @@
+const expect = require('expect');
+const sinon = require('sinon');
+const _ = require('lodash');
+
+const DistanceMatrixHelper = require('../../../helpers/distanceMatrix');
+const DistanceMatrix = require('../../../models/DistanceMatrix');
+const maps = require('../../../models/Google/Maps');
+
+describe('getOrCreateDistanceMatrix', () => {
+  const distanceMatrixRequest = {
+    origins: 'Washington, DC',
+    destinations: 'New York City, NY',
+    mode: 'DRIVING'
+  };
+  const distanceMatrixResult = {
+    data: {
+      rows: [{
+        elements: [{
+          distance: { value: 363998 },
+          duration: { value: 13790 },
+        }]
+      }]
+    },
+    status: 200
+  };
+  let findOne;
+  let save;
+  let getDistanceMatrix;
+
+  beforeEach(() => {
+    findOne = sinon.stub(DistanceMatrix, 'findOne');
+    save = sinon.stub(DistanceMatrix.prototype, 'save').returnsThis();
+    getDistanceMatrix = sinon.stub(maps, 'getDistanceMatrix').returns(distanceMatrixResult);
+  });
+
+  afterEach(() => {
+    findOne.restore();
+    save.restore();
+    getDistanceMatrix.restore();
+  });
+
+  it('should return the document already saved', async () => {
+    findOne.returns({ duration: 13792 });
+
+    const result = await DistanceMatrixHelper.getOrCreateDistanceMatrix(distanceMatrixRequest);
+
+    sinon.assert.calledOnce(findOne);
+    sinon.assert.notCalled(getDistanceMatrix);
+    expect(result).toMatchObject({ duration: 13792 });
+  });
+
+  it('should return null if distance is missing', async () => {
+    findOne.returns(null);
+
+    const mapResult = _.cloneDeep(distanceMatrixResult);
+    delete mapResult.data.rows[0].elements[0].distance;
+    getDistanceMatrix.returns(mapResult);
+
+    const result = await DistanceMatrixHelper.getOrCreateDistanceMatrix(distanceMatrixRequest);
+
+    sinon.assert.calledOnce(getDistanceMatrix);
+    sinon.assert.notCalled(save);
+    expect(result).toBe(null);
+  });
+
+  it('should return null if duration is missing', async () => {
+    findOne.returns(null);
+
+    const mapResult = _.cloneDeep(distanceMatrixResult);
+    delete mapResult.data.rows[0].elements[0].duration;
+    getDistanceMatrix.returns(mapResult);
+
+    const result = await DistanceMatrixHelper.getOrCreateDistanceMatrix(distanceMatrixRequest);
+
+    sinon.assert.calledOnce(getDistanceMatrix);
+    sinon.assert.notCalled(save);
+    expect(result).toBe(null);
+  });
+
+  it('should return null if the request failed', async () => {
+    findOne.returns(null);
+
+    const mapResult = _.cloneDeep(distanceMatrixResult);
+    mapResult.status = 400;
+    getDistanceMatrix.returns(mapResult);
+
+    const result = await DistanceMatrixHelper.getOrCreateDistanceMatrix(distanceMatrixRequest);
+
+    sinon.assert.calledOnce(getDistanceMatrix);
+    sinon.assert.notCalled(save);
+    expect(result).toBe(null);
+  });
+
+  it('should return a new DistanceMatrix', async () => {
+    findOne.returns(null);
+    getDistanceMatrix.returns(distanceMatrixResult);
+
+    const result = await DistanceMatrixHelper.getOrCreateDistanceMatrix(distanceMatrixRequest);
+
+    sinon.assert.calledOnce(save);
+    expect(result).toEqual(expect.objectContaining({
+      _id: expect.any(Object),
+      destinations: 'New York City, NY',
+      distance: 363998,
+      duration: 13790,
+      origins: 'Washington, DC',
+      mode: 'DRIVING'
+    }));
+  });
+});
