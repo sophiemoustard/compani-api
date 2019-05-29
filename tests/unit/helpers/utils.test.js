07598f6686745759ea732955e53a34d4e4e04042
jctrebalag <jctrebalag@gmail.com>
2019-05-29 15:34:00 +0200

    Add mergeLastVersionWithBaseObject util + unit test

--- tests/unit/helpers/utils.test.js
+++ tests/unit/helpers/utils.test.js
@@ -1,14 +1,9 @@
 const expect = require('expect');
 const moment = require('moment');
+const sinon = require('sinon');
+const omit = require('lodash/omit');
 
-const {
-  getLastVersion,
-  getMatchingVersion,
-  clean,
-  getFixedNumber,
-  removeSpaces,
-  formatPrice
-} = require('../../../helpers/utils');
+const UtilsHelper = require('../../../helpers/utils');
 
 describe('getLastVersion', () => {
   it('should return the last version based on the date key', () => {
@@ -25,40 +20,60 @@ describe('getLastVersion', () => {
       }
     ];
 
-    expect(getLastVersion(versions, 'startDate')).toBeDefined();
-    expect(getLastVersion(versions, 'startDate')._id).toEqual(2);
-    expect(getLastVersion(versions, 'createdAt')).toBeDefined();
-    expect(getLastVersion(versions, 'createdAt')._id).toEqual(1);
+    expect(UtilsHelper.getLastVersion(versions, 'startDate')).toBeDefined();
+    expect(UtilsHelper.getLastVersion(versions, 'startDate')._id).toEqual(2);
+    expect(UtilsHelper.getLastVersion(versions, 'createdAt')).toBeDefined();
+    expect(UtilsHelper.getLastVersion(versions, 'createdAt')._id).toEqual(1);
   });
 
   it('should throw an error if version is not an array', () => {
-    expect(() => getLastVersion({ toto: 'lala' }, 'createdAt')).toThrow(new Error('versions must be an array !'));
+    expect(() => UtilsHelper.getLastVersion({ toto: 'lala' }, 'createdAt')).toThrow(new Error('versions must be an array !'));
   });
 
   it('should throw an error if the key is not a string', () => {
-    expect(() => getLastVersion([{ toto: 'lala' }], 3)).toThrow(new Error('sortKey must be a string !'));
+    expect(() => UtilsHelper.getLastVersion([{ toto: 'lala' }], 3)).toThrow(new Error('sortKey must be a string !'));
   });
 
   it('should return null if versions is empty', () => {
-    expect(getLastVersion([], 'toto')).toBeNull();
+    expect(UtilsHelper.getLastVersion([], 'toto')).toBeNull();
   });
 
   it('should return the single element is versions only contains one element', () => {
     const versions = [{ startDate: moment().toISOString(), createdAt: moment().toISOString(), _id: 1 }];
 
-    const result = getLastVersion(versions, 'startDate');
+    const result = UtilsHelper.getLastVersion(versions, 'startDate');
     expect(result).toBeDefined();
     expect(result._id).toEqual(1);
   });
 });
 
+describe('mergeLastVersionWithBaseObject', () => {
+  it('should merge last version of given object with that same object', () => {
+    const baseObj = { tpp: '123456', versions: [{ frequency: 'once', createdAt: moment().toISOString() }] };
+    const getLastVersionStub = sinon.stub(UtilsHelper, 'getLastVersion');
+
+    getLastVersionStub.returns(baseObj.versions[0]);
+    const result = UtilsHelper.mergeLastVersionWithBaseObject(baseObj, 'createdAt');
+    expect(result).toEqual(expect.objectContaining({ ...baseObj.versions[0], ...omit(baseObj, ['versions']) }));
+    sinon.assert.called(getLastVersionStub);
+    getLastVersionStub.restore();
+  });
+
+  it('should throw an error if last version cannot be found', () => {
+    const baseObj = { tpp: '123456', versions: [{ frequency: 'once', createdAt: moment().toISOString() }] };
+    const getLastVersionStub = sinon.stub(UtilsHelper, 'getLastVersion').returns(null);
+    expect(() => UtilsHelper.mergeLastVersionWithBaseObject(baseObj, 'createdAt')).toThrowError('Unable to find last version from base object !');
+    getLastVersionStub.restore();
+  });
+});
+
 describe('getMatchingVersion', () => {
   it('should throw an error if version is not an array', () => {
-    expect(() => getMatchingVersion(moment().toISOString(), { versions: 'lala' }, 'startDate')).toThrow(new Error('versions must be an array !'));
+    expect(() => UtilsHelper.getMatchingVersion(moment().toISOString(), { versions: 'lala' }, 'startDate')).toThrow(new Error('versions must be an array !'));
   });
 
   it('should return null if versions is empty', () => {
-    expect(getMatchingVersion(moment().toISOString(), { versions: [] }, 'startDate')).toBeNull();
+    expect(UtilsHelper.getMatchingVersion(moment().toISOString(), { versions: [] }, 'startDate')).toBeNull();
   });
 
   it('should return the matching version', () => {
@@ -74,7 +89,7 @@ describe('getMatchingVersion', () => {
       ]
     };
 
-    const result = getMatchingVersion(moment().add(1, 'd').toISOString(), obj, 'startDate');
+    const result = UtilsHelper.getMatchingVersion(moment().add(1, 'd').toISOString(), obj, 'startDate');
     expect(result).toBeDefined();
     expect(result.versionId).toEqual(1);
   });
@@ -88,7 +103,7 @@ describe('getMatchingVersion', () => {
       ],
     };
 
-    const result = getMatchingVersion(moment().add(1, 'd').toISOString(), obj, 'startDate');
+    const result = UtilsHelper.getMatchingVersion(moment().add(1, 'd').toISOString(), obj, 'startDate');
     expect(result).toBeDefined();
     expect(result.versionId).toEqual(3);
   });
@@ -112,63 +127,63 @@ describe('getMatchingVersion', () => {
       ]
     };
 
-    expect(getMatchingVersion(moment().add(2, 'd').toISOString(), obj, 'startDate')).toBeNull();
+    expect(UtilsHelper.getMatchingVersion(moment().add(2, 'd').toISOString(), obj, 'startDate')).toBeNull();
   });
 });
 
 describe('clean', () => {
   it('should delete undefined value', () => {
-    const result = clean({ _id: 1, value: undefined });
+    const result = UtilsHelper.clean({ _id: 1, value: undefined });
     expect(result.value).not.toBeDefined();
   });
 
   it('should delete empty array', () => {
-    const result = clean({ _id: 1, value: [] });
+    const result = UtilsHelper.clean({ _id: 1, value: [] });
     expect(result.value).not.toBeDefined();
   });
 
   it('should delete empty object', () => {
-    const result = clean({ _id: 1, value: {} });
+    const result = UtilsHelper.clean({ _id: 1, value: {} });
     expect(result.value).not.toBeDefined();
   });
 
   it('should delete empty string', () => {
-    const result = clean({ _id: 1, value: '' });
+    const result = UtilsHelper.clean({ _id: 1, value: '' });
     expect(result.value).not.toBeDefined();
   });
 });
 
 describe('getFixedNumber', () => {
   it('should return number to string with number of decimals as provided by parameter', () => {
-    const result = getFixedNumber(10, 2);
+    const result = UtilsHelper.getFixedNumber(10, 2);
     expect(result).toBe('10.00');
   });
   it('should return an error if number parameter is not a number', () => {
     expect(() => {
-      getFixedNumber('test', 3);
+      UtilsHelper.getFixedNumber('test', 3);
     }).toThrow(new Error('You must provide a number !'));
   });
 });
 
 describe('removeSpaces', () => {
   it('should remove all spaces from string', () => {
-    const result = removeSpaces('he llo  world  ');
+    const result = UtilsHelper.removeSpaces('he llo  world  ');
     expect(result).toBe('helloworld');
   });
   it('should return an error if parameter is not a string', () => {
     expect(() => {
-      removeSpaces(3);
+      UtilsHelper.removeSpaces(3);
     }).toThrow(new Error('Parameter must be a string !'));
   });
   it('should return an empty string if parameter is missing', () => {
-    const result = removeSpaces();
+    const result = UtilsHelper.removeSpaces();
     expect(result).toBe('');
   });
 });
 
 describe('formatPrice', () => {
   it('should format price', () => {
-    const res = formatPrice(5.5);
+    const res = UtilsHelper.formatPrice(5.5);
     expect(res).toEqual('5,50\u00a0€');
   });
 });
