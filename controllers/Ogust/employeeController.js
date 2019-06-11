cf7a8a58d1cf44ba26742163fb02dfa8d53c1275
Camille Alemany <camillealemany23@gmail.com>
2019-06-11 17:33:54 +0200

    [313] Remove Ogust getById, list, and create routes

--- controllers/Ogust/employeeController.js
+++ controllers/Ogust/employeeController.js
@@ -1,54 +1,9 @@
-const moment = require('moment');
-const flat = require('flat');
 const Boom = require('boom');
 
 const translate = require('../../helpers/translate');
 const employees = require('../../models/Ogust/Employee');
-const Counter = require('../../models/idNumber');
-
 
 const { language } = translate;
-
-const list = async (req) => {
-  try {
-    const params = req.query;
-    params.token = req.headers['x-ogust-token'];
-    const users = await employees.getEmployees(params);
-    if (users.data.status == 'KO') {
-      return Boom.badRequest(users.data.message);
-      // throw new Error(`Error while getting employees: ${result.data.message}`);
-    }
-    return {
-      message: translate[language].userShowAllFound,
-      data: { users: users.data }
-    };
-  } catch (e) {
-    req.log('error', e);
-    return Boom.badImplementation(e);
-  }
-};
-
-const getById = async (req) => {
-  try {
-    const params = {
-      token: req.headers['x-ogust-token'],
-      id_employee: req.params.id
-    };
-    const user = await employees.getEmployeeById(params);
-    if (user.data.status == 'KO') {
-      return Boom.badRequest(user.data.message);
-    } else if (Object.keys(user.data.employee).length === 0) {
-      return Boom.notFound();
-    }
-    return {
-      message: translate[language].userFound,
-      data: { user: user.data }
-    };
-  } catch (e) {
-    req.log('error', e);
-    return Boom.badImplementation(e);
-  }
-};
 
 const getEmployeeSalaries = async (req) => {
   try {
@@ -71,33 +26,6 @@ const getEmployeeSalaries = async (req) => {
   }
 };
 
-const create = async (req) => {
-  try {
-    const query = {
-      idNumber: { prefix: `SA${moment().format('YYMM')}` },
-    };
-    const payload = {
-      idNumber: { seq: 1 }
-    };
-    const number = await Counter.findOneAndUpdate(flat(query), { $inc: flat(payload) }, { new: true, upsert: true, setDefaultsOnInsert: true });
-    const idNumber = `${number.idNumber.prefix}-${number.idNumber.seq.toString().padStart(3, '0')}`;
-    const params = req.payload;
-    params.token = req.headers['x-ogust-token'];
-    params.number = idNumber;
-    const user = await employees.createEmployee(params);
-    return {
-      message: translate[language].userSaved,
-      data: user.data
-    };
-  } catch (e) {
-    req.log('error', e);
-    return Boom.badImplementation(e);
-  }
-};
-
 module.exports = {
-  list,
-  getById,
   getEmployeeSalaries,
-  create,
 };
--- routes/Ogust/employee.js
+++ routes/Ogust/employee.js
@@ -2,54 +2,11 @@
 
 const Joi = require('joi');
 
-const {
-  list,
-  getById,
-  getEmployeeSalaries,
-  create,
-} = require('../../controllers/Ogust/employeeController');
+const { getEmployeeSalaries } = require('../../controllers/Ogust/employeeController');
 
 exports.plugin = {
   name: 'routes-ogust-employees',
   register: async (server) => {
-    // Get all employees
-    server.route({
-      method: 'GET',
-      path: '/',
-      options: {
-        validate: {
-          headers: Joi.object().keys({
-            'x-ogust-token': Joi.string().required()
-          }).options({ allowUnknown: true }),
-          query: {
-            status: Joi.string().default('A'),
-            nature: Joi.string().default('S'),
-            mobile_phone: Joi.string().regex(/^[0]{1}[1-9]{1}[0-9]{8}$/),
-            sector: Joi.string(),
-            email: Joi.string().email(),
-            nbperpage: Joi.number().default(100),
-            pagenum: Joi.number().default(1)
-          }
-        },
-        auth: false
-      },
-      handler: list
-    });
-    // Get employee by id
-    server.route({
-      method: 'GET',
-      path: '/{id}',
-      options: {
-        validate: {
-          headers: Joi.object().keys({
-            'x-ogust-token': Joi.string().required()
-          }).options({ allowUnknown: true }),
-          params: { id: [Joi.number(), Joi.string()] },
-        },
-        auth: false
-      },
-      handler: getById
-    });
     // Get employee salaries
     server.route({
       method: 'GET',
@@ -68,42 +25,6 @@ exports.plugin = {
         auth: false
       },
       handler: getEmployeeSalaries
-    });
-    // Create employee
-    server.route({
-      method: 'POST',
-      path: '/',
-      options: {
-        validate: {
-          headers: Joi.object().keys({
-            'x-ogust-token': Joi.string().required()
-          }).options({ allowUnknown: true }),
-          payload: Joi.object().keys({
-            title: Joi.string().required(),
-            last_name: Joi.string().required(),
-            first_name: Joi.string(),
-            main_address: Joi.object().keys({
-              line: Joi.string(),
-              supplement: Joi.string(),
-              zip: Joi.string(),
-              city: Joi.string(),
-              type: Joi.string().default('Adrpri'),
-              country: Joi.string().default('FR')
-            }).required(),
-            email: Joi.string().email().required(),
-            sector: Joi.string(),
-            mobile_phone: Joi.string().regex(/^[0]{1}[1-9]{1}[0-9]{8}$/),
-            picture: Joi.string(),
-            nature: Joi.string().default('S'),
-            status: Joi.string().default('A'),
-            method_of_payment: Joi.string().default('7268'),
-            manager: Joi.string().default('232220179'),
-            default_means_of_transport: Joi.string().default('C')
-          }).required()
-        },
-        auth: false
-      },
-      handler: create
     });
   },
 };
