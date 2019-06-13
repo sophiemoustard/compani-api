f508f42eb4931707cd5559c402889f91e00c3289
Sophie Moustard <sophie.moustard@gmail.com>
2019-06-13 11:27:05 +0200

    [313] Remove ogust list

--- models/Ogust/Utils.js
+++
@@ -1,5 +0,0 @@
-const { Ogust } = require('../../config/config');
-const axios = require('axios');
-
-// Get List
-exports.getList = async payload => axios.post(`${Ogust.API_LINK}getList`, payload);
--- routes/Ogust/utils.js
+++ routes/Ogust/utils.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const Joi = require('joi');
-
 const { getOgustToken } = require('../../controllers/Ogust/tokenController');
-const { getList } = require('../../controllers/Ogust/utilsController');
 
 exports.plugin = {
   name: 'routes-ogust-utils',
@@ -12,27 +9,8 @@ exports.plugin = {
     server.route({
       method: 'GET',
       path: '/token',
-      options: {
-        auth: 'jwt'
-      },
-      handler: getOgustToken
-    });
-    // Get List
-    server.route({
-      method: 'POST',
-      path: '/utils/getList',
-      options: {
-        validate: {
-          headers: Joi.object().keys({
-            'x-ogust-token': Joi.string().required()
-          }).options({ allowUnknown: true }),
-          query: {
-            key: Joi.string().required()
-          }
-        },
-        auth: false
-      },
-      handler: getList
+      options: { auth: 'jwt' },
+      handler: getOgustToken,
     });
   }
 };
