21ce44784bfdf6cedf8039c3234943b3699e0c04
Sophie Moustard <sophie.moustard@gmail.com>
2019-07-01 14:49:27 +0200

    [377] fix drive upload (#285)
    * [377] fix drive upload
    
    * [377] Refacto retour camille
    
    * [377] Remove unecessary async

--- models/Google/Drive.js
+++ models/Google/Drive.js
@@ -1,78 +1,71 @@
 const fs = require('fs');
 const { google } = require('googleapis');
 
-const jwtClient = () => {
-  const client = new google.auth.JWT(
-    process.env.GOOGLE_DRIVE_API_EMAIL,
-    null,
-    process.env.GOOGLE_DRIVE_API_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    ['https://www.googleapis.com/auth/drive'],
-    null
-  );
-
-  client.authorize((err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-
-  return client;
-};
+const jwtClient = () => new google.auth.JWT(
+  process.env.GOOGLE_DRIVE_API_EMAIL,
+  null,
+  process.env.GOOGLE_DRIVE_API_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  ['https://www.googleapis.com/auth/drive'],
+  null
+);
 
 const drive = google.drive('v3');
 
-exports.add = params => new Promise((resolve, reject) => {
+exports.add = async (params) => {
   const fileMetadata = {
     name: params.name,
     mimeType: params.folder ? 'application/vnd.google-apps.folder' : null,
     parents: [params.parentFolderId] || []
   };
   const media = params.folder ? null : { body: params.body, mimeType: params.type };
-  drive.files.create({
-    auth: jwtClient(),
-    resource: fileMetadata,
-    media,
-    fields: 'id'
-  }, (err, item) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(item.data);
-    }
-  });
-});
 
-exports.deleteFile = params => new Promise((resolve, reject) => {
-  drive.files.delete(
-    { auth: jwtClient(), fileId: params.fileId },
-    (err, file) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(file.data);
+  const auth = jwtClient();
+  await auth.authorize();
+
+  return new Promise((resolve, reject) => {
+    drive.files.create(
+      { auth, resource: fileMetadata, media, fields: 'id' },
+      (err, item) => {
+        if (err) reject(new Error(`Google Drive API ${err}`));
+        else resolve(item.data);
       }
-    }
-  );
-});
-
-exports.getFileById = params => new Promise((resolve, reject) => {
-  drive.files.get({
-    auth: jwtClient(),
-    fileId: `${params.fileId}`,
-    fields: ['name, webViewLink, thumbnailLink']
-  }, (err, response) => {
-    if (err) {
-      reject(new Error(`Google Drive API ${err}`));
-    } else {
-      resolve(response.data);
-    }
+    );
   });
-});
+};
 
-exports.downloadFileById = params => new Promise((resolve, reject) => {
+exports.deleteFile = async (params) => {
+  const auth = jwtClient();
+  await auth.authorize();
+
+  return new Promise((resolve, reject) => drive.files.delete(
+    { auth, fileId: params.fileId },
+    (err, file) => {
+      if (err) reject(new Error(`Google Drive API ${err}`));
+      else resolve(file.data);
+    }
+  ));
+};
+
+exports.getFileById = async (params) => {
+  const auth = jwtClient();
+  await auth.authorize();
+
+  return new Promise((resolve, reject) => drive.files.get(
+    { auth, fileId: `${params.fileId}`, fields: ['name, webViewLink, thumbnailLink'] },
+    (err, response) => {
+      if (err) reject(new Error(`Google Drive API ${err}`));
+      else resolve(response.data);
+    }
+  ));
+};
+
+exports.downloadFileById = async (params) => {
   const dest = fs.createWriteStream(params.tmpFilePath);
-  drive.files.get(
-    { auth: jwtClient(), fileId: `${params.fileId}`, alt: 'media' },
+  const auth = jwtClient();
+  await auth.authorize();
+
+  return new Promise((resolve, reject) => drive.files.get(
+    { auth, fileId: `${params.fileId}`, alt: 'media' },
     { responseType: 'stream' },
     (err, res) => {
       if (err || !res || !res.data) {
@@ -85,20 +78,20 @@ exports.downloadFileById = params => new Promise((resolve, reject) => {
         reject(new Error(`Error during Google drive doc download ${err}`));
       }).pipe(dest);
     }
-  );
-});
+  ));
+};
 
-exports.list = params => new Promise((resolve, reject) => {
-  drive.files.list({
-    auth: jwtClient(),
+exports.list = async (params) => {
+  const auth = jwtClient();
+  await auth.authorize();
+
+  return new Promise((resolve, reject) => drive.files.list({
+    auth,
     ...(params.folderId && { q: `'${params.folderId}' in parents and mimeType != 'application/vnd.google-apps.folder'` }),
     fields: 'nextPageToken, files(name, webViewLink, createdTime)',
     pageToken: params.nextPageToken || '',
   }, (err, response) => {
-    if (err) {
-      reject(new Error(`Google Drive API ${err}`));
-    } else {
-      resolve(response.data);
-    }
-  });
-});
+    if (err) reject(new Error(`Google Drive API ${err}`));
+    else resolve(response.data);
+  }));
+};
