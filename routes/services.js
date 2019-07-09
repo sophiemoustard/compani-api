4972d41ed92d492c44f0928bd06ae5d41e316f82
jctrebalag <jctrebalag@gmail.com>
2019-07-09 16:17:20 +0200

    [COM-457] Add scope to services routes

--- routes/services.js
+++ routes/services.js
@@ -20,7 +20,7 @@ exports.plugin = {
       path: '/',
       handler: create,
       options: {
-        auth: { strategy: 'jwt' },
+        auth: { strategy: 'jwt', scope: ['rhConfig:edit'] },
         validate: {
           payload: Joi.object().keys({
             type: Joi.string().required().valid(CUSTOMER_CONTRACT, COMPANY_CONTRACT),
@@ -43,7 +43,7 @@ exports.plugin = {
       path: '/',
       handler: list,
       options: {
-        auth: { strategy: 'jwt' },
+        auth: { strategy: 'jwt', scope: ['rhConfig:read'] },
         validate: {
           query: { company: Joi.objectId() }
         }
@@ -55,7 +55,7 @@ exports.plugin = {
       path: '/{_id}',
       handler: remove,
       options: {
-        auth: { strategy: 'jwt' },
+        auth: { strategy: 'jwt', scope: ['rhConfig:edit'] },
         validate: {
           params: {
             _id: Joi.objectId().required(),
@@ -69,7 +69,7 @@ exports.plugin = {
       path: '/{_id}',
       handler: update,
       options: {
-        auth: { strategy: 'jwt' },
+        auth: { strategy: 'jwt', scope: ['rhConfig:edit'] },
         validate: {
           params: {
             _id: Joi.objectId().required(),
