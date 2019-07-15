a6c98447d74a64e4e86ebe6c629fef7788432810
Sophie Moustard <sophie.moustard@gmail.com>
2019-07-15 14:03:32 +0200

    COM-389 - Update role model

--- models/Role.js
+++ models/Role.js
@@ -2,29 +2,18 @@ const mongoose = require('mongoose');
 const autopopulate = require('mongoose-autopopulate');
 
 const RoleSchema = mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    required: true,
-    dropDups: true,
-    default: 'Invité'
-  },
+  name: { type: String, unique: true, required: true },
   rights: [{
     right_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Right',
-      autopopulate: { select: 'name description permission _id' }
+      autopopulate: { select: 'name description permission _id' },
     },
-    hasAccess: {
-      type: Boolean,
-      default: false
-    },
+    hasAccess: { type: Boolean, default: false },
     rolesConcerned: [{
-      role_id: {
-        type: mongoose.Schema.Types.ObjectId,
-      },
-      name: String
-    }]
+      role_id: { type: mongoose.Schema.Types.ObjectId },
+      name: String,
+    }],
   }],
 }, { timestamps: true });
 
