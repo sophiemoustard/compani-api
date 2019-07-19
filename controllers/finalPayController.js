278c384c01a9daf385c47b0b133b306237b6b4f2
Camille Alemany <camillealemany23@gmail.com>
2019-07-19 10:27:45 +0200

    COM-495 Fix error handling of finalPayController

--- controllers/finalPayController.js
+++ controllers/finalPayController.js
@@ -15,7 +15,7 @@ const draftFinalPayList = async (req) => {
     };
   } catch (e) {
     req.log('error', e);
-    Boom.badImplementation(e);
+    return Boom.badImplementation(e);
   }
 };
 
@@ -35,7 +35,7 @@ const createList = async (req) => {
     return { message: translate[language].finalPayListCreated };
   } catch (e) {
     req.log('error', e);
-    Boom.badImplementation(e);
+    return Boom.badImplementation(e);
   }
 };
 
