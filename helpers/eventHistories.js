149b3542d2953ef3b8398dca60239ee165c8ac2f
Sophie Moustard <sophie.moustard@gmail.com>
2019-07-25 14:42:26 +0200

    COM-515 - dates update history

--- helpers/eventHistories.js
+++ helpers/eventHistories.js
@@ -1,5 +1,6 @@
+const moment = require('moment');
 const EventHistory = require('../models/EventHistory');
-const { EVENT_CREATION, EVENT_DELETION, EVENT_UPDATE, INTERNAL_HOUR } = require('./constants');
+const { EVENT_CREATION, EVENT_DELETION, EVENT_UPDATE, INTERNAL_HOUR, ABSENCE } = require('./constants');
 const UtilsHelper = require('./utils');
 
 exports.getListQuery = (query) => {
@@ -60,10 +61,16 @@ exports.createEventHistoryOnUpdate = async (payload, event, credentials) => {
   };
   if (payload.shouldUpdateRepetition) eventHistory.event.repetition = repetition;
   if (event.type === INTERNAL_HOUR) eventHistory.event.internalHour = payload.internalHour || event.internalHour;
+  if (event.type === ABSENCE) eventHistory.event.absence = payload.absence || event.absence;
 
   if ((event.auxiliary && event.auxiliary.toHexString() !== payload.auxiliary) || (!event.auxiliary && payload.auxiliary)) {
     const auxiliaryUpdateHistory = exports.formatEventHistoryForAuxiliaryUpdate(eventHistory, payload, event);
     promises.push((new EventHistory(auxiliaryUpdateHistory)).save());
+  }
+
+  if (!moment(event.startDate).isSame(payload.startDate, 'day') || !moment(event.endDate).isSame(payload.endDate, 'day')) {
+    const datesUpdateHistory = exports.formatEventHistoryForDatesUpdate(eventHistory, payload, event);
+    promises.push((new EventHistory(datesUpdateHistory)).save());
   }
 
   await Promise.all(promises);
@@ -92,4 +99,24 @@ exports.formatEventHistoryForAuxiliaryUpdate = (mainInfo, payload, event) => {
   else auxiliaryUpdateHistory.sectors = [event.sector.toHexString(), payload.sector];
 
   return auxiliaryUpdateHistory;
+};
+
+exports.formatEventHistoryForDatesUpdate = (mainInfo, payload, event) => {
+  const datesUpdateHistory = {
+    ...mainInfo,
+    sectors: [payload.sector],
+    update: {
+      startDate: { from: event.startDate, to: payload.startDate },
+    },
+  };
+
+  const isOneDayEvent = moment(event.endDate).isSame(event.startDate, 'day') && moment(payload.endDate).isSame(payload.startDate, 'day');
+  if (!isOneDayEvent) datesUpdateHistory.update.endDate = { from: event.endDate, to: payload.endDate };
+
+  if (payload.auxiliary) {
+    datesUpdateHistory.auxiliaries = [payload.auxiliary];
+    datesUpdateHistory.event.auxiliary = payload.auxiliary;
+  }
+
+  return datesUpdateHistory;
 };
