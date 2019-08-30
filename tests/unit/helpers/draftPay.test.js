26b5bc563c5b652ca293b03ff080744e5a06259b
Sophie Moustard <sophie.moustard@gmail.com>
2019-08-30 09:39:19 +0200

    COM-575 - contract start date on pay if start during period (#387)

--- tests/unit/helpers/draftPay.test.js
+++ tests/unit/helpers/draftPay.test.js
@@ -1203,7 +1203,7 @@ describe('getDraftPayByAuxiliary', () => {
     expect(result).toEqual({
       auxiliaryId: '1234567890',
       auxiliary: { _id: '1234567890', identity: { firstname: 'Hugo', lastname: 'Lloris' }, sector: { name: 'La ruche' } },
-      startDate: '2019-05-01T00:00:00',
+      startDate: '2019-05-13T00:00:00',
       endDate: '2019-05-31T23:59:59',
       month: '05-2019',
       contractHours: 150,
