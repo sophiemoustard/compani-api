30d67ae1f0da5180ce5299c75664d429794fa871
Sophie Moustard <sophie.moustard@gmail.com>
2019-09-09 10:49:16 +0200

    COM-582 - Update contacts on eventValidation

--- helpers/eventsValidation.js
+++ helpers/eventsValidation.js
@@ -6,12 +6,59 @@ const {
   ABSENCE,
   UNAVAILABILITY,
   NEVER,
+  COMPANY_CONTRACT,
+  CUSTOMER_CONTRACT,
+  INTERNAL_HOUR,
 } = require('./constants');
 const User = require('../models/User');
-const EventsHelper = require('./events');
+const Customer = require('../models/Customer');
+const Contract = require('../models/Contract');
+const { populateSubscriptionsServices } = require('../helpers/subscriptions');
 const EventRepository = require('../repositories/EventRepository');
 
 momentRange.extendMoment(moment);
+
+exports.auxiliaryHasActiveCompanyContractOnDay = (contracts, day) =>
+  contracts.some(contract =>
+    contract.status === COMPANY_CONTRACT &&
+      moment(contract.startDate).isSameOrBefore(day, 'd') &&
+      (!contract.endDate || (contract.endDate && moment(contract.endDate).isSameOrAfter(day, 'd'))));
+
+exports.checkContracts = async (event, user) => {
+  if (!user.contracts || user.contracts.length === 0) return false;
+
+  // If the event is an intervention :
+  // - if it's a customer contract subscription, the auxiliary should have an active contract with the customer on the day of the intervention
+  // - else (company contract subscription) the auxiliary should have an active contract on the day of the intervention and this customer
+  //   should have an active subscription
+  if (event.type === INTERVENTION) {
+    let customer = await Customer
+      .findOne({ _id: event.customer })
+      .populate({ path: 'subscriptions.service', populate: { path: 'versions.surcharge' } })
+      .lean();
+    customer = populateSubscriptionsServices(customer);
+
+    const eventSubscription = customer.subscriptions.find(sub => sub._id.toHexString() == event.subscription);
+    if (!eventSubscription) return false;
+
+    if (eventSubscription.service.type === CUSTOMER_CONTRACT) {
+      const contractBetweenAuxAndCus = await Contract.findOne({ user: event.auxiliary, customer: event.customer });
+      if (!contractBetweenAuxAndCus) return false;
+      return contractBetweenAuxAndCus.endDate
+        ? moment(event.startDate).isBetween(contractBetweenAuxAndCus.startDate, contractBetweenAuxAndCus.endDate, '[]')
+        : moment(event.startDate).isSameOrAfter(contractBetweenAuxAndCus.startDate);
+    }
+
+    return exports.auxiliaryHasActiveCompanyContractOnDay(user.contracts, event.startDate);
+  }
+
+  // If the auxiliary is only under customer contract, create internal hours is not allowed
+  if (event.type === INTERNAL_HOUR) {
+    return exports.auxiliaryHasActiveCompanyContractOnDay(user.contracts, event.startDate);
+  }
+
+  return true;
+};
 
 exports.hasConflicts = async (event) => {
   const { _id, auxiliary, startDate, endDate } = event;
@@ -35,7 +82,7 @@ exports.isCreationAllowed = async (event) => {
   if (!event.auxiliary) return event.type === INTERVENTION;
 
   const user = await User.findOne({ _id: event.auxiliary }).populate('contracts').lean();
-  if (!await EventsHelper.checkContracts(event, user)) return false;
+  if (!await exports.checkContracts(event, user)) return false;
 
   if (!(isRepetition(event) && event.type === INTERVENTION) && await exports.hasConflicts(event)) return false;
 
@@ -56,7 +103,7 @@ exports.isEditionAllowed = async (eventFromDB, payload) => {
   if (!event.auxiliary) return event.type === INTERVENTION;
 
   const user = await User.findOne({ _id: event.auxiliary }).populate('contracts').lean();
-  if (!await EventsHelper.checkContracts(event, user)) return false;
+  if (!await exports.checkContracts(event, user)) return false;
 
   if (!(isRepetition(event) && event.type === INTERVENTION) && !event.isCancelled && (await exports.hasConflicts(event))) return false;
 
--- tests/unit/helpers/eventsValidation.test.js
+++ tests/unit/helpers/eventsValidation.test.js
@@ -1,17 +1,355 @@
 const expect = require('expect');
 const sinon = require('sinon');
+const moment = require('moment');
 const { ObjectID } = require('mongodb');
 const User = require('../../../models/User');
-const EventHelper = require('../../../helpers/events');
+const Customer = require('../../../models/Customer');
+const Contract = require('../../../models/Contract');
+const Surcharge = require('../../../models/Surcharge');
 const EventsValidationHelper = require('../../../helpers/eventsValidation');
 const EventRepository = require('../../../repositories/EventRepository');
 const {
   INTERVENTION,
   ABSENCE,
   INTERNAL_HOUR,
+  CUSTOMER_CONTRACT,
+  COMPANY_CONTRACT,
 } = require('../../../helpers/constants');
 
 require('sinon-mongoose');
+
+describe('auxiliaryHasActiveCompanyContractOnDay', () => {
+  it('should return false as no company contract', () => {
+    const contracts = [{ status: CUSTOMER_CONTRACT }];
+    const date = '2019-01-11T08:38:18';
+    const result = EventsValidationHelper.auxiliaryHasActiveCompanyContractOnDay(contracts, date);
+
+    expect(result).toBeFalsy();
+  });
+
+  it('should return false as no company contract on day (startDate after day)', () => {
+    const contracts = [
+      { status: CUSTOMER_CONTRACT },
+      { status: COMPANY_CONTRACT, startDate: '2019-03-11T08:38:18' },
+    ];
+    const date = '2019-01-11T08:38:18';
+    const result = EventsValidationHelper.auxiliaryHasActiveCompanyContractOnDay(contracts, date);
+
+    expect(result).toBeFalsy();
+  });
+
+  it('should return false as no company contract on day (end date before day)', () => {
+    const contracts = [
+      { status: CUSTOMER_CONTRACT },
+      { status: COMPANY_CONTRACT, startDate: '2019-01-01T08:38:18', endDate: '2019-01-10T08:38:18' },
+    ];
+    const date = '2019-01-11T08:38:18';
+    const result = EventsValidationHelper.auxiliaryHasActiveCompanyContractOnDay(contracts, date);
+
+    expect(result).toBeFalsy();
+  });
+
+  it('should return true as company contract on day (end date after day)', () => {
+    const contracts = [
+      { status: CUSTOMER_CONTRACT },
+      { status: COMPANY_CONTRACT, startDate: '2019-01-01T08:38:18', endDate: '2019-01-31T08:38:18' },
+    ];
+    const date = '2019-01-11T08:38:18';
+    const result = EventsValidationHelper.auxiliaryHasActiveCompanyContractOnDay(contracts, date);
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should return true as company contract on day (no endDate)', () => {
+    const contracts = [
+      { status: CUSTOMER_CONTRACT },
+      { status: COMPANY_CONTRACT, startDate: '2019-01-01T08:38:18' },
+    ];
+    const date = '2019-01-11T08:38:18';
+    const result = EventsValidationHelper.auxiliaryHasActiveCompanyContractOnDay(contracts, date);
+
+    expect(result).toBeTruthy();
+  });
+});
+
+describe('checkContracts', () => {
+  let hasConflicts;
+  let UserModel;
+  let CustomerModel;
+  let findOneContract;
+  let findOneSurcharge;
+  beforeEach(() => {
+    hasConflicts = sinon.stub(EventsValidationHelper, 'hasConflicts');
+    UserModel = sinon.mock(User);
+    CustomerModel = sinon.mock(Customer);
+    findOneContract = sinon.stub(Contract, 'findOne');
+    findOneSurcharge = sinon.stub(Surcharge, 'findOne');
+  });
+  afterEach(() => {
+    hasConflicts.restore();
+    UserModel.restore();
+    CustomerModel.restore();
+    findOneContract.restore();
+    findOneSurcharge.restore();
+  });
+
+  it('should return false as user has no contract', async () => {
+    const event = { auxiliary: (new ObjectID()).toHexString() };
+    const user = { _id: event.auxiliary };
+
+    hasConflicts.returns(false);
+    const result = await EventsValidationHelper.checkContracts(event, user);
+
+    expect(result).toBeFalsy();
+  });
+
+  it('should return false if service event is customer contract and auxiliary does not have contract with customer', async () => {
+    const subscriptionId = new ObjectID();
+    const sectorId = new ObjectID();
+    const event = {
+      auxiliary: (new ObjectID()).toHexString(),
+      customer: (new ObjectID()).toHexString(),
+      type: INTERVENTION,
+      subscription: subscriptionId.toHexString(),
+      startDate: '2019-10-02T08:00:00.000Z',
+      endDate: '2019-10-02T10:00:00.000Z',
+      sector: sectorId.toHexString(),
+    };
+    const customer = {
+      _id: event.customer,
+      subscriptions: [{
+        _id: subscriptionId,
+        service: { type: CUSTOMER_CONTRACT, versions: [{ startDate: '2019-10-02T00:00:00.000Z' }, { startDate: '2018-10-02T00:00:00.000Z' }] },
+        versions: [{ startDate: moment(event.startDate).subtract(1, 'd') }],
+      }],
+    };
+    CustomerModel.expects('findOne')
+      .chain('populate')
+      .chain('lean')
+      .once()
+      .returns(customer);
+
+    const contract = new Contract({
+      user: event.auxiliary,
+      customer: event.customer,
+      versions: [{}],
+      startDate: moment(event.startDate).add(1, 'd'),
+    });
+    findOneContract.returns(contract);
+
+    const user = { _id: event.auxiliary, contracts: [contract], sector: sectorId };
+
+    const result = await EventsValidationHelper.checkContracts(event, user);
+    expect(result).toBeFalsy();
+  });
+
+  it('should return true if service event is customer contract and auxiliary has contract with customer', async () => {
+    const subscriptionId = new ObjectID();
+    const sectorId = new ObjectID();
+    const event = {
+      auxiliary: (new ObjectID()).toHexString(),
+      customer: (new ObjectID()).toHexString(),
+      type: INTERVENTION,
+      subscription: subscriptionId.toHexString(),
+      startDate: '2019-10-03T08:00:00.000Z',
+      endDate: '2019-10-03T10:00:00.000Z',
+      sector: sectorId.toHexString(),
+    };
+
+    const customer = {
+      _id: event.customer,
+      subscriptions: [{
+        _id: subscriptionId,
+        service: { type: CUSTOMER_CONTRACT, versions: [{ startDate: '2019-10-02T00:00:00.000Z' }, { startDate: '2018-10-02T00:00:00.000Z' }] },
+        versions: [{ startDate: moment(event.startDate).subtract(1, 'd') }],
+      }],
+    };
+    CustomerModel.expects('findOne')
+      .chain('populate')
+      .chain('lean')
+      .once()
+      .returns(customer);
+
+    const contract = {
+      user: event.auxiliary,
+      customer: event.customer,
+      versions: [{ weeklyHours: 12 }],
+      startDate: moment(event.startDate).subtract(1, 'd'),
+    };
+    findOneContract.returns(contract);
+
+    const user = { _id: event.auxiliary, contracts: [contract], sector: sectorId };
+
+    const result = await EventsValidationHelper.checkContracts(event, user);
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should return false if company contract and no active contract on day', async () => {
+    const subscriptionId = new ObjectID();
+    const sectorId = new ObjectID();
+    const event = {
+      auxiliary: (new ObjectID()).toHexString(),
+      customer: (new ObjectID()).toHexString(),
+      type: INTERVENTION,
+      subscription: subscriptionId.toHexString(),
+      startDate: '2019-10-03T08:00:00.000Z',
+      endDate: '2019-10-03T10:00:00.000Z',
+      sector: sectorId.toHexString(),
+    };
+
+    const customer = {
+      _id: event.customer,
+      subscriptions: [{
+        _id: subscriptionId,
+        service: { type: COMPANY_CONTRACT, versions: [{ startDate: '2019-10-02T00:00:00.000Z' }, { startDate: '2018-10-02T00:00:00.000Z' }] },
+        versions: [{ startDate: moment(event.startDate).subtract(1, 'd') }],
+      }],
+    };
+    CustomerModel.expects('findOne')
+      .chain('populate')
+      .chain('lean')
+      .once()
+      .returns(customer);
+
+    const contract = {
+      user: event.auxiliary,
+      customer: event.customer,
+      versions: [{}],
+      status: COMPANY_CONTRACT,
+      startDate: moment(event.startDate).add(1, 'd'),
+    };
+    findOneContract.returns(contract);
+
+    const user = { _id: event.auxiliary, contracts: [contract], sector: sectorId };
+
+    const result = await EventsValidationHelper.checkContracts(event, user);
+
+    expect(result).toBeFalsy();
+  });
+
+  it('should return true if company contract and active contract on day', async () => {
+    const subscriptionId = new ObjectID();
+    const sectorId = new ObjectID();
+    const event = {
+      auxiliary: (new ObjectID()).toHexString(),
+      customer: (new ObjectID()).toHexString(),
+      type: INTERVENTION,
+      subscription: subscriptionId.toHexString(),
+      startDate: '2019-10-03T08:00:00.000Z',
+      endDate: '2019-10-03T10:00:00.000Z',
+      sector: sectorId.toHexString(),
+    };
+
+    const customer = {
+      _id: event.customer,
+      subscriptions: [{
+        _id: subscriptionId,
+        service: { type: COMPANY_CONTRACT, versions: [{ startDate: '2019-10-02T00:00:00.000Z' }, { startDate: '2018-10-02T00:00:00.000Z' }] },
+        versions: [{ startDate: moment(event.startDate).subtract(1, 'd') }],
+      }],
+    };
+    CustomerModel.expects('findOne')
+      .chain('populate')
+      .chain('lean')
+      .once()
+      .returns(customer);
+
+    const contract = {
+      user: event.auxiliary,
+      customer: event.customer,
+      versions: [{ weeklyHours: 12 }],
+      status: COMPANY_CONTRACT,
+      startDate: moment(event.startDate).subtract(1, 'd'),
+    };
+    findOneContract.returns(contract);
+
+    const user = { _id: event.auxiliary, contracts: [contract], sector: sectorId };
+
+    const result = await EventsValidationHelper.checkContracts(event, user);
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should return false if company contract and customer has no subscription', async () => {
+    const sectorId = new ObjectID();
+    const event = {
+      auxiliary: (new ObjectID()).toHexString(),
+      customer: (new ObjectID()).toHexString(),
+      type: INTERVENTION,
+      subscription: (new ObjectID()).toHexString(),
+      startDate: '2019-10-03T08:00:00.000Z',
+      endDate: '2019-10-03T10:00:00.000Z',
+      sector: sectorId.toHexString(),
+    };
+
+    const customer = {
+      _id: event.customer,
+      subscriptions: [{
+        _id: new ObjectID(),
+        service: { type: COMPANY_CONTRACT, versions: [{ startDate: '2019-10-02T00:00:00.000Z' }, { startDate: '2018-10-02T00:00:00.000Z' }] },
+        versions: [{ startDate: moment(event.startDate).add(1, 'd') }],
+      }],
+    };
+    CustomerModel.expects('findOne')
+      .chain('populate')
+      .chain('lean')
+      .once()
+      .returns(customer);
+
+    const contract = {
+      user: event.auxiliary,
+      customer: event.customer,
+      versions: [{ weeklyHours: 12 }],
+      status: COMPANY_CONTRACT,
+      startDate: moment(event.startDate).subtract(1, 'd'),
+    };
+    findOneContract.returns(contract);
+
+    const user = { _id: event.auxiliary, contracts: [contract], sector: sectorId };
+
+    const result = await EventsValidationHelper.checkContracts(event, user);
+
+    expect(result).toBeFalsy();
+  });
+
+  it('should return false if event is internal hour and auxiliary does not have contract with company', async () => {
+    const sectorId = new ObjectID();
+    const event = {
+      auxiliary: (new ObjectID()).toHexString(),
+      type: INTERNAL_HOUR,
+      startDate: '2019-10-03T00:00:00.000Z',
+      sector: sectorId.toHexString(),
+    };
+
+    const customer = {
+      _id: event.customer,
+      subscriptions: [{
+        _id: event.subscription,
+        service: { type: '', versions: [{ startDate: '2019-10-02T00:00:00.000Z' }, { startDate: '2018-10-02T00:00:00.000Z' }] },
+      }],
+    };
+    CustomerModel.expects('findOne')
+      .chain('populate')
+      .chain('lean')
+      .once()
+      .returns(customer);
+
+    const contract = {
+      user: event.auxiliary,
+      customer: event.customer,
+      versions: [{}],
+      status: CUSTOMER_CONTRACT,
+    };
+    findOneContract.returns(contract);
+
+    const user = { _id: event.auxiliary, contracts: [contract], sector: sectorId };
+
+    const result = await EventsValidationHelper.checkContracts(event, user);
+
+    expect(result).toBeFalsy();
+  });
+});
 
 describe('hasConflicts', () => {
   let getAuxiliaryEventsBetweenDates;
@@ -96,7 +434,7 @@ describe('isCreationAllowed', () => {
   let hasConflicts;
   beforeEach(() => {
     UserModel = sinon.mock(User);
-    checkContracts = sinon.stub(EventHelper, 'checkContracts');
+    checkContracts = sinon.stub(EventsValidationHelper, 'checkContracts');
     hasConflicts = sinon.stub(EventsValidationHelper, 'hasConflicts');
   });
   afterEach(() => {
@@ -266,7 +604,7 @@ describe('isEditionAllowed', () => {
   let hasConflicts;
   beforeEach(() => {
     UserModel = sinon.mock(User);
-    checkContracts = sinon.stub(EventHelper, 'checkContracts');
+    checkContracts = sinon.stub(EventsValidationHelper, 'checkContracts');
     hasConflicts = sinon.stub(EventsValidationHelper, 'hasConflicts');
   });
   afterEach(() => {
