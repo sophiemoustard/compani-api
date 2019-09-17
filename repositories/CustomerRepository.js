d6c4857865d2e559a1f60a99077ecd29ea3a6063
Camille Alemany <camillealemany23@gmail.com>
2019-09-17 12:42:29 +0200

    COM-597 Add repo method to retrieve a customer's auxiliaries

--- repositories/CustomerRepository.js
+++ repositories/CustomerRepository.js
@@ -33,3 +33,90 @@ exports.getCustomerFundings = async () => Customer.aggregate([
     $project: { funding: '$fundings', identity: 1 },
   },
 ]);
+
+exports.getCustomerAuxiliaries = async (customerId) => {
+  const now = new Date();
+
+  const aggregateHourlySubscriptions = [
+    { $match: { _id: customerId } },
+    { $unwind: { path: '$subscriptions' } },
+    {
+      $lookup: {
+        from: 'services',
+        localField: 'subscriptions.service',
+        foreignField: '_id',
+        as: 'service',
+      },
+    },
+    { $unwind: { path: '$service' } },
+    { $match: { 'service.nature': 'hourly' } },
+    { $project: { subscriptionId: '$subscriptions._id' } },
+  ];
+
+  const aggregateEventsFromSubscriptions = [
+    {
+      $lookup: {
+        from: 'events',
+        as: 'events',
+        let: { subscriptionId: '$subscriptionId', customerId: '$_id' },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $and: [
+                  { $eq: ['$customer', '$$customerId'] },
+                  { $eq: ['$subscription', '$$subscriptionId'] },
+                  { $eq: ['$type', 'intervention'] },
+                  { $lte: ['$startDate', now] },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    },
+    { $unwind: { path: '$events' } },
+    { $replaceRoot: { newRoot: '$events' } },
+    { $sort: { startDate: -1 } },
+  ];
+
+  const aggregateAuxiliariesFromEvents = [
+    {
+      $project: {
+        duration: {
+          $subtract: ['$endDate', '$startDate'],
+        },
+        startDate: 1,
+        auxiliary: 1,
+      },
+    },
+    {
+      $group: {
+        _id: '$auxiliary',
+        events: { $push: '$$ROOT' },
+      },
+    },
+    {
+      $project: {
+        totalDuration: { $sum: '$events.duration' },
+        lastEvent: { $arrayElemAt: ['$events', 0] },
+      },
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: '_id',
+        foreignField: '_id',
+        as: 'auxiliary',
+      },
+    },
+    { $unwind: { path: '$auxiliary' } },
+    { $sort: { 'lastEvent.startDate': -1 } },
+  ];
+
+  return Customer.aggregate([
+    ...aggregateHourlySubscriptions,
+    ...aggregateEventsFromSubscriptions,
+    ...aggregateAuxiliariesFromEvents,
+  ]);
+};
