5fa8df48b3501a21109627c946ea312e0e5a1a45
Sophie Moustard <sophie.moustard@gmail.com>
2019-09-19 11:04:51 +0200

    Com 583 signature (#416)
    * COM-583 - Fix contract update
    
    * COM-583 - update signature
    
    * COM-583 - refacto upload file
    
    * COM-583 - update test
    
    * COM-583 - remove online signature
    
    * COM-583  -Add missing await
    
    * COM-583 - rename
    
    * COM-583 - Add meta validation
    
    * COM-583 - Previous version index

--- helpers/contracts.js
+++ helpers/contracts.js
@@ -8,6 +8,7 @@ const Contract = require('../models/Contract');
 const User = require('../models/User');
 const Drive = require('../models/Google/Drive');
 const ESign = require('../models/ESign');
+const Customer = require('../models/Customer');
 const EventHelper = require('./events');
 const { addFile } = require('./gdriveStorage');
 const { CUSTOMER_CONTRACT, COMPANY_CONTRACT } = require('./constants');
@@ -50,7 +51,8 @@ exports.createVersion = async (contractId, newVersion) => {
 
     newVersion.signature = { eversignId: doc.data.document_hash };
   }
-  const contract = await Contract.findOneAndUpdate(
+
+  let contract = await Contract.findOneAndUpdate(
     { _id: contractId },
     { $push: { versions: newVersion } },
     { new: true, autopopulate: false }
@@ -58,25 +60,37 @@ exports.createVersion = async (contractId, newVersion) => {
   if (!contract) return null;
 
   if (contract.versions.length > 1) {
-    await exports.updatePreviousVersion(contract, contract.versions.length - 1, newVersion.startDate);
+    const previousVersionIndex = contract.versions.length - 2;
+    contract = await exports.updatePreviousVersion(contract, previousVersionIndex, newVersion.startDate);
   }
 
   return contract;
 };
 
-exports.updatePreviousVersion = async (contract, versionIndex, versionStartDate) => {
+exports.updatePreviousVersion = async (contract, previousVersionIndex, versionStartDate) => {
   const previousVersionStartDate = moment(versionStartDate).subtract(1, 'd').endOf('d').toISOString();
-  await Contract.findOneAndUpdate(
+  return Contract.findOneAndUpdate(
     { _id: contract._id },
-    { $set: { [`versions.${versionIndex - 1}.endDate`]: previousVersionStartDate } }
+    { $set: { [`versions.${previousVersionIndex}.endDate`]: previousVersionStartDate } }
   );
 };
 
 exports.updateVersion = async (contractId, versionId, versionToUpdate) => {
-  const payload = { 'versions.$[version]': { ...versionToUpdate } };
+  let unset;
+  if (versionToUpdate.signature) {
+    const doc = await ESignHelper.generateSignatureRequest(versionToUpdate.signature);
+    if (doc.data.error) throw Boom.badRequest(`Eversign: ${doc.data.error.type}`);
+
+    versionToUpdate.signature = { eversignId: doc.data.document_hash };
+  } else {
+    unset = { 'versions.$[version].signature': '' };
+  }
+
+  const payload = { $set: flat({ 'versions.$[version]': { ...versionToUpdate } }) };
+  if (unset) payload.$unset = unset;
   const contract = await Contract.findOneAndUpdate(
     { _id: contractId },
-    { $set: flat(payload) },
+    { ...payload },
     {
       // Conversion to objectIds is mandatory as we use directly mongo arrayFilters
       arrayFilters: [{ 'version._id': mongoose.Types.ObjectId(versionId) }],
@@ -89,93 +103,84 @@ exports.updateVersion = async (contractId, versionId, versionToUpdate) => {
     if (index === 0) {
       await Contract.updateOne({ _id: contractId }, { startDate: versionToUpdate.startDate });
     } else {
-      await exports.updatePreviousVersion(contract, index, versionToUpdate.startDate);
+      const previousVersionIndex = index - 1;
+      await exports.updatePreviousVersion(contract, previousVersionIndex, versionToUpdate.startDate);
     }
   }
 
   return contract;
 };
 
-exports.createAndSaveFile = async (administrativeKeys, params, payload) => {
-  const uploadedFile = await addFile({
-    driveFolderId: params.driveId,
-    name: payload.fileName || payload[administrativeKeys[0]].hapi.filename,
-    type: payload['Content-Type'],
-    body: payload[administrativeKeys[0]],
-  });
-  const driveFileInfo = await Drive.getFileById({ fileId: uploadedFile.id });
-
-  let file = {};
-  if (payload.type) {
-    if (payload.type === COMPANY_CONTRACT) {
-      file = { 'versions.$[version]': { auxiliaryDoc: { driveId: uploadedFile.id, link: driveFileInfo.webViewLink } } };
-    }
-    if (payload.type === CUSTOMER_CONTRACT) {
-      file = {
-        'versions.$[version]': {
-          customerDoc: { driveId: uploadedFile.id, link: driveFileInfo.webViewLink },
-          auxiliaryDoc: { driveId: uploadedFile.id, link: driveFileInfo.webViewLink },
-        },
-      };
-    }
-  } else {
-    file = { 'versions.$[version]': { driveId: uploadedFile.id, link: driveFileInfo.webViewLink } };
+exports.createAndSaveFile = async (version, fileInfo) => {
+  if (version.status === CUSTOMER_CONTRACT) {
+    const customer = await Customer.findOne({ _id: version.customer }).lean();
+    fileInfo.customerDriveId = customer.driveFolder.driveId;
   }
-  await Contract.findOneAndUpdate(
-    { _id: params._id },
-    { $set: flat(file) },
+  const payload = await exports.uploadFile(fileInfo, version.status);
+
+  return Contract.findOneAndUpdate(
+    { _id: version.contractId },
+    { $set: { 'versions.$[version]': payload } },
     {
       new: true,
-      arrayFilters: [{ 'version._id': mongoose.Types.ObjectId(payload.versionId) }],
+      arrayFilters: [{ 'version._id': mongoose.Types.ObjectId(version._id) }],
       autopopulate: false,
     }
   );
+};
 
-  return uploadedFile;
+exports.uploadFile = async (fileInfo, status) => {
+  if (status === COMPANY_CONTRACT) {
+    const uploadedFile = await addFile({ ...fileInfo, driveFolderId: fileInfo.auxiliaryDriveId });
+    const driveFileInfo = await Drive.getFileById({ fileId: uploadedFile.id });
+
+    return { auxiliaryDoc: { driveId: uploadedFile.id, link: driveFileInfo.webViewLink } };
+  }
+
+  const addFilePromises = [
+    addFile({ ...fileInfo, driveFolderId: fileInfo.auxiliaryDriveId }),
+    addFile({ ...fileInfo, driveFolderId: fileInfo.customerDriveId }),
+  ];
+  const [auxiliaryFileUploaded, customerFileUploaded] = await Promise.all(addFilePromises);
+
+  const fileInfoPromises = [
+    Drive.getFileById({ fileId: auxiliaryFileUploaded.id }),
+    Drive.getFileById({ fileId: customerFileUploaded.id }),
+  ];
+  const [auxiliaryDriveFile, customerDriveFile] = await Promise.all(fileInfoPromises);
+
+  return {
+    auxiliaryDoc: { driveId: auxiliaryFileUploaded.id, link: auxiliaryDriveFile.webViewLink },
+    customerDoc: { driveId: customerFileUploaded.id, link: customerDriveFile.webViewLink },
+  };
 };
 
 exports.saveCompletedContract = async (everSignDoc) => {
   const finalPDF = await ESign.downloadFinalDocument(everSignDoc.data.document_hash);
   const tmpPath = path.join(os.tmpdir(), `signedDoc-${moment().format('DDMMYYYY-HHmm')}.pdf`);
   const file = await createAndReadFile(finalPDF.data, tmpPath);
+
   let payload = {};
   if (everSignDoc.data.meta.type === CUSTOMER_CONTRACT) {
-    const addFilePromises = [
-      addFile({
-        driveFolderId: everSignDoc.data.meta.auxiliaryDriveId,
-        name: everSignDoc.data.title,
-        type: 'application/pdf',
-        body: file,
-      }),
-      addFile({
-        driveFolderId: everSignDoc.data.meta.customerDriveId,
-        name: everSignDoc.data.title,
-        type: 'application/pdf',
-        body: file,
-      }),
-    ];
-    const [auxiliaryDoc, customerDoc] = await Promise.all(addFilePromises);
-    const fileInfoPromises = [Drive.getFileById({ fileId: auxiliaryDoc.id }), Drive.getFileById({ fileId: customerDoc.id })];
-    const [auxiliaryDocInfo, customerDocInfo] = await Promise.all(fileInfoPromises);
-    payload = {
-      'versions.$': {
-        auxiliaryDoc: { driveId: auxiliaryDoc.id, link: auxiliaryDocInfo.webViewLink },
-        customerDoc: { driveId: customerDoc.id, link: customerDocInfo.webViewLink },
-      },
-    };
-  } else {
-    const auxiliaryDoc = await addFile({
-      driveFolderId: everSignDoc.data.meta.auxiliaryDriveId,
+    payload = await exports.uploadFile({
+      auxiliaryDriveId: everSignDoc.data.meta.auxiliaryDriveId,
+      customerDriveId: everSignDoc.data.meta.customerDriveId,
       name: everSignDoc.data.title,
       type: 'application/pdf',
       body: file,
-    });
-    const auxiliaryDocInfo = await Drive.getFileById({ fileId: auxiliaryDoc.id });
-    payload = {
-      'versions.$': {
-        auxiliaryDoc: { driveId: auxiliaryDoc.id, link: auxiliaryDocInfo.webViewLink },
-      },
-    };
+    }, CUSTOMER_CONTRACT);
+  } else {
+    payload = await exports.uploadFile({
+      auxiliaryDriveId: everSignDoc.data.meta.auxiliaryDriveId,
+      name: everSignDoc.data.title,
+      type: 'application/pdf',
+      body: file,
+    }, COMPANY_CONTRACT);
   }
-  await Contract.findOneAndUpdate({ 'versions.signature.eversignId': everSignDoc.data.document_hash }, { $set: flat(payload) }, { new: true });
+
+  await Contract.findOneAndUpdate(
+    { 'versions.signature.eversignId': everSignDoc.data.document_hash },
+    { $set: flat({ 'versions.$': payload }) },
+    { new: true }
+  );
 };
