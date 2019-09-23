be67e64b871e30a4e7a01b0d67295a1dc0592498
Sophie Moustard <sophie.moustard@gmail.com>
2019-09-23 17:18:34 +0200

    Fix right (#432)

--- models/Role.js
+++ models/Role.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const autopopulate = require('mongoose-autopopulate');
+const Right = require('./Right'); // required to help mongoose detect the Right model
 
 const RoleSchema = mongoose.Schema({
   name: { type: String, unique: true, required: true },
   rights: [{
     right_id: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Right',
-      autopopulate: { select: 'name description permission _id' },
+      ref: Right,
+      autopopulate: { select: 'description permission _id' },
     },
     hasAccess: { type: Boolean, default: false },
   }],
