013bd8688f39574b45ed5be90843757104434e69
Sophie Moustard <sophie.moustard@gmail.com>
2019-09-24 12:05:04 +0200

    Update isUsedInFundings (#433)

--- models/ThirdPartyPayer.js
+++ models/ThirdPartyPayer.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const { BILLING_DIRECT, BILLING_INDIRECT } = require('../helpers/constants');
 const addressSchemaDefinition = require('./schemaDefinitions/address');
+const Customer = require('./Customer');
 
 const ThirdPartyPayerSchema = mongoose.Schema({
   name: String,
@@ -10,9 +11,20 @@ const ThirdPartyPayerSchema = mongoose.Schema({
   unitTTCRate: Number,
   billingMode: {
     type: String,
-    enum: [BILLING_DIRECT, BILLING_INDIRECT]
+    enum: [BILLING_DIRECT, BILLING_INDIRECT],
   },
-  company: { type: mongoose.Schema.Types.ObjectId }
+  company: { type: mongoose.Schema.Types.ObjectId },
 }, { timestamps: true });
+
+const countFundings = async (docs) => {
+  if (docs.length > 0) {
+    for (const tpp of docs) {
+      const customerCount = await Customer.countDocuments({ fundings: { $exists: true }, 'fundings.thirdPartyPayer': tpp._id });
+      tpp.isUsedInFundings = customerCount > 0;
+    }
+  }
+};
+
+ThirdPartyPayerSchema.post('find', countFundings);
 
 module.exports = mongoose.model('ThirdPartyPayer', ThirdPartyPayerSchema);
