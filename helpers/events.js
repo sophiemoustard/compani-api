745e86eccad6db4b5abf0e6dd192f95cc8fec356
jctrebalag <jctrebalag@gmail.com>
2019-09-26 10:39:24 +0200

    COM-707 Fix misc event update for unassigned events

--- helpers/events.js
+++ helpers/events.js
@@ -138,9 +138,9 @@ exports.updateEventsInternalHourType = async (oldInternalHourId, newInternalHour
 const isMiscOnlyUpdated = (event, payload) => {
   const mainEventInfo = {
     ..._.pick(event, ['isCancelled', 'startDate', 'endDate', 'status']),
-    auxiliary: event.auxiliary.toHexString(),
     sector: event.sector.toHexString(),
   };
+  if (event.auxiliary) mainEventInfo.auxiliary = event.auxiliary.toHexString();
   if (event.subscription) mainEventInfo.subscription = event.subscription.toHexString();
   const mainPayloadInfo = _.omit({ ...payload, ...(!payload.isCancelled && { isCancelled: false }) }, ['misc']);
 
