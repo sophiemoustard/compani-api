0221b1eff3ed422999a2f76f2c49cc339610377f
jctrebalag <jctrebalag@gmail.com>
2019-11-05 11:18:13 +0100

    COM-791 Welcoming email is now send on behalf of the user company

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -1,23 +1,12 @@
 const Boom = require('boom');
-const nodemailer = require('nodemailer');
 const translate = require('../helpers/translate');
-const { sendinBlueTransporter, testTransporter } = require('../helpers/nodemailer');
-const { welcomeEmailContent } = require('../helpers/emailOptions');
-const { SENDER_MAIL } = require('../helpers/constants');
+const { helperWelcomeEmail } = require('../helpers/email');
 
 const { language } = translate;
 
 const sendWelcome = async (req) => {
   try {
-    const mailOptions = {
-      from: `Compani <${SENDER_MAIL}>`,
-      to: req.payload.receiver.email,
-      subject: 'Alenvi - Bienvenue dans votre espace Compani',
-      html: welcomeEmailContent(req.payload.receiver),
-    };
-    const mailInfo = process.env.NODE_ENV !== 'test'
-      ? await sendinBlueTransporter.sendMail(mailOptions)
-      : await testTransporter(await nodemailer.createTestAccount()).sendMail(mailOptions);
+    const mailInfo = await helperWelcomeEmail(req.payload.receiver, req.auth.credentials.company);
 
     return { message: translate[language].emailSent, data: { mailInfo } };
   } catch (e) {
