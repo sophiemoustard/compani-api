12bbd38fae5f277af97b79d71295d0ba39444708
Chloe Molle <chloe.molle@neuf.fr>
2019-11-05 17:38:03 +0100

    COM-785 trying to fix tests

--- tests/integration/seed/authenticationSeed.js
+++ tests/integration/seed/authenticationSeed.js
@@ -3,15 +3,9 @@ const uuidv4 = require('uuid/v4');
 const memoize = require('lodash/memoize');
 const Role = require('../../../src/models/Role');
 const Right = require('../../../src/models/Right');
-const Company = require('../../../src/models/Company');
 const User = require('../../../src/models/User');
+const Company = require('../../../src/models/Company');
 const app = require('../../../server');
-
-const authCompany = {
-  _id: new ObjectID(),
-  name: 'Test SAS',
-  tradeName: 'Test',
-};
 
 const rightsList = [
   {
@@ -196,6 +190,12 @@ const rolesList = [
   },
 ];
 
+const authCompany = {
+  _id: new ObjectID(),
+  name: 'Test SAS',
+  tradeName: 'Test',
+};
+
 const userList = [
   {
     _id: new ObjectID(),
@@ -242,11 +242,11 @@ const userList = [
 const populateDBForAuthentication = async () => {
   await Role.deleteMany({});
   await Right.deleteMany({});
-  await Company.deleteMany({});
   await User.deleteMany({});
+  await Company.deleteMany({});
+  await new Company(authCompany).save();
   await Right.insertMany(rightsList);
   await Role.insertMany(rolesList);
-  await new Company(authCompany).save();
   for (let i = 0; i < userList.length; i++) {
     await (new User(userList[i]).save());
   }
