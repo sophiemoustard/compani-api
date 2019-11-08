61b61445eefb5fe2ffdd91f79c15e33ed97dc29a
jctrebalag <jctrebalag@gmail.com>
2019-11-08 13:54:23 +0100

    COM-829 Remove some log

--- src/models/preHooks/validate.js
+++ src/models/preHooks/validate.js
@@ -3,7 +3,6 @@ const Boom = require('boom');
 module.exports = {
   validateQuery(next) {
     const query = this.getQuery();
-    console.log('this', this.getQuery());
     if (!query.company) next(Boom.badRequest());
     next();
   },
