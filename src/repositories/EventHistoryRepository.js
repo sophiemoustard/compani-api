008a65c4b84d74444cbb2e0afc050c4b336ce9ce
jctrebalag <jctrebalag@gmail.com>
2019-11-15 16:41:37 +0100

    COM-818 Add internal hour populate on eventHistory

--- src/repositories/EventHistoryRepository.js
+++ src/repositories/EventHistoryRepository.js
@@ -1,6 +1,7 @@
+const get = require('lodash/get');
 const EventHistory = require('../models/EventHistory');
 
-exports.paginate = async (query, createdAt = null) => {
+exports.paginate = async (query, createdAt = null, credentials) => {
   if (createdAt) query.createdAt = { $lt: createdAt };
   return EventHistory
     .find(query)
@@ -8,6 +9,7 @@ exports.paginate = async (query, createdAt = null) => {
     .populate({ path: 'createdBy', select: '_id identity picture' })
     .populate({ path: 'event.customer', select: '_id identity' })
     .populate({ path: 'event.auxiliary', select: '_id identity' })
+    .populate({ path: 'event.internalHour', select: 'name', match: { company: get(credentials, 'company._id', null) } })
     .populate({ path: 'update.auxiliary.from', select: '_id identity' })
     .populate({ path: 'update.auxiliary.to', select: '_id identity' })
     .sort({ createdAt: -1 })
