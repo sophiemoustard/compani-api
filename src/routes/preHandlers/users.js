ebf883faf30edcbbf0267d0a89b9de3276e7ee58
jctrebalag <jctrebalag@gmail.com>
2019-11-26 18:59:05 +0100

    COM-802 Remove unnecessary autopopulate

--- src/routes/preHandlers/users.js
+++ src/routes/preHandlers/users.js
@@ -7,7 +7,7 @@ const { language } = translate;
 exports.getUser = async (req) => {
   try {
     const userId = req.params._id;
-    const user = await User.findById(userId).lean({ autopopulate: true });
+    const user = await User.findById(userId).lean();
     if (!user) throw Boom.notFound(translate[language].userNotFound);
 
     return user;
@@ -21,7 +21,7 @@ exports.authorizeUserUpdate = (req) => {
   const { credentials } = req.auth;
   const user = req.pre.user || req.payload;
 
-  if (user.company._id.toHexString() === credentials.company._id.toHexString()) return null;
+  if (user.company.toHexString() === credentials.company._id.toHexString()) return null;
 
   throw Boom.forbidden();
 };
