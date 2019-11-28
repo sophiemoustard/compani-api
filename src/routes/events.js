36d50ffe20de2cc19e5b8ee6b0cbaf9a0e4e1bb2
Chloe Molle <chloe.molle@neuf.fr>
2019-11-28 08:25:55 +0100

    COM-866 minor change

--- src/routes/events.js
+++ src/routes/events.js
@@ -218,7 +218,7 @@ exports.plugin = {
 
     server.route({
       method: 'PUT',
-      path: '/many-events',
+      path: '/remove-many-events',
       options: {
         auth: { scope: ['events:edit'] },
         validate: {
