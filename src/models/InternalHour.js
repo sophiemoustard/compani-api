9b95cb9b320516811393978e7868a15c23d64e80
Sophie Moustard <sophie.moustard@gmail.com>
2019-12-09 18:16:24 +0100

    COM-886 - remove unique condition

--- src/models/InternalHour.js
+++ src/models/InternalHour.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { validateQuery, validatePayload } = require('./preHooks/validate');
 
 const InternalHourSchema = mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
   default: { type: Boolean, default: false },
   company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
 }, { timestamps: true });
