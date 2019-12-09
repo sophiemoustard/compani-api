ea67ae6ee68dd6eef6cb95970c0532c358ce62a4
cmolle <chloe.molle@neuf.fr>
2019-12-09 16:35:39 +0100

    COM-888 add prehook on validate + field company for event history cre… (#575)
    * COM-888 add prehook on validate + field company for event history creation
    
    * fix tests

--- src/helpers/eventHistories.js
+++ src/helpers/eventHistories.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const get = require('lodash/get');
 const EventHistory = require('../models/EventHistory');
 const { EVENT_CREATION, EVENT_DELETION, EVENT_UPDATE, INTERNAL_HOUR, ABSENCE } = require('./constants');
 const UtilsHelper = require('./utils');
@@ -15,9 +16,22 @@ exports.getListQuery = (query) => {
 
 exports.createEventHistory = async (payload, credentials, action) => {
   const { _id: createdBy } = credentials;
-  const { customer, startDate, endDate, type, auxiliary, sector, absence, internalHour, address, misc, repetition } = payload;
+  const {
+    customer,
+    startDate,
+    endDate,
+    type,
+    auxiliary,
+    sector,
+    absence,
+    internalHour,
+    address,
+    misc,
+    repetition,
+  } = payload;
 
   const eventHistory = new EventHistory({
+    company: get(credentials, 'company._id', null),
     createdBy,
     action,
     event: {
@@ -39,16 +53,20 @@ exports.createEventHistory = async (payload, credentials, action) => {
   await eventHistory.save();
 };
 
-exports.createEventHistoryOnCreate = async (payload, credentials) => exports.createEventHistory(payload, credentials, EVENT_CREATION);
+exports.createEventHistoryOnCreate = async (payload, credentials) =>
+  exports.createEventHistory(payload, credentials, EVENT_CREATION);
 
-exports.createEventHistoryOnDelete = async (payload, credentials) => exports.createEventHistory(payload, credentials, EVENT_DELETION);
+exports.createEventHistoryOnDelete = async (payload, credentials) =>
+  exports.createEventHistory(payload, credentials, EVENT_DELETION);
 
 exports.createEventHistoryOnUpdate = async (payload, event, credentials) => {
   const promises = [];
   const { _id: createdBy } = credentials;
   const { customer, type, repetition } = event;
   const { startDate, endDate, misc } = payload;
+
   const eventHistory = {
+    company: get(credentials, 'company._id', null),
     createdBy,
     action: EVENT_UPDATE,
     event: {
@@ -63,14 +81,16 @@ exports.createEventHistoryOnUpdate = async (payload, event, credentials) => {
   if (event.type === INTERNAL_HOUR) eventHistory.event.internalHour = payload.internalHour || event.internalHour;
   if (event.type === ABSENCE) eventHistory.event.absence = payload.absence || event.absence;
 
-  if ((event.auxiliary && event.auxiliary.toHexString() !== payload.auxiliary) || (!event.auxiliary && payload.auxiliary)) {
+  if ((event.auxiliary && event.auxiliary.toHexString() !== payload.auxiliary)
+    || (!event.auxiliary && payload.auxiliary)) {
     const auxiliaryUpdateHistory = exports.formatEventHistoryForAuxiliaryUpdate(eventHistory, payload, event);
-    promises.push((new EventHistory(auxiliaryUpdateHistory)).save());
+    promises.push(new EventHistory(auxiliaryUpdateHistory).save());
   }
 
-  if (!moment(event.startDate).isSame(payload.startDate, 'day') || !moment(event.endDate).isSame(payload.endDate, 'day')) {
+  if (!moment(event.startDate).isSame(payload.startDate, 'day')
+  || !moment(event.endDate).isSame(payload.endDate, 'day')) {
     const datesUpdateHistory = exports.formatEventHistoryForDatesUpdate(eventHistory, payload, event);
-    promises.push((new EventHistory(datesUpdateHistory)).save());
+    promises.push(new EventHistory(datesUpdateHistory).save());
   } else {
     const eventStartHour = moment(event.startDate).format('HH:mm');
     const eventEndHour = moment(event.endDate).format('HH:mm');
@@ -79,13 +99,13 @@ exports.createEventHistoryOnUpdate = async (payload, event, credentials) => {
 
     if (eventStartHour !== payloadStartHour || eventEndHour !== payloadEndHour) {
       const hoursUpdateHistory = exports.formatEventHistoryForHoursUpdate(eventHistory, payload, event);
-      promises.push((new EventHistory(hoursUpdateHistory)).save());
+      promises.push(new EventHistory(hoursUpdateHistory).save());
     }
   }
 
   if (payload.isCancelled && !event.isCancelled) {
     const cancelUpdateHistory = exports.formatEventHistoryForCancelUpdate(eventHistory, payload, event);
-    promises.push((new EventHistory(cancelUpdateHistory)).save());
+    promises.push(new EventHistory(cancelUpdateHistory).save());
   }
 
   await Promise.all(promises);
@@ -110,7 +130,8 @@ exports.formatEventHistoryForAuxiliaryUpdate = (mainInfo, payload, event) => {
     };
   }
 
-  if (!payload.sector || event.sector.toHexString() === payload.sector) auxiliaryUpdateHistory.sectors = [event.sector.toHexString()];
+  if (!payload.sector
+    || event.sector.toHexString() === payload.sector)auxiliaryUpdateHistory.sectors = [event.sector.toHexString()];
   else auxiliaryUpdateHistory.sectors = [event.sector.toHexString(), payload.sector];
 
   return auxiliaryUpdateHistory;
@@ -125,7 +146,8 @@ exports.formatEventHistoryForDatesUpdate = (mainInfo, payload, event) => {
     },
   };
 
-  const isOneDayEvent = moment(event.endDate).isSame(event.startDate, 'day') && moment(payload.endDate).isSame(payload.startDate, 'day');
+  const isOneDayEvent = moment(event.endDate).isSame(event.startDate, 'day')
+    && moment(payload.endDate).isSame(payload.startDate, 'day');
   if (!isOneDayEvent) datesUpdateHistory.update.endDate = { from: event.endDate, to: payload.endDate };
 
   if (payload.auxiliary) {
