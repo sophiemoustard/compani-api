38928d15c6c6c112c3c80c3dbde4ae5f981e324e
jctrebalag <jctrebalag@gmail.com>
2019-12-11 10:29:26 +0100

    COM-809 Fix tests

--- src/helpers/companies.js
+++ src/helpers/companies.js
@@ -6,7 +6,7 @@ exports.createCompany = async (companyPayload) => {
   const payload = cloneDeep(companyPayload);
 
   const companyFolder = await GdriveStorageHelper.createFolderForCompany(payload.name);
-  const directDebitsFolder = await GdriveStorageHelper.createFolder('direct debits', payload.folderId);
+  const directDebitsFolder = await GdriveStorageHelper.createFolder('direct debits', companyFolder.id);
 
   return Company.create({ ...payload, directDebitsFolderId: directDebitsFolder.id, folderId: companyFolder.id });
 };
