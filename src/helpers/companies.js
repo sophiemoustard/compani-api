a14d46ab52ac697429311882c67c1ab35a4aaa3e
jctrebalag <jctrebalag@gmail.com>
2019-12-11 11:36:27 +0100

    COM-809 Fixes after PR comments

--- src/helpers/companies.js
+++ src/helpers/companies.js
@@ -1,12 +1,9 @@
-const cloneDeep = require('lodash/cloneDeep');
 const Company = require('../models/Company');
 const GdriveStorageHelper = require('./gdriveStorage');
 
 exports.createCompany = async (companyPayload) => {
-  const payload = cloneDeep(companyPayload);
-
-  const companyFolder = await GdriveStorageHelper.createFolderForCompany(payload.name);
+  const companyFolder = await GdriveStorageHelper.createFolderForCompany(companyPayload.name);
   const directDebitsFolder = await GdriveStorageHelper.createFolder('direct debits', companyFolder.id);
 
-  return Company.create({ ...payload, directDebitsFolderId: directDebitsFolder.id, folderId: companyFolder.id });
+  return Company.create({ ...companyPayload, directDebitsFolderId: directDebitsFolder.id, folderId: companyFolder.id });
 };
