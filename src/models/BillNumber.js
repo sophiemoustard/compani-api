7e3e1b47a83eaa349bfcca0c5c697efbdf51a870
Sophie Moustard <sophie.moustard@gmail.com>
2019-12-26 16:22:53 +0100

    COM-884 - update billNumber model

--- src/models/BillNumber.js
+++ src/models/BillNumber.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
+const { validatePayload, validateQuery, validateAggregation } = require('./preHooks/validate');
 
 const BillNumberSchema = mongoose.Schema({
-  prefix: String,
-  seq: { type: Number, default: 1 },
-});
+  prefix: { type: String, required: true },
+  seq: { type: Number, default: 1, required: true },
+  company: { type: mongoose.Types.ObjectId, required: true },
+}, { timestamps: true });
+
+BillNumberSchema.pre('validate', validatePayload);
+BillNumberSchema.pre('find', validateQuery);
+BillNumberSchema.pre('aggregate', validateAggregation);
 
 module.exports = mongoose.model('BillNumber', BillNumberSchema);
