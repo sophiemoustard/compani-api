e41e07e7a93c979ee75a190066ac3fd97f13bffe
Sophie Moustard <sophie.moustard@gmail.com>
2020-01-16 10:31:04 +0100

    COM-958 - unit tests

--- src/helpers/taxCertificates.js
+++ src/helpers/taxCertificates.js
@@ -4,7 +4,6 @@ const pick = require('lodash/pick');
 const PdfHelper = require('./pdf');
 const UtilsHelper = require('./utils');
 const TaxCertificate = require('../models/TaxCertificate');
-const Company = require('../models/Company');
 
 exports.generateTaxCertificatesList = async (customer, credentials) => {
   const companyId = get(credentials, 'company._id', null);
@@ -12,28 +11,29 @@ exports.generateTaxCertificatesList = async (customer, credentials) => {
   return TaxCertificate.find({ customer, company: companyId }).lean();
 };
 
+exports.formatPdf = (taxCertificate, company) => ({
+  taxCertificate: {
+    company: pick(company, ['logo', 'name', 'address']),
+    year: taxCertificate.year,
+    date: moment(taxCertificate.year, 'YYYY')
+      .add(1, 'y')
+      .startOf('y')
+      .endOf('month')
+      .format('DD/MM/YYYY'),
+    director: 'Clément de Saint Olive',
+    customer: {
+      name: UtilsHelper.formatIdentity(taxCertificate.customer.identity, 'TFL'),
+      address: get(taxCertificate, 'customer.contact.primaryAddress', {}),
+    },
+  },
+});
+
 exports.generateTaxCertificatePdf = async (taxCertificateId, credentials) => {
-  const company = await Company.findOne({ _id: get(credentials, 'company._id', null) }).lean();
   const taxCertificate = await TaxCertificate.findOne({ _id: taxCertificateId })
     .populate({ path: 'customer', select: 'identity contact' })
     .lean();
 
-  const data = {
-    taxCertificate: {
-      company: pick(company, ['logo', 'name', 'address']),
-      year: taxCertificate.year,
-      date: moment(taxCertificate.year, 'YYYY')
-        .add(1, 'y')
-        .startOf('y')
-        .endOf('month')
-        .format('DD/MM/YYYY'),
-      director: 'Clément de Saint Olive',
-      customer: {
-        name: UtilsHelper.formatIdentity(taxCertificate.customer.identity, 'TFL'),
-        address: get(taxCertificate, 'customer.contact.primaryAddress', {}),
-      },
-    },
-  };
+  const data = exports.formatPdf(taxCertificate, credentials.company);
   const pdf = await PdfHelper.generatePdf(data, './src/data/taxCertificates.html');
 
   return pdf;
