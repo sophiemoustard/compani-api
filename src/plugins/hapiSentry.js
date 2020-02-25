cda8d30ca2ec7f810c9ef65f6ea2f42d5854bf22
jctrebalag <jctrebalag@gmail.com>
2020-02-25 09:55:58 +0100

    Update hapi and sentry WIP

--- src/plugins/hapiSentry.js
+++ src/plugins/hapiSentry.js
@@ -3,7 +3,7 @@ const pick = require('lodash/pick');
 const beforeSendHandler = (event) => {
   if (event.user) {
     if (event.user.company) {
-      event.user.company = { _id: event.user.company._id.toHexString(), name: event.user.company.name };
+      event.user.company = { _id: event.user.company._id, name: event.user.company.name };
     }
     if (event.user.identity) event.user.identity = pick(event.user.identity, ['firstname', 'lastname']);
     if (event.user.scope) delete event.user.scope;
