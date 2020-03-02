5034206b63ad98643eb6ba88225e2b9754532413
jctrebalag <jctrebalag@gmail.com>
2020-03-02 16:10:48 +0100

    COM-1040 Add company and identity to sentry event user payload

--- src/plugins/hapiSentry.js
+++ src/plugins/hapiSentry.js
@@ -1,9 +1,13 @@
+const pick = require('lodash/pick');
+
 const beforeSendHandler = (event) => {
   const { user } = event;
   const payload = {};
   if (user) {
     payload.id = user._id;
     payload.email = user.email;
+    if (user.company) payload.user.company = { _id: user.company._id.toHexString(), name: user.company.name };
+    if (event.user.identity) payload.user.identity = pick(user.identity, ['firstname', 'lastname']);
     event.user = payload;
   }
   return event;
