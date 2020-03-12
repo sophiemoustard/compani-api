efbeb2fab1504c482420d13fc8eea1c0bff8cd6b
Sophie Moustard <sophie.moustard@gmail.com>
2020-03-12 10:59:24 +0100

    COM-1049 - fix auxiliaries export

--- src/helpers/dataExport.js
+++ src/helpers/dataExport.js
@@ -12,6 +12,7 @@ const {
   FUNDING_NATURES,
   CONTRACT_STATUS_LIST,
   SERVICE_NATURES,
+  AUXILIARY_WITHOUT_COMPANY,
 } = require('./constants');
 const UtilsHelper = require('./utils');
 const Customer = require('../models/Customer');
@@ -164,7 +165,7 @@ const getDataForAuxiliariesExport = (aux, contractsLength, contract) => {
 
 exports.exportAuxiliaries = async (credentials) => {
   const companyId = get(credentials, 'company._id', null);
-  const roles = await Role.find({ name: { $in: [AUXILIARY, PLANNING_REFERENT] } }).lean();
+  const roles = await Role.find({ name: { $in: [AUXILIARY, PLANNING_REFERENT, AUXILIARY_WITHOUT_COMPANY] } }).lean();
   const roleIds = roles.map(role => role._id);
   const auxiliaries = await User
     .find({ 'role.client': { $in: roleIds }, company: companyId })
