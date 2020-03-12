65f0482b6f7500d78e4b3c4b835c3f7357c999b6
Sophie Moustard <sophie.moustard@gmail.com>
2020-03-12 08:51:16 +0100

    COM-1069 - add date query in repetition script

--- src/jobs/eventRepetitions.js
+++ src/jobs/eventRepetitions.js
@@ -1,5 +1,6 @@
 const moment = require('moment');
 const pick = require('lodash/pick');
+const get = require('lodash/get');
 const Repetition = require('../models/Repetition');
 const Company = require('../models/Company');
 const Event = require('../models/Event');
@@ -9,17 +10,18 @@ const EmailHelper = require('../helpers/email');
 
 const eventRepetitions = {
   async method(server) {
+    const date = get(server, 'query.date') || new Date();
     const errors = [];
     const newEvents = [];
     const companies = await Company.find({}).lean();
     for (const company of companies) {
       const repetitions = await Repetition
-        .find({ company: company._id, startDate: { $lt: moment(new Date()).startOf('d').toDate() } }).lean();
+        .find({ company: company._id, startDate: { $lt: moment(date).startOf('d').toDate() } }).lean();
       if (!repetitions.length) return server.log(['cron', 'jobs'], 'Event repetitions: No repetitions found.');
 
       for (const repetition of repetitions) {
         const { startDate, frequency } = repetition;
-        const newEventStartDate = moment(new Date()).add(90, 'd')
+        const newEventStartDate = moment(date).add(90, 'd')
           .set(pick(moment(startDate).toObject(), ['hours', 'minutes', 'seconds', 'milliseconds']));
         let futureEvent;
         try {
@@ -27,23 +29,23 @@ const eventRepetitions = {
             case EVERY_TWO_WEEKS:
               if (moment(startDate).day() === moment(newEventStartDate).day()
                 && (newEventStartDate.diff(moment(startDate), 'week') % 2 === 0)) {
-                futureEvent = await EventsRepetitionHelper.createFutureEventBasedOnRepetition(repetition);
+                futureEvent = await EventsRepetitionHelper.createFutureEventBasedOnRepetition(repetition, date);
                 newEvents.push(futureEvent);
               }
               break;
             case EVERY_WEEK:
               if (moment(startDate).day() === newEventStartDate.day()) {
-                futureEvent = await EventsRepetitionHelper.createFutureEventBasedOnRepetition(repetition);
+                futureEvent = await EventsRepetitionHelper.createFutureEventBasedOnRepetition(repetition, date);
                 newEvents.push(futureEvent);
               }
               break;
             case EVERY_DAY:
-              futureEvent = await EventsRepetitionHelper.createFutureEventBasedOnRepetition(repetition);
+              futureEvent = await EventsRepetitionHelper.createFutureEventBasedOnRepetition(repetition, date);
               newEvents.push(futureEvent);
               break;
             case EVERY_WEEK_DAY:
               if (newEventStartDate.day() !== 0 && newEventStartDate.day() !== 6) {
-                futureEvent = await EventsRepetitionHelper.createFutureEventBasedOnRepetition(repetition);
+                futureEvent = await EventsRepetitionHelper.createFutureEventBasedOnRepetition(repetition, date);
                 newEvents.push(futureEvent);
               }
               break;
