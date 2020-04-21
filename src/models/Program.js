c2fa42ac4ed511ecc8e4cf1581643771f0b3e763
Corentin <corentin.martinot@gmail.com>
2020-04-21 10:18:58 +0200

    add 'learningGoal' to objects 'programs'

--- src/models/Program.js
+++ src/models/Program.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const ProgramSchema = mongoose.Schema({
   name: { type: String, required: true },
+  learningGoals: { type: String },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Program', ProgramSchema);
