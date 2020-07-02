6fda9ddadada8d0b44f4b6e46f9646a099f5405f
Chloe Molle <chloe.molle@neuf.fr>
2020-07-02 15:25:08 +0200

    COM-1304 add modules

--- src/helpers/programs.js
+++ src/helpers/programs.js
@@ -4,7 +4,7 @@ exports.createProgram = payload => (new Program(payload)).save();
 
 exports.list = async query => Program.find(query).lean();
 
-exports.getProgram = async programId => Program.findOne({ _id: programId }).lean();
+exports.getProgram = async programId => Program.findOne({ _id: programId }).populate('modules').lean();
 
 exports.updateProgram = async (programId, payload) =>
   Program.findOneAndUpdate({ _id: programId }, { $set: payload }, { new: true }).lean();
