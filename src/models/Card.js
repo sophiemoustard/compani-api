85235e456ec41ff70dc548ded2420404e6a287fe
Corentin Martinot <corentin.martinot@gmail.com>
2020-07-27 18:06:30 +0200

    COM-1372 add title-text card

--- src/models/Card.js
+++ src/models/Card.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const { TRANSITION, TITLE_TEXT_MEDIA } = require('../helpers/constants');
+const { TRANSITION, TITLE_TEXT_MEDIA, TITLE_TEXT } = require('../helpers/constants');
 
-const CARD_TEMPLATES = [TRANSITION, TITLE_TEXT_MEDIA];
+const CARD_TEMPLATES = [TRANSITION, TITLE_TEXT_MEDIA, TITLE_TEXT];
 
 const CardSchema = mongoose.Schema({
   template: { type: String, enum: CARD_TEMPLATES, immutable: true, required: true },
