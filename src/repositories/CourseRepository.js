9b7dc9eee6ee230262e9ab19ff8fc346353b8b2e
Chloe Molle <chloe.molle@neuf.fr>
2020-07-30 13:41:07 +0200

    COM-1361 get steps

--- src/repositories/CourseRepository.js
+++ src/repositories/CourseRepository.js
@@ -2,8 +2,8 @@ const Course = require('../models/Course');
 
 exports.findCourseAndPopulate = (query, populateVirtual = false) => Course.find(query)
   .populate({ path: 'company', select: 'name' })
-  .populate({ path: 'program', select: 'name image' })
-  .populate({ path: 'slots', select: 'startDate endDate' })
+  .populate({ path: 'program', select: 'name image steps' })
+  .populate({ path: 'slots', select: 'startDate endDate step', populate: { path: 'step', select: 'name' } })
   .populate({ path: 'slotsToPlan', select: '_id' })
   .populate({ path: 'trainer', select: 'identity.firstname identity.lastname' })
   .populate({ path: 'trainees', select: 'company', populate: { path: 'company', select: 'name' } })
