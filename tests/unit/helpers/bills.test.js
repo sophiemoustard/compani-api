8b88b2ceb4809697901791b9bfb1cc57705cd9aa
Chloe Molle <chloe.molle@neuf.fr>
2020-08-26 16:44:17 +0200

    COM-1452 display nbr of events on bill if service is fixed

--- tests/unit/helpers/bills.test.js
+++ tests/unit/helpers/bills.test.js
@@ -1732,16 +1732,52 @@ describe('formatBillSubscriptionsForPdf', () => {
         hours: 40,
         exclTaxes: 1018.009,
         inclTaxes: 1074,
-        service: { name: 'Temps de qualité - autonomie' },
+        service: { name: 'Temps de qualité - autonomie', nature: 'hourly' },
       }],
     };
 
     const result = {
       formattedSubs: [{
         vat: '5,5',
-        hours: 40,
+        hours: '40,00 h',
         inclTaxes: '1 074,00 €',
         service: 'Temps de qualité - autonomie',
+        unitInclTaxes: '24,64 €',
+      }],
+      totalExclTaxes: '1 018,01 €',
+      totalVAT: '55,99 €',
+    };
+
+    const formattedBillSubscriptions = BillHelper.formatBillSubscriptionsForPdf(bill);
+
+    expect(formattedBillSubscriptions).toEqual(result);
+  });
+
+  it('should return formatted subscriptions for fixed service', () => {
+    getUnitInclTaxes.returns('24.63');
+    formatPrice.onCall(0).returns('24,64 €');
+    formatPrice.onCall(1).returns('1 074,00 €');
+    formatPrice.onCall(2).returns('1 018,01 €');
+    formatPrice.onCall(3).returns('55,99 €');
+
+    const bill = {
+      subscriptions: [{
+        events: [{ id: '1234' }],
+        unitInclTaxes: 24.644549763033176,
+        vat: 5.5,
+        hours: 40,
+        exclTaxes: 1018.009,
+        inclTaxes: 1074,
+        service: { name: 'forfait nuit', nature: 'fixed' },
+      }],
+    };
+
+    const result = {
+      formattedSubs: [{
+        vat: '5,5',
+        hours: 1,
+        inclTaxes: '1 074,00 €',
+        service: 'forfait nuit',
         unitInclTaxes: '24,64 €',
       }],
       totalExclTaxes: '1 018,01 €',
