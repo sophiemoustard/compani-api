5bfcc3f2a648a821a93addb426a4b2e591944afb
Sophie Moustard <sophie.moustard@gmail.com>
2020-08-26 18:03:25 +0200

    COM-1451 - remove steps from program

--- tests/integration/programs.test.js
+++ tests/integration/programs.test.js
@@ -133,7 +133,6 @@ describe('PROGRAMS ROUTES - GET /programs/{_id}', () => {
       expect(response.result.data.program).toMatchObject({
         _id: programId,
         name: 'program',
-        steps: expect.arrayContaining([expect.any(ObjectID)]),
         subPrograms: [
           {
             name: 'c\'est un sous-programme',
