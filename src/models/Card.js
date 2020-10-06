2406839778916d1e6de66f5afac0e0b36972cec1
KennyCallegari <mel.kenny@gmail.com>
2020-10-06 17:50:35 +0200

    COM-1555 - Fix Sophie's review

--- src/models/Card.js
+++ src/models/Card.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const mongooseLeanVirtuals = require('mongoose-lean-virtuals');
-
 const {
   TRANSITION,
   TITLE_TEXT_MEDIA,
@@ -14,16 +13,8 @@ const {
   OPEN_QUESTION,
   SURVEY,
   QUESTION_ANSWER,
-  SINGLE_CHOICE_QUESTION_MAX_FALSY_ANSWERS_COUNT,
-  FILL_THE_GAPS_MAX_ANSWERS_COUNT,
-  MULTIPLE_CHOICE_QUESTION_MAX_ANSWERS_COUNT,
-  ORDER_THE_SEQUENCE_MAX_ANSWERS_COUNT,
-  QUESTION_ANSWER_MAX_ANSWERS_COUNT,
-  SURVEY_LABEL_MAX_LENGTH,
-  QC_ANSWER_MAX_LENGTH,
-  QUESTION_MAX_LENGTH,
-  GAP_ANSWER_MAX_LENGTH,
 } = require('../helpers/constants');
+const { cardValidationByTemplate } = require('./validations/cardValidation');
 
 const CARD_TEMPLATES = [
   TRANSITION,
@@ -113,76 +104,9 @@ function save(next) {
 }
 
 function setIsValid() {
-  switch (this.template) {
-    case TRANSITION:
-      return typeof this.title === 'string' && this.title !== '';
-    case TITLE_TEXT_MEDIA:
-      return typeof this.title === 'string' && this.title !== '' &&
-        typeof this.text === 'string' && this.text !== '' &&
-        typeof this.media === 'object' && this.media !== {} &&
-        typeof this.media.publicId === 'string' && this.media.publicId !== '' &&
-        typeof this.media.link === 'string' && this.media.link !== '';
-    case TITLE_TEXT:
-      return typeof this.title === 'string' && this.title !== '' &&
-        typeof this.text === 'string' && this.text !== '';
-    case TEXT_MEDIA:
-      return typeof this.text === 'string' && this.text !== '' &&
-        typeof this.media === 'object' && this.media !== {} &&
-        typeof this.media.publicId === 'string' && this.media.publicId !== '' &&
-        typeof this.media.link === 'string' && this.media.link !== '';
-    case FLASHCARD:
-      return typeof this.text === 'string' && this.text !== '' &&
-      typeof this.backText === 'string' && this.backText !== '';
-    case FILL_THE_GAPS:
-      return typeof this.gappedText === 'string' && this.gappedText !== '' &&
-        Array.isArray(this.falsyGapAnswers) && this.falsyGapAnswers.length > 1 &&
-        this.falsyGapAnswers.length <= FILL_THE_GAPS_MAX_ANSWERS_COUNT &&
-        this.falsyGapAnswers.every(answer =>
-          typeof answer === 'string' && answer !== '' && answer.length <= GAP_ANSWER_MAX_LENGTH) &&
-        typeof this.explanation === 'string' && this.explanation !== '';
-    case SINGLE_CHOICE_QUESTION:
-      return typeof this.question === 'string' && this.question !== '' && this.question.length <= QUESTION_MAX_LENGTH &&
-        typeof this.qcuGoodAnswer === 'string' && this.qcuGoodAnswer !== '' &&
-        this.qcuGoodAnswer.length <= QUESTION_MAX_LENGTH &&
-        Array.isArray(this.qcuFalsyAnswers) && this.qcuFalsyAnswers.length > 0 &&
-        this.qcuFalsyAnswers.length <= SINGLE_CHOICE_QUESTION_MAX_FALSY_ANSWERS_COUNT &&
-        this.qcuFalsyAnswers.every(answer =>
-          typeof answer === 'string' && answer !== '' && answer.length <= QC_ANSWER_MAX_LENGTH) &&
-        typeof this.explanation === 'string' && this.explanation !== '';
-    case ORDER_THE_SEQUENCE:
-      return typeof this.question === 'string' && this.question !== '' && this.question.length <= QUESTION_MAX_LENGTH &&
-      Array.isArray(this.orderedAnswers) && this.orderedAnswers.length > 1 &&
-      this.orderedAnswers.length <= ORDER_THE_SEQUENCE_MAX_ANSWERS_COUNT &&
-      this.orderedAnswers.every(answer => typeof answer === 'string') &&
-      typeof this.explanation === 'string' && this.explanation !== '';
-    case MULTIPLE_CHOICE_QUESTION:
-      return typeof this.question === 'string' && this.question !== '' && this.question.length <= QUESTION_MAX_LENGTH &&
-        Array.isArray(this.qcmAnswers) && this.qcmAnswers.length > 1 &&
-        this.qcmAnswers.length <= MULTIPLE_CHOICE_QUESTION_MAX_ANSWERS_COUNT &&
-        this.qcmAnswers.every(answer => typeof answer === 'object' &&
-          typeof answer.label === 'string' && answer.label !== '' && answer.label.length <= QC_ANSWER_MAX_LENGTH &&
-          typeof answer.correct === 'boolean') &&
-        this.qcmAnswers.some(answer => answer.correct === true) &&
-        typeof this.explanation === 'string' && this.explanation !== '';
-    case SURVEY:
-      if (!(typeof this.question === 'string' && this.question !== '' && this.question.length <= QUESTION_MAX_LENGTH)) {
-        return false;
-      }
-      if (!this.label || (!this.label.right && !this.label.left)) return true;
-      if (this.label && this.label.right && !this.label.left) return false;
-      if (this.label && !this.label.right && this.label.left) return false;
-
-      return this.label.right.length < SURVEY_LABEL_MAX_LENGTH && this.label.left.length < SURVEY_LABEL_MAX_LENGTH;
-    case OPEN_QUESTION:
-      return typeof this.question === 'string' && this.question !== '' && this.question.length < QUESTION_MAX_LENGTH;
-    case QUESTION_ANSWER:
-      return typeof this.question === 'string' && this.question !== '' && this.question.length < QUESTION_MAX_LENGTH &&
-        Array.isArray(this.questionAnswers) && this.questionAnswers.length > 1 &&
-        this.questionAnswers.length < QUESTION_ANSWER_MAX_ANSWERS_COUNT &&
-        this.questionAnswers.every(answer => typeof answer === 'string' && answer !== '');
-    default:
-      return false;
-  }
+  const cardHasError = !!cardValidationByTemplate(this.template)
+    .validate(this, { allowUnknown: true }).error;
+  return !cardHasError;
 }
 
 CardSchema.pre('save', save);
