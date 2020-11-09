30a16602949fa1f8982f13c2ea63bb2a28c20865
Sophie Moustard <sophie.moustard@gmail.com>
2020-11-09 15:42:39 +0100

    COM-1646 - fix bug on slot deletion

--- tests/unit/helpers/courseHistories.test.js
+++ tests/unit/helpers/courseHistories.test.js
@@ -41,15 +41,17 @@ describe('createHistoryOnSlotCreation', () => {
     createHistory.restore();
   });
 
-  it('should create a courseHistory', async () => {
+  it('should create a courseHistory with address', async () => {
     const payload = {
       startDate: '2019-02-03T09:00:00.000Z',
       endDate: '2019-02-03T10:00:00.000Z',
-      address: { fullAddress: 'ertyui',
+      address: {
+        fullAddress: 'ertyui',
         street: '12345',
         zipCode: '12345',
         city: 'qwert',
-        location: { type: 'Point', coordinates: [0, 1] } },
+        location: { type: 'Point', coordinates: [0, 1] },
+      },
       courseId: new ObjectID(),
     };
     const userId = new ObjectID();
@@ -61,13 +63,26 @@ describe('createHistoryOnSlotCreation', () => {
       payload.courseId,
       userId,
       SLOT_CREATION,
-      {
-        slot: {
-          startDate: payload.startDate,
-          endDate: payload.endDate,
-          address: payload.address,
-        },
-      }
+      { slot: { startDate: payload.startDate, endDate: payload.endDate, address: payload.address } }
+    );
+  });
+
+  it('should create a courseHistory without address', async () => {
+    const payload = {
+      startDate: '2019-02-03T09:00:00.000Z',
+      endDate: '2019-02-03T10:00:00.000Z',
+      courseId: new ObjectID(),
+    };
+    const userId = new ObjectID();
+
+    await CourseHistoriesHelper.createHistoryOnSlotCreation(payload, userId);
+
+    sinon.assert.calledOnceWithExactly(
+      createHistory,
+      payload.courseId,
+      userId,
+      SLOT_CREATION,
+      { slot: { startDate: payload.startDate, endDate: payload.endDate } }
     );
   });
 });
@@ -87,11 +102,13 @@ describe('createHistoryOnSlotDeletion', () => {
     const payload = {
       startDate: '2019-02-03T09:00:00.000Z',
       endDate: '2019-02-03T10:00:00.000Z',
-      address: { fullAddress: 'ertyui',
+      address: {
+        fullAddress: 'ertyui',
         street: '12345',
         zipCode: '12345',
         city: 'qwert',
-        location: { type: 'Point', coordinates: [0, 1] } },
+        location: { type: 'Point', coordinates: [0, 1] },
+      },
       courseId: new ObjectID(),
     };
     const userId = new ObjectID();
@@ -110,11 +127,14 @@ describe('createHistoryOnSlotDeletion', () => {
 
 describe('createHistoryOnSlotEdition', () => {
   let createHistory;
+  let createHistoryOnSlotCreation;
   beforeEach(() => {
     createHistory = sinon.stub(CourseHistoriesHelper, 'createHistory');
+    createHistoryOnSlotCreation = sinon.stub(CourseHistoriesHelper, 'createHistoryOnSlotCreation');
   });
   afterEach(() => {
     createHistory.restore();
+    createHistoryOnSlotCreation.restore();
   });
 
   it('should create history if date is updated', async () => {
@@ -132,6 +152,19 @@ describe('createHistoryOnSlotEdition', () => {
       SLOT_EDITION,
       { update: { startDate: { from: '2020-01-10T09:00:00', to: '2020-01-11T09:00:00' } } }
     );
+    sinon.assert.notCalled(createHistoryOnSlotCreation);
+  });
+
+  it('should create history with slot_creation action if not date in db', async () => {
+    const courseId = new ObjectID();
+    const slotFromDb = { courseId };
+    const payload = { startDate: '2020-01-11T09:00:00' };
+    const userId = new ObjectID();
+
+    await CourseHistoriesHelper.createHistoryOnSlotEdition(slotFromDb, payload, userId);
+
+    sinon.assert.calledOnceWithExactly(createHistoryOnSlotCreation, { ...slotFromDb, ...payload }, userId);
+    sinon.assert.notCalled(createHistory);
   });
 
   it('should not create history if date is not updated', async () => {
@@ -143,6 +176,7 @@ describe('createHistoryOnSlotEdition', () => {
     await CourseHistoriesHelper.createHistoryOnSlotEdition(slotFromDb, payload, userId);
 
     sinon.assert.notCalled(createHistory);
+    sinon.assert.notCalled(createHistoryOnSlotCreation);
   });
 
   it('should create history if hour is updated', async () => {
@@ -165,85 +199,7 @@ describe('createHistoryOnSlotEdition', () => {
         },
       }
     );
-  });
-});
-
-describe('list', () => {
-  let CourseHistoryMock;
-
-  beforeEach(() => {
-    CourseHistoryMock = sinon.mock(CourseHistory);
-  });
-
-  afterEach(() => {
-    CourseHistoryMock.restore();
-  });
-
-  it('should return the requested course histories', async () => {
-    const returnedList = [{
-      startDate: '2019-02-03T09:00:00.000Z',
-      endDate: '2019-02-03T10:00:00.000Z',
-      address: {
-        fullAddress: 'ertyui',
-        street: '12345',
-        zipCode: '12345',
-        city: 'qwert',
-        location: { type: 'Point', coordinates: [0, 1] },
-      },
-      course: new ObjectID(),
-    }];
-    const query = { course: returnedList[0].course };
-
-    CourseHistoryMock
-      .expects('find')
-      .withExactArgs(query)
-      .chain('populate')
-      .withExactArgs({ path: 'createdBy', select: '_id identity picture' })
-      .chain('populate')
-      .withExactArgs({ path: 'trainee', select: '_id identity' })
-      .chain('sort')
-      .withExactArgs({ createdAt: -1 })
-      .chain('lean')
-      .returns(returnedList);
-
-    const result = await CourseHistoriesHelper.list(query);
-
-    expect(result).toMatchObject(returnedList);
-    CourseHistoryMock.verify();
-  });
-
-  it('should return the requested course histories before createdAt', async () => {
-    const returnedList = [{
-      startDate: '2019-02-03T09:00:00.000Z',
-      endDate: '2019-02-03T10:00:00.000Z',
-      address: {
-        fullAddress: 'ertyui',
-        street: '12345',
-        zipCode: '12345',
-        city: 'qwert',
-        location: { type: 'Point', coordinates: [0, 1] },
-      },
-      course: new ObjectID(),
-      createdAt: '2019-02-03T10:00:00.000Z',
-    }];
-    const query = { course: returnedList[0].course, createdAt: '2019-02-04T10:00:00.000Z' };
-
-    CourseHistoryMock
-      .expects('find')
-      .withExactArgs({ course: query.course, createdAt: { $lt: query.createdAt } })
-      .chain('populate')
-      .withExactArgs({ path: 'createdBy', select: '_id identity picture' })
-      .chain('populate')
-      .withExactArgs({ path: 'trainee', select: '_id identity' })
-      .chain('sort')
-      .withExactArgs({ createdAt: -1 })
-      .chain('lean')
-      .returns(returnedList);
-
-    const result = await CourseHistoriesHelper.list(query);
-
-    expect(result).toMatchObject(returnedList);
-    CourseHistoryMock.verify();
+    sinon.assert.notCalled(createHistoryOnSlotCreation);
   });
 });
 
@@ -340,5 +296,84 @@ describe('createHistoryOnTraineeDeletion', () => {
       TRAINEE_DELETION,
       { trainee: payload.traineeId }
     );
+  });
+});
+
+describe('list', () => {
+  let CourseHistoryMock;
+
+  beforeEach(() => {
+    CourseHistoryMock = sinon.mock(CourseHistory);
+  });
+
+  afterEach(() => {
+    CourseHistoryMock.restore();
+  });
+
+  it('should return the requested course histories', async () => {
+    const returnedList = [{
+      startDate: '2019-02-03T09:00:00.000Z',
+      endDate: '2019-02-03T10:00:00.000Z',
+      address: {
+        fullAddress: 'ertyui',
+        street: '12345',
+        zipCode: '12345',
+        city: 'qwert',
+        location: { type: 'Point', coordinates: [0, 1] },
+      },
+      course: new ObjectID(),
+    }];
+    const query = { course: returnedList[0].course };
+
+    CourseHistoryMock
+      .expects('find')
+      .withExactArgs(query)
+      .chain('populate')
+      .withExactArgs({ path: 'createdBy', select: '_id identity picture' })
+      .chain('populate')
+      .withExactArgs({ path: 'trainee', select: '_id identity' })
+      .chain('sort')
+      .withExactArgs({ createdAt: -1 })
+      .chain('lean')
+      .returns(returnedList);
+
+    const result = await CourseHistoriesHelper.list(query);
+
+    expect(result).toMatchObject(returnedList);
+    CourseHistoryMock.verify();
+  });
+
+  it('should return the requested course histories before createdAt', async () => {
+    const returnedList = [{
+      startDate: '2019-02-03T09:00:00.000Z',
+      endDate: '2019-02-03T10:00:00.000Z',
+      address: {
+        fullAddress: 'ertyui',
+        street: '12345',
+        zipCode: '12345',
+        city: 'qwert',
+        location: { type: 'Point', coordinates: [0, 1] },
+      },
+      course: new ObjectID(),
+      createdAt: '2019-02-03T10:00:00.000Z',
+    }];
+    const query = { course: returnedList[0].course, createdAt: '2019-02-04T10:00:00.000Z' };
+
+    CourseHistoryMock
+      .expects('find')
+      .withExactArgs({ course: query.course, createdAt: { $lt: query.createdAt } })
+      .chain('populate')
+      .withExactArgs({ path: 'createdBy', select: '_id identity picture' })
+      .chain('populate')
+      .withExactArgs({ path: 'trainee', select: '_id identity' })
+      .chain('sort')
+      .withExactArgs({ createdAt: -1 })
+      .chain('lean')
+      .returns(returnedList);
+
+    const result = await CourseHistoriesHelper.list(query);
+
+    expect(result).toMatchObject(returnedList);
+    CourseHistoryMock.verify();
   });
 });
