33a1c735d3c73760773c118623d41af347311c43
Félix <felix@alenvi.io>
2020-11-24 11:35:00 +0100

    COM-1657 add POST and GET routes

---
+++ src/controllers/categoryController.js
@@ -0,0 +1,37 @@
+const Boom = require('@hapi/boom');
+const CategoryHelper = require('../helpers/programs');
+const translate = require('../helpers/translate');
+
+const { language } = translate;
+
+const list = async (req) => {
+  try {
+    const categories = await CategoryHelper.list();
+
+    return {
+      message: categories.length ? translate[language].categoriesFound : translate[language].categoriesNotFound,
+      data: { categories },
+    };
+  } catch (e) {
+    req.log('error', e);
+    return Boom.isBoom(e) ? e : Boom.badImplementation(e);
+  }
+};
+
+const create = async (req) => {
+  try {
+    await CategoryHelper.create(req.payload);
+
+    return {
+      message: translate[language].categoryCreated,
+    };
+  } catch (e) {
+    req.log('error', e);
+    return Boom.isBoom(e) ? e : Boom.badImplementation(e);
+  }
+};
+
+module.exports = {
+  list,
+  create,
+};
---
+++ src/helpers/categories.js
@@ -0,0 +1,5 @@
+const Category = require('../models/Category');
+
+exports.create = payload => (new Category(payload)).save();
+
+exports.list = async () => Category.find({}).lean();
