dfde691820c3b834fd88fcecc6ea22430c2403c1
Félix <felix@alenvi.io>
2020-11-26 11:46:05 +0100

    COM-1657 using CountDocuments

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const CategorySchema = mongoose.Schema({
-  name: { type: String, unique: true, required: true },
+  name: { type: String, unique: true, required: true, lowercase: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Category', CategorySchema);
--- src/routes/preHandlers/categories.js
+++ src/routes/preHandlers/categories.js
@@ -1,7 +1,8 @@
 const Boom = require('@hapi/boom');
 const Category = require('../../models/Category');
 
-exports.checkCategoryNameExists = async req => (await Category.findOne({ name: req.payload.name.toLowerCase() })
-  ? Boom.conflict()
-  : null
-);
+exports.checkCategoryNameExists = async (req) => {
+  const existingCourse = await Category.countDocuments({ name: req.payload.name.toLowerCase() });
+
+  return existingCourse ? Boom.conflict() : null;
+};
