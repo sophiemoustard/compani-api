be5b858f2c0293c147e2bb8571c915627193be54
Félix <felix@alenvi.io>
2020-11-26 14:29:33 +0100

    COM-1657 add unit test

--- tests/unit/helpers/categories.test.js
+++ tests/unit/helpers/categories.test.js
@@ -1,5 +1,6 @@
 const sinon = require('sinon');
 const expect = require('expect');
+const { ObjectID } = require('mongodb');
 const Category = require('../../../src/models/Category');
 const CategoryHelper = require('../../../src/helpers/categories');
 
@@ -42,5 +43,27 @@ describe('list', () => {
 
     const result = await CategoryHelper.list();
     expect(result).toMatchObject(categoriesList);
+  });
+});
+
+describe('update', () => {
+  let CategoryMock;
+  beforeEach(() => {
+    CategoryMock = sinon.mock(Category);
+  });
+  afterEach(() => {
+    CategoryMock.restore();
+  });
+
+  it('should update name', async () => {
+    const categoryId = new ObjectID();
+    const payload = { name: 'nouveau nom' };
+
+    CategoryMock.expects('updateOne')
+      .withExactArgs({ _id: categoryId }, { $set: payload })
+      .returns({ _id: categoryId, name: 'nouveau nom' });
+
+    const result = await CategoryHelper.update(categoryId, payload);
+    expect(result).toMatchObject({ _id: categoryId, name: 'nouveau nom' });
   });
 });
