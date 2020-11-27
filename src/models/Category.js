f63d9638d836c18f1763f15ef9d6f57f925f9812
Félix <felix@alenvi.io>
2020-11-27 13:14:10 +0100

    COM-1658 add categories to program model

--- src/models/Category.js
+++ src/models/Category.js
@@ -4,4 +4,10 @@ const CategorySchema = mongoose.Schema({
   name: { type: String, unique: true, required: true, lowercase: true },
 }, { timestamps: true });
 
+CategorySchema.virtual('programs', {
+  ref: 'Program',
+  localField: '_id',
+  foreignField: 'categories',
+});
+
 module.exports = mongoose.model('Category', CategorySchema);
