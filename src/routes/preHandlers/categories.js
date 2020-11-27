82fd6210e40b999177e0212554082a822d007b80
Félix <felix@alenvi.io>
2020-11-27 16:33:15 +0100

    COM-1657 using countDocuments

--- src/routes/preHandlers/categories.js
+++ src/routes/preHandlers/categories.js
@@ -1,14 +1,12 @@
 const Boom = require('@hapi/boom');
 const Category = require('../../models/Category');
 
-exports.checkCategoryNameExists = async req => (
-  await Category.countDocuments({ name: req.payload.name.toLowerCase() })
-    ? Boom.conflict()
-    : null
-);
+exports.checkCategoryNameExists = async (req) => {
+  const existingCategoryName = await Category.countDocuments({ name: req.payload.name.toLowerCase() });
+  return existingCategoryName ? Boom.conflict() : null;
+};
 
-exports.checkCategoryExists = async req => (
-  await Category.findOne({ _id: req.params._id }).lean()
-    ? null
-    : Boom.notFound()
-);
+exports.checkCategoryExists = async (req) => {
+  const existingCategory = await Category.countDocuments({ _id: req.params._id });
+  return !existingCategory ? Boom.notFound() : null;
+};
