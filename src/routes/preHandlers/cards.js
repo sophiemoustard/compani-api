3fbb1ce0b086dfe439a7a5589c2fa75e5c8af70c
KennyCallegari <mel.kenny@gmail.com>
2020-12-15 17:30:07 +0100

    COM-1693 - answer deletion

--- src/routes/preHandlers/cards.js
+++ src/routes/preHandlers/cards.js
@@ -20,6 +20,7 @@ const {
   ORDER_THE_SEQUENCE_MAX_ANSWERS_COUNT,
   ORDER_THE_SEQUENCE_MIN_ANSWERS_COUNT,
   FILL_THE_GAPS_MAX_ANSWERS_COUNT,
+  FILL_THE_GAPS_MIN_ANSWERS_COUNT,
 } = require('../../helpers/constants');
 const Activity = require('../../models/Activity');
 
@@ -119,7 +120,11 @@ exports.authorizeCardAnswerUpdate = async (req) => {
 exports.authorizeCardAnswerDeletion = async (req) => {
   const card = await Card.findOne({
     _id: req.params._id,
-    $or: [{ 'qcAnswers._id': req.params.answerId }, { 'orderedAnswers._id': req.params.answerId }],
+    $or: [
+      { 'qcAnswers._id': req.params.answerId },
+      { 'orderedAnswers._id': req.params.answerId },
+      { 'falsyGapAnswers._id': req.params.answerId },
+    ],
   }).lean();
   if (!card) throw Boom.notFound();
 
@@ -135,6 +140,9 @@ exports.authorizeCardAnswerDeletion = async (req) => {
       break;
     case ORDER_THE_SEQUENCE:
       if (card.orderedAnswers.length <= ORDER_THE_SEQUENCE_MIN_ANSWERS_COUNT) return Boom.forbidden();
+      break;
+    case FILL_THE_GAPS:
+      if (card.falsyGapAnswers.length <= FILL_THE_GAPS_MIN_ANSWERS_COUNT) return Boom.forbidden();
       break;
   }
 
