27fd9e2ad36d9b6c8211d0551867ae1eb2aa0b2e
KennyCallegari <mel.kenny@gmail.com>
2020-12-16 17:19:18 +0100

    sinon-mongoose-is-dead - working proposition for stubChainedQueries and calledWithExactly

--- tests/unit/utils.js
+++ tests/unit/utils.js
@@ -1,30 +1,36 @@
 const sinon = require('sinon');
-const get = require('lodash/get');
 
-const checkSinon = (Stub, skusku) => {
-  let { query } = skusku[0];
-  sinon.assert.calledWithExactly(Stub, skusku[0].arg);
+const stubChainedQueries = (stubbedMethodReturns, chainedQueries = ['populate', 'lean']) => {
+  const chainedQueriesStubs = {};
+  const chainedQueriesCount = chainedQueries.length;
 
-  for (let i = 1; i < skusku.length; i++) {
-    query = `${query}.getCall(0).returnValue.${skusku[i].query}`;
-
-    sinon.assert.calledWithExactly(get(Stub, query), skusku[i].arg); // ne marche pas
+  for (let i = 0; i < chainedQueriesCount - 1; i++) {
+    chainedQueriesStubs[chainedQueries[i]] = sinon.stub().returnsThis();
   }
+
+  let lastChainedQueryStub = sinon.stub();
+  for (let i = 0; i < stubbedMethodReturns.length; i++) {
+    lastChainedQueryStub = lastChainedQueryStub.onCall(i).returns(stubbedMethodReturns[i]);
+  }
+
+  chainedQueriesStubs[chainedQueries[chainedQueriesCount - 1]] = lastChainedQueryStub;
+
+  return chainedQueriesStubs;
 };
 
-const chainedMongoose = (returnedValues) => {
-  let previousReturned = sinon.stub().onCall(0).returns(returnedValues[0]);
-  for (let i = 1; i < returnedValues.length; i++) {
-    previousReturned = previousReturned.onCall(i).returns(returnedValues[i]);
-  }
+const calledWithExactly = (stubbedMethod, chainedPayload, callCount = 0) => {
+  let chainedQuery = stubbedMethod;
+  sinon.assert.calledWithExactly(chainedQuery.getCall(callCount), chainedPayload[0].args);
 
-  return {
-    populate: sinon.stub().returnsThis(),
-    lean: previousReturned,
-  };
+  for (let i = 1; i < chainedPayload.length; i++) {
+    const { query, args } = chainedPayload[i];
+    chainedQuery = chainedQuery.getCall(callCount).returnValue[query];
+
+    sinon.assert.calledWithExactly(chainedQuery, args);
+  }
 };
 
 module.exports = {
-  checkSinon,
-  chainedMongoose,
+  stubChainedQueries,
+  calledWithExactly,
 };
