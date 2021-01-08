a525c39aaae1a9faf69c4a52b9ea3e9a0927e215
ulysse <ulysse@alenvi.io>
2021-01-08 11:21:11 +0100

    COM-1741: add test to check if roles has good permissions

--- tests/unit/data/rights.test.js
+++ tests/unit/data/rights.test.js
@@ -1,5 +1,14 @@
 const expect = require('expect');
 const { rights } = require('../../../src/data/rights');
+const {
+  AUXILIARY,
+  PLANNING_REFERENT,
+  COACH,
+  CLIENT_ADMIN,
+  TRAINER,
+  TRAINING_ORGANISATION_MANAGER,
+  VENDOR_ADMIN,
+} = require('../../../src/helpers/constants');
 
 describe('checking the format of right.js file', () => {
   it('there must be only one right per permission', async () => {
@@ -7,5 +16,40 @@ describe('checking the format of right.js file', () => {
     const noDuplicatedRights = new Set(permissions).size === permissions.length;
 
     expect(noDuplicatedRights).toBeTruthy();
+  });
+
+  it('a role must have the permissions associated with the role below it (client side)', async () => {
+    const auxiliaryPermissions = rights
+      .filter(right => right.rolesConcerned.includes(AUXILIARY)).map(right => right.permission);
+    const planningReferentPermissions = rights
+      .filter(right => right.rolesConcerned.includes(PLANNING_REFERENT)).map(right => right.permission);
+    const coachPermissions = rights
+      .filter(right => right.rolesConcerned.includes(COACH)).map(right => right.permission);
+    const clientAdminPermissions = rights
+      .filter(right => right.rolesConcerned.includes(CLIENT_ADMIN)).map(right => right.permission);
+
+    const arePermissionsIncludedList = [
+      auxiliaryPermissions.map(permission => planningReferentPermissions.includes(permission)),
+      planningReferentPermissions.map(permission => coachPermissions.includes(permission)),
+      coachPermissions.map(permission => clientAdminPermissions.includes(permission)),
+    ];
+
+    expect(arePermissionsIncludedList.every(value => value)).toBeTruthy();
+  });
+
+  it('a role must have the permissions associated with the role below it (vendor side)', async () => {
+    const trainerPermissions = rights
+      .filter(right => right.rolesConcerned.includes(TRAINER)).map(right => right.permission);
+    const trainingOrganisationManagerPermissions = rights
+      .filter(right => right.rolesConcerned.includes(TRAINING_ORGANISATION_MANAGER)).map(right => right.permission);
+    const vendorAdminPermissions = rights
+      .filter(right => right.rolesConcerned.includes(VENDOR_ADMIN)).map(right => right.permission);
+
+    const arePermissionsIncludedList = [
+      trainerPermissions.map(permission => trainingOrganisationManagerPermissions.includes(permission)),
+      trainingOrganisationManagerPermissions.map(permission => vendorAdminPermissions.includes(permission)),
+    ];
+
+    expect(arePermissionsIncludedList.every(value => value)).toBeTruthy();
   });
 });
