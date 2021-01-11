0f3a3057808ff5d9f96036e729f5a5cbd1628b22
ulysse <ulysse@alenvi.io>
2021-01-11 11:54:04 +0100

    COM-1741: a test per inclusion

--- tests/unit/data/rights.test.js
+++ tests/unit/data/rights.test.js
@@ -19,41 +19,75 @@ describe('checking the format of right.js file', () => {
     expect(noDuplicatedRights).toBeTruthy();
   });
 
-  it('a role must have the permissions associated with the role below it (client side)', async () => {
+  it('an auxiliary must have the permissions associated with an auxiliary without company', async () => {
     const auxiliaryWithoutCompanyPermissions = rights
       .filter(right => right.rolesConcerned.includes(AUXILIARY_WITHOUT_COMPANY)).map(right => right.permission);
     const auxiliaryPermissions = rights
       .filter(right => right.rolesConcerned.includes(AUXILIARY)).map(right => right.permission);
+
+    const arePermissionsIncluded = auxiliaryWithoutCompanyPermissions
+      .map(permission => auxiliaryPermissions.includes(permission)).every(value => value);
+
+    expect(arePermissionsIncluded).toBeTruthy();
+  });
+
+  it('a planing referent must have the permissions associated with an auxiliary', async () => {
+    const auxiliaryPermissions = rights
+      .filter(right => right.rolesConcerned.includes(AUXILIARY)).map(right => right.permission);
     const planningReferentPermissions = rights
       .filter(right => right.rolesConcerned.includes(PLANNING_REFERENT)).map(right => right.permission);
+
+    const arePermissionsIncluded = auxiliaryPermissions
+      .map(permission => planningReferentPermissions.includes(permission)).every(value => value);
+
+    expect(arePermissionsIncluded).toBeTruthy();
+  });
+
+  it('a coach must have the permissions associated with a planning referent', async () => {
+    const planningReferentPermissions = rights
+      .filter(right => right.rolesConcerned.includes(PLANNING_REFERENT)).map(right => right.permission);
+    const coachPermissions = rights
+      .filter(right => right.rolesConcerned.includes(COACH)).map(right => right.permission);
+
+    const arePermissionsIncluded = planningReferentPermissions
+      .map(permission => coachPermissions.includes(permission)).every(value => value);
+
+    expect(arePermissionsIncluded).toBeTruthy();
+  });
+
+  it('a client admin must have the permissions associated with a coach', async () => {
     const coachPermissions = rights
       .filter(right => right.rolesConcerned.includes(COACH)).map(right => right.permission);
     const clientAdminPermissions = rights
       .filter(right => right.rolesConcerned.includes(CLIENT_ADMIN)).map(right => right.permission);
 
-    const arePermissionsIncludedList = [
-      auxiliaryWithoutCompanyPermissions.map(permission => auxiliaryPermissions.includes(permission)),
-      auxiliaryPermissions.map(permission => planningReferentPermissions.includes(permission)),
-      planningReferentPermissions.map(permission => coachPermissions.includes(permission)),
-      coachPermissions.map(permission => clientAdminPermissions.includes(permission)),
-    ];
+    const arePermissionsIncluded = coachPermissions
+      .map(permission => clientAdminPermissions.includes(permission)).every(value => value);
 
-    expect(arePermissionsIncludedList.every(value => value)).toBeTruthy();
+    expect(arePermissionsIncluded).toBeTruthy();
   });
 
-  it('a role must have the permissions associated with the role below it (vendor side)', async () => {
+  it('a ROF must have the permissions associated with a trainer', async () => {
     const trainerPermissions = rights
       .filter(right => right.rolesConcerned.includes(TRAINER)).map(right => right.permission);
+    const trainingOrganisationManagerPermissions = rights
+      .filter(right => right.rolesConcerned.includes(TRAINING_ORGANISATION_MANAGER)).map(right => right.permission);
+
+    const arePermissionsIncluded = trainerPermissions
+      .map(permission => trainingOrganisationManagerPermissions.includes(permission)).every(value => value);
+
+    expect(arePermissionsIncluded).toBeTruthy();
+  });
+
+  it('a vendor admin must have the permissions associated with a ROF', async () => {
     const trainingOrganisationManagerPermissions = rights
       .filter(right => right.rolesConcerned.includes(TRAINING_ORGANISATION_MANAGER)).map(right => right.permission);
     const vendorAdminPermissions = rights
       .filter(right => right.rolesConcerned.includes(VENDOR_ADMIN)).map(right => right.permission);
 
-    const arePermissionsIncludedList = [
-      trainerPermissions.map(permission => trainingOrganisationManagerPermissions.includes(permission)),
-      trainingOrganisationManagerPermissions.map(permission => vendorAdminPermissions.includes(permission)),
-    ];
+    const arePermissionsIncluded = trainingOrganisationManagerPermissions
+      .map(permission => vendorAdminPermissions.includes(permission)).every(value => value);
 
-    expect(arePermissionsIncludedList.every(value => value)).toBeTruthy();
+    expect(arePermissionsIncluded).toBeTruthy();
   });
 });
