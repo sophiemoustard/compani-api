767c205ce43c2014d6129771308fead84ae06dc1
Félix <felix@alenvi.io>
2021-01-11 15:54:50 +0100

    COM-1751 add fileName generation

--- src/helpers/attendanceSheets.js
+++ src/helpers/attendanceSheets.js
@@ -1,12 +1,20 @@
 const omit = require('lodash/omit');
 const get = require('lodash/get');
+const moment = require('moment');
 const AttendanceSheet = require('../models/AttendanceSheet');
 const Course = require('../models/Course');
+const User = require('../models/User');
 const GCloudStorageHelper = require('./gCloudStorage');
+const UtilsHelper = require('./utils');
 
 exports.create = async (payload) => {
+  let fileName = moment(payload.date).format('DD-MMMM-YYYY');
+  if (payload.trainee) {
+    const { identity } = await User.findOne({ _id: payload.trainee }).lean();
+    fileName = UtilsHelper.formatIdentity(identity, 'FL');
+  }
   const fileUploaded = await GCloudStorageHelper.uploadCourseFile({
-    fileName: get(payload, 'file.hapi.filename') || '',
+    fileName: `emargement_${fileName}`,
     file: payload.file,
   });
   const newAttendanceSheet = await AttendanceSheet.create({ ...omit(payload, 'file'), file: fileUploaded });
--- tests/unit/helpers/attendanceSheets.test.js
+++ tests/unit/helpers/attendanceSheets.test.js
@@ -2,10 +2,12 @@ const sinon = require('sinon');
 const expect = require('expect');
 const { ObjectID } = require('mongodb');
 const Course = require('../../../src/models/Course');
+const User = require('../../../src/models/User');
 const AttendanceSheet = require('../../../src/models/AttendanceSheet');
 const attendanceSheetHelper = require('../../../src/helpers/attendanceSheets');
 const SinonMongoose = require('../sinonMongoose');
 const GCloudStorageHelper = require('../../../src/helpers/gCloudStorage');
+const UtilsHelper = require('../../../src/helpers/utils');
 
 describe('list', () => {
   let findOne;
@@ -44,21 +46,27 @@ describe('create', () => {
   let uploadCourseFile;
   let create;
   let updateOne;
+  let findOne;
+  let formatIdentity;
 
   beforeEach(() => {
     uploadCourseFile = sinon.stub(GCloudStorageHelper, 'uploadCourseFile');
     create = sinon.stub(AttendanceSheet, 'create');
     updateOne = sinon.stub(Course, 'updateOne');
+    findOne = sinon.stub(User, 'findOne');
+    formatIdentity = sinon.stub(UtilsHelper, 'formatIdentity');
   });
 
   afterEach(() => {
     uploadCourseFile.restore();
     create.restore();
     updateOne.restore();
+    findOne.restore();
+    formatIdentity.restore();
   });
 
-  it('should create an attendance sheet', async () => {
-    const payload = { date: '2020-04-03T10:00:00', course: new ObjectID(), file: { hapi: { filename: 'test.pdf' } } };
+  it('should create an attendance sheet for INTRA course', async () => {
+    const payload = { date: '2020-04-03T10:00:00', course: new ObjectID(), file: 'test.pdf' };
     uploadCourseFile.returns({ publicId: 'yo', link: 'yo' });
     const attendanceSheetId = new ObjectID();
     create.returns({ _id: attendanceSheetId });
@@ -66,11 +74,47 @@ describe('create', () => {
     await attendanceSheetHelper.create(payload);
     sinon.assert.calledOnceWithExactly(
       uploadCourseFile,
-      { fileName: 'test.pdf', file: { hapi: { filename: 'test.pdf' } } }
+      { fileName: 'emargement_03-avril-2020', file: 'test.pdf' }
     );
     sinon.assert.calledOnceWithExactly(
       create,
       { course: payload.course, date: '2020-04-03T10:00:00', file: { publicId: 'yo', link: 'yo' } }
+    );
+    sinon.assert.calledOnceWithExactly(
+      updateOne,
+      { _id: payload.course },
+      { $push: { attendanceSheets: attendanceSheetId } }
+    );
+    sinon.assert.notCalled(findOne);
+    sinon.assert.notCalled(formatIdentity);
+  });
+
+  it('should create an attendance sheet for INTER course', async () => {
+    const payload = { trainee: 'id de quelqun', course: new ObjectID(), file: 'test.pdf' };
+    const returnedUser = { identity: { firstName: 'monsieur', lastname: 'patate' } };
+    uploadCourseFile.returns({ publicId: 'yo', link: 'yo' });
+    const attendanceSheetId = new ObjectID();
+    findOne.returns(SinonMongoose.stubChainedQueries([returnedUser]));
+    formatIdentity.returns('monsieurPATATE');
+    create.returns({ _id: attendanceSheetId });
+
+    await attendanceSheetHelper.create(payload);
+    SinonMongoose.calledWithExactly(findOne, [
+      { query: '', args: [{ _id: 'id de quelqun' }] },
+      { query: 'lean' },
+    ]);
+    sinon.assert.calledOnceWithExactly(
+      formatIdentity,
+      { firstName: 'monsieur', lastname: 'patate' },
+      'FL'
+    );
+    sinon.assert.calledOnceWithExactly(
+      uploadCourseFile,
+      { fileName: 'emargement_monsieurPATATE', file: 'test.pdf' }
+    );
+    sinon.assert.calledOnceWithExactly(
+      create,
+      { course: payload.course, trainee: 'id de quelqun', file: { publicId: 'yo', link: 'yo' } }
     );
     sinon.assert.calledOnceWithExactly(
       updateOne,
