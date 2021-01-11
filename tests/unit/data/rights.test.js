18c403aae5a0dc85b7547330f5975b46a1684dd2
ulysse <ulysse@alenvi.io>
2021-01-11 14:06:07 +0100

    COM-1741: clarify tests 2

--- tests/unit/data/rights.test.js
+++ tests/unit/data/rights.test.js
@@ -21,9 +21,11 @@ describe('checking the format of right.js file', () => {
 
   it('an auxiliary must have the permissions associated with an auxiliary without company', async () => {
     const auxiliaryWithoutCompanyPermissions = rights
-      .filter(right => right.rolesConcerned.includes(AUXILIARY_WITHOUT_COMPANY)).map(right => right.permission);
+      .filter(right => right.rolesConcerned.includes(AUXILIARY_WITHOUT_COMPANY))
+      .map(right => right.permission);
     const auxiliaryPermissions = rights
-      .filter(right => right.rolesConcerned.includes(AUXILIARY)).map(right => right.permission);
+      .filter(right => right.rolesConcerned.includes(AUXILIARY))
+      .map(right => right.permission);
 
     const arePermissionsIncluded = auxiliaryWithoutCompanyPermissions
       .map(permission => auxiliaryPermissions.includes(permission))
@@ -34,9 +36,11 @@ describe('checking the format of right.js file', () => {
 
   it('a planing referent must have the permissions associated with an auxiliary', async () => {
     const auxiliaryPermissions = rights
-      .filter(right => right.rolesConcerned.includes(AUXILIARY)).map(right => right.permission);
+      .filter(right => right.rolesConcerned.includes(AUXILIARY))
+      .map(right => right.permission);
     const planningReferentPermissions = rights
-      .filter(right => right.rolesConcerned.includes(PLANNING_REFERENT)).map(right => right.permission);
+      .filter(right => right.rolesConcerned.includes(PLANNING_REFERENT))
+      .map(right => right.permission);
 
     const arePermissionsIncluded = auxiliaryPermissions
       .map(permission => planningReferentPermissions.includes(permission))
@@ -47,9 +51,11 @@ describe('checking the format of right.js file', () => {
 
   it('a coach must have the permissions associated with a planning referent', async () => {
     const planningReferentPermissions = rights
-      .filter(right => right.rolesConcerned.includes(PLANNING_REFERENT)).map(right => right.permission);
+      .filter(right => right.rolesConcerned.includes(PLANNING_REFERENT))
+      .map(right => right.permission);
     const coachPermissions = rights
-      .filter(right => right.rolesConcerned.includes(COACH)).map(right => right.permission);
+      .filter(right => right.rolesConcerned.includes(COACH))
+      .map(right => right.permission);
 
     const arePermissionsIncluded = planningReferentPermissions
       .map(permission => coachPermissions.includes(permission))
@@ -60,9 +66,11 @@ describe('checking the format of right.js file', () => {
 
   it('a client admin must have the permissions associated with a coach', async () => {
     const coachPermissions = rights
-      .filter(right => right.rolesConcerned.includes(COACH)).map(right => right.permission);
+      .filter(right => right.rolesConcerned.includes(COACH))
+      .map(right => right.permission);
     const clientAdminPermissions = rights
-      .filter(right => right.rolesConcerned.includes(CLIENT_ADMIN)).map(right => right.permission);
+      .filter(right => right.rolesConcerned.includes(CLIENT_ADMIN))
+      .map(right => right.permission);
 
     const arePermissionsIncluded = coachPermissions
       .map(permission => clientAdminPermissions.includes(permission))
@@ -73,9 +81,11 @@ describe('checking the format of right.js file', () => {
 
   it('a ROF must have the permissions associated with a trainer', async () => {
     const trainerPermissions = rights
-      .filter(right => right.rolesConcerned.includes(TRAINER)).map(right => right.permission);
+      .filter(right => right.rolesConcerned.includes(TRAINER))
+      .map(right => right.permission);
     const trainingOrganisationManagerPermissions = rights
-      .filter(right => right.rolesConcerned.includes(TRAINING_ORGANISATION_MANAGER)).map(right => right.permission);
+      .filter(right => right.rolesConcerned.includes(TRAINING_ORGANISATION_MANAGER))
+      .map(right => right.permission);
 
     const arePermissionsIncluded = trainerPermissions
       .map(permission => trainingOrganisationManagerPermissions.includes(permission))
@@ -86,9 +96,11 @@ describe('checking the format of right.js file', () => {
 
   it('a vendor admin must have the permissions associated with a ROF', async () => {
     const trainingOrganisationManagerPermissions = rights
-      .filter(right => right.rolesConcerned.includes(TRAINING_ORGANISATION_MANAGER)).map(right => right.permission);
+      .filter(right => right.rolesConcerned.includes(TRAINING_ORGANISATION_MANAGER))
+      .map(right => right.permission);
     const vendorAdminPermissions = rights
-      .filter(right => right.rolesConcerned.includes(VENDOR_ADMIN)).map(right => right.permission);
+      .filter(right => right.rolesConcerned.includes(VENDOR_ADMIN))
+      .map(right => right.permission);
 
     const arePermissionsIncluded = trainingOrganisationManagerPermissions
       .map(permission => vendorAdminPermissions.includes(permission))
