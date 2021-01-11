a1d62448dba462cabe202ab30461f9b7c9162b36
Félix <felix@alenvi.io>
2021-01-11 16:58:09 +0100

    COM-1751 add extra lines

--- src/helpers/attendanceSheets.js
+++ src/helpers/attendanceSheets.js
@@ -11,17 +11,21 @@ exports.create = async (payload) => {
   let fileName = moment(payload.date).format('DD-MMMM-YYYY');
   if (payload.trainee) {
     const { identity } = await User.findOne({ _id: payload.trainee }).lean();
+
     fileName = UtilsHelper.formatIdentity(identity, 'FL');
   }
+
   const fileUploaded = await GCloudStorageHelper.uploadCourseFile({
     fileName: `emargement_${fileName}`,
     file: payload.file,
   });
+
   const newAttendanceSheet = await AttendanceSheet.create({ ...omit(payload, 'file'), file: fileUploaded });
   await Course.updateOne({ _id: payload.course }, { $push: { attendanceSheets: newAttendanceSheet._id } });
 };
 
 exports.list = async (query) => {
   const course = await Course.findOne({ _id: query.course }).populate({ path: 'attendanceSheets' }).lean();
+
   return get(course, 'attendanceSheets', []);
 };
