560950b4ea5a57d9cd20e82a61f89b04406d5894
ulysse <ulysse@alenvi.io>
2021-01-11 12:09:08 +0100

    COM-1771: route for various versions

--- src/routes/version.js
+++ src/routes/version.js
@@ -8,6 +8,20 @@ exports.plugin = {
   register: async (server) => {
     server.route({
       method: 'GET',
+      path: '/check-update',
+      options: {
+        auth: false,
+        validate: {
+          query: Joi.object({
+            apiVersion: Joi.string(),
+            mobileVersion: Joi.string(),
+          }).required(),
+        },
+      },
+      handler: shouldUpdate,
+    });
+    server.route({
+      method: 'GET',
       path: '/should-update',
       options: {
         auth: false,
