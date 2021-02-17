05a49ab3d37e7969e7aee8d22a380d804640799d
KennyCallegari <mel.kenny@gmail.com>
2021-02-17 16:07:07 +0100

    COM-1895 - disable slot deletion if attendance exist

--- src/routes/preHandlers/courseSlot.js
+++ src/routes/preHandlers/courseSlot.js
@@ -4,6 +4,7 @@ const moment = require('moment');
 const CourseSlot = require('../../models/CourseSlot');
 const Course = require('../../models/Course');
 const Step = require('../../models/Step');
+const Attendance = require('../../models/Attendance');
 const translate = require('../../helpers/translate');
 const { checkAuthorization } = require('./courses');
 const { E_LEARNING } = require('../../helpers/constants');
@@ -77,6 +78,9 @@ exports.authorizeDeletion = async (req) => {
   try {
     const courseId = get(req, 'pre.courseSlot.course') || '';
     await formatAndCheckAuthorization(courseId, req.auth.credentials);
+
+    const attendanceExists = await Attendance.countDocuments({ courseSlot: get(req, 'pre.courseSlot') });
+    if (attendanceExists) throw Boom.conflict(translate[language].attendanceExists);
 
     return null;
   } catch (e) {
