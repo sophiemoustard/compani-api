1f3f43b693b3fec18ffc2af13824bc8b4661e2ec
ulysse <ulysse@alenvi.io>
2021-04-01 09:42:31 +0200

    COM-1974: alleviate seed

--- tests/integration/seed/questionnairesSeed.js
+++ tests/integration/seed/questionnairesSeed.js
@@ -1,13 +1,6 @@
 const { ObjectID } = require('mongodb');
 const Questionnaire = require('../../../src/models/Questionnaire');
-const Card = require('../../../src/models/Card');
 const { populateDBForAuthentication } = require('./authenticationSeed');
-const { OPEN_QUESTION, SURVEY } = require('../../../src/helpers/constants');
-
-const cardsList = [
-  { _id: new ObjectID(), template: OPEN_QUESTION },
-  { _id: new ObjectID(), template: SURVEY },
-];
 
 const questionnairesList = [
   {
@@ -15,30 +8,19 @@ const questionnairesList = [
     title: 'test',
     status: 'draft',
     type: 'expectations',
-    cards: [cardsList[0]._id],
+    cards: [[new ObjectID()]],
   },
-  {
-    _id: new ObjectID(),
-    title: 'test2',
-    status: 'published',
-    type: 'expectations',
-    cards: [cardsList[1]._id],
-  },
-
 ];
 
 const populateDB = async () => {
   await Questionnaire.deleteMany({});
-  await Card.deleteMany({});
 
   await populateDBForAuthentication();
 
   await Questionnaire.insertMany(questionnairesList);
-  await Card.insertMany(cardsList);
 };
 
 module.exports = {
   populateDB,
   questionnairesList,
-  cardsList,
 };
