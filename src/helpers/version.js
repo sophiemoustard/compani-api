0022b6e8286f62ba375c81d4227b81f79a8c3776
Chloe Molle <chloe.molle@neuf.fr>
2021-04-06 14:43:51 +0200

    fix bug

--- src/helpers/version.js
+++ src/helpers/version.js
@@ -4,7 +4,7 @@ exports.shouldUpdate = async (query) => {
   const { apiVersion, mobileVersion, appName } = query;
   if (apiVersion) return true;
   if ((!appName || appName === FORMATION) && process.env.FORMATION_MOBILE_VERSION.includes(mobileVersion)) return false;
-  if (process.env[`${appName.toUpperCase()}_MOBILE_VERSION`].includes(mobileVersion)) return false;
+  if (appName && process.env[`${appName.toUpperCase()}_MOBILE_VERSION`].includes(mobileVersion)) return false;
 
   return true;
 };
