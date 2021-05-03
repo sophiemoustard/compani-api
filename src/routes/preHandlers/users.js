e385c082ac9fd75238e3d7a43810ccbddf6e12c0
KennyCallegari <mel.kenny@gmail.com>
2021-05-03 12:32:30 +0200

    COM-2034 - Fix validation bug and allow same token for same user

--- src/routes/preHandlers/users.js
+++ src/routes/preHandlers/users.js
@@ -46,7 +46,7 @@ exports.authorizeUserUpdate = async (req) => {
   if (get(req, 'payload.establishment')) await checkEstablishment(userCompany, req.payload);
   if (get(req, 'payload.role')) await checkRole(userFromDB, req.payload);
   if (get(req, 'payload.customer')) await checkCustomer(userCompany, req.payload);
-  if (get(req, 'payload.formationExpoToken')) await checkExpoToken(req.payload);
+  if (get(req, 'payload.formationExpoToken')) await checkExpoToken(req);
   if (!isLoggedUserVendor && (!loggedUserClientRole || loggedUserClientRole === AUXILIARY_WITHOUT_COMPANY)) {
     checkUpdateRestrictions(req.payload);
   }
@@ -104,9 +104,15 @@ const checkCustomer = async (userCompany, payload) => {
   if (!customerCount) throw Boom.forbidden();
 };
 
-const checkExpoToken = async (payload) => {
-  const expoTokenAlreadyExists = await User.countDocuments({ formationExpoTokenList: payload.formationExpoToken });
+const checkExpoToken = async (req) => {
+  const { params, payload } = req;
+  const expoTokenAlreadyExists = await User.countDocuments({
+    _id: { $ne: params._id },
+    formationExpoTokenList: payload.formationExpoToken,
+  });
   if (expoTokenAlreadyExists) throw Boom.forbidden();
+
+  return null;
 };
 
 const checkUpdateRestrictions = (payload) => {
