537100b7f0a3c99edc05593f0a571840c8c68351
KennyCallegari <mel.kenny@gmail.com>
2021-05-05 16:37:22 +0200

    COM-2034 - Send notification when trainee is added

--- src/helpers/notifications.js
+++ src/helpers/notifications.js
@@ -1,11 +1,6 @@
 const ExpoNotification = require('../models/ExpoNotification');
 
 exports.sendNotificationToUser = (payload) => {
-  const expoPayload = {
-    to: 'ExponentPushToken[_UgI-UCZhBhcmvII74H3U2]',
-    title: payload.title,
-    body: payload.body,
-    data: payload.data,
-  };
+  const expoPayload = { to: payload.expoToken, title: payload.title, body: payload.body, data: payload.data };
   ExpoNotification.sendNotificationToUser(expoPayload);
 };
