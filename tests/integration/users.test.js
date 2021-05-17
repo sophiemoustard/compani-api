2dbd437eabff7e0cc056437358a228cb69940ec7
KennyCallegari <mel.kenny@gmail.com>
2021-05-17 10:37:06 +0200

    COM-2078 - Remove 403 when 2 users have same expotoken

--- tests/integration/users.test.js
+++ tests/integration/users.test.js
@@ -1269,17 +1269,6 @@ describe('PUT /users/:id/', () => {
       expect(response.statusCode).toBe(400);
     });
 
-    it('should return a 200 but not update if formationExpoToken already exists on user', async () => {
-      const response = await app.inject({
-        method: 'PUT',
-        url: `/users/${usersSeedList[0]._id}`,
-        payload: { formationExpoToken: 'ExponentPushToken[jeSuisUnIdExpo]' },
-        headers: { Cookie: `alenvi_token=${authToken}` },
-      });
-
-      expect(response.statusCode).toBe(200);
-    });
-
     it('should return a 400 if formationExpoToken has wrong type', async () => {
       const response = await app.inject({
         method: 'PUT',
@@ -1289,17 +1278,6 @@ describe('PUT /users/:id/', () => {
       });
 
       expect(response.statusCode).toBe(400);
-    });
-
-    it('should return a 403 if formationExpoToken already exists on another user', async () => {
-      const response = await app.inject({
-        method: 'PUT',
-        url: `/users/${usersSeedList[0]._id}`,
-        payload: { formationExpoToken: 'ExponentPushToken[jeSuisUnAutreIdExpo]' },
-        headers: { Cookie: `alenvi_token=${authToken}` },
-      });
-
-      expect(response.statusCode).toBe(403);
     });
   });
 
