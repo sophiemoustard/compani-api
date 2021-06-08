f68c778023ecfa23da549b6eb55f920e1c55e36b
Chloe Molle <chloe.molle@neuf.fr>
2021-06-08 17:08:28 +0200

    findone usercompany only on user

--- src/helpers/userCompanies.js
+++ src/helpers/userCompanies.js
@@ -3,7 +3,7 @@ const UserCompany = require('../models/UserCompany');
 const UtilsHelper = require('./utils');
 
 exports.create = async (user, company) => {
-  const userCompany = await UserCompany.findOne({ user, company }).lean();
+  const userCompany = await UserCompany.findOne({ user }, { company: 1 }).lean();
 
   if (!userCompany) await UserCompany.create({ user, company });
   else if (!UtilsHelper.areObjectIdsEquals(userCompany.company, company)) throw Boom.conflict();
