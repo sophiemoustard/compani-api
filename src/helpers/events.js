3a881d6545c931e9b2f14a67ad357b348ff3fef3
Chloe Molle <chloe.molle@neuf.fr>
2021-06-09 14:24:06 +0200

    Revert "COM-2075 - refacto get events groupBy"
    This reverts commit b7384366d4db82845ce11d285c64cb39938dba88.

--- src/helpers/events.js
+++ src/helpers/events.js
@@ -43,8 +43,11 @@ exports.list = async (query, credentials) => {
   const eventsQuery = exports.getListQuery(query, credentials);
   const { groupBy } = query;
 
-  if (groupBy === CUSTOMER) return EventRepository.getEventsGroupedByCustomers(eventsQuery, companyId);
-  if (groupBy === AUXILIARY) return EventRepository.getEventsGroupedByAuxiliaries(eventsQuery, companyId);
+  if (groupBy === CUSTOMER) {
+    return EventRepository.getEventsGroupedByCustomers(eventsQuery, get(credentials, 'company._id', null));
+  } if (groupBy === AUXILIARY) {
+    return EventRepository.getEventsGroupedByAuxiliaries(eventsQuery, get(credentials, 'company._id', null));
+  }
 
   return exports.populateEvents(await EventRepository.getEventList(eventsQuery, companyId));
 };
