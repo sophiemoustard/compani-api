6582cdb87127de42d9066a13b260f4d91bfd9a6a
ulysse <ulysse@alenvi.io>
2021-06-23 15:11:33 +0200

    COM-2175: minor changes

--- src/models/CustomerNote.js
+++ src/models/CustomerNote.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+const { validateQuery } = require('./preHooks/validate');
 
-const CustomerNote = mongoose.Schema({
+const CustomerNoteSchema = mongoose.Schema({
   customer: { type: mongoose.Schema.Types.ObjectId, immutable: true, required: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
   company: { type: mongoose.Schema.Types.ObjectId, immutable: true, required: true },
 }, { timestamps: true });
 
-module.exports = mongoose.model('CustomerNote', CustomerNote);
+CustomerNoteSchema.pre('find', validateQuery);
+
+module.exports = mongoose.model('CustomerNote', CustomerNoteSchema);
