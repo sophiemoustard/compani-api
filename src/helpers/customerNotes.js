1fefdddeae77c33f479819edb7f8c46223ee780f
ulysse <ulysse@alenvi.io>
2021-06-24 11:21:30 +0200

    COM-2175: sophie's review

--- src/helpers/customerNotes.js
+++ src/helpers/customerNotes.js
@@ -3,10 +3,6 @@ const CustomerNote = require('../models/CustomerNote');
 exports.createCustomerNote = async (payload, credentials) => CustomerNote
   .create({ ...payload, company: credentials.company._id });
 
-exports.list = async (customer, credentials) => {
-  const customerNotes = await CustomerNote.find({ customer, company: credentials.company._id })
-    .sort({ updatedAt: -1 })
-    .lean();
-
-  return customerNotes;
-};
+exports.list = async (customer, credentials) => CustomerNote.find({ customer, company: credentials.company._id })
+  .sort({ updatedAt: -1 })
+  .lean();
