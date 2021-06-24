e0ad0cde5daa1877282131bb4e73aa8b82d05b95
Chloe Molle <chloe.molle@neuf.fr>
2021-06-24 18:16:02 +0200

    COM-2150 minor changes + tests

---
+++ tests/unit/data/courseConvocation.test.js
@@ -0,0 +1,251 @@
+const sinon = require('sinon');
+const expect = require('expect');
+const { ObjectID } = require('mongodb');
+const FileHelper = require('../../../src/helpers/file');
+const CourseConvocation = require('../../../src/data/pdf/courseConvocation');
+
+describe('getPdfContent', () => {
+  let downloadImages;
+
+  beforeEach(() => {
+    downloadImages = sinon.stub(FileHelper, 'downloadImages');
+  });
+
+  afterEach(() => {
+    downloadImages.restore();
+  });
+
+  it('it should format and return pdf content', async () => {
+    const paths = [
+      'src/data/pdf/tmp/aux-pouce.png',
+      'src/data/pdf/tmp/doct-explication.png',
+      'src/data/pdf/tmp/doct-explication.png',
+      'src/data/pdf/tmp/aux-perplexite.png',
+    ];
+    downloadImages.returns(paths);
+
+    const data = {
+      misc: 'groupe 3',
+      subProgram: { program: { name: 'test', description: 'on va apprendre' } },
+      slots: [
+        { date: '23/12/2020', hours: '12h - 14h', address: '' },
+        { date: '14/01/2020', hours: '12h - 14h', address: '24 avenue du test' },
+      ],
+      slotsToPlan: [{ _id: new ObjectID() }],
+      trainer: { identity: { firstname: 'test', lastname: 'ok' }, biography: 'Voici ma bio' },
+      contact: { phone: '0987654321', email: 'test@test.fr' },
+    };
+
+    const result = await CourseConvocation.getPdfContent(data);
+
+    const header = [
+      {
+        columns: [
+          { image: 'src/data/pdf/tmp/aux-pouce.png', width: 64, style: 'img' },
+          [
+            { text: 'Vous êtes convoqué(e) à la formation', style: 'surtitle' },
+            { text: 'test - groupe 3', style: 'title' },
+            { canvas: [{ type: 'line', x1: 20, y1: 10, x2: 400, y2: 10, lineWidth: 1, lineColor: '#E2ECF0' }] },
+          ],
+        ],
+      },
+    ];
+    const table = [
+      {
+        table: {
+          body: [
+            [
+              { text: 'Dates', style: 'tableHeader' },
+              { text: 'Heures', style: 'tableHeader' },
+              { text: 'Lieux', style: 'tableHeader' },
+            ],
+            [
+              { text: '23/12/2020', style: 'tableContent' },
+              { text: '12h - 14h', style: 'tableContent' },
+              { text: '', style: 'tableContent' },
+            ],
+            [
+              { text: '14/01/2020', style: 'tableContent' },
+              { text: '12h - 14h', style: 'tableContent' },
+              { text: '24 avenue du test', style: 'tableContent' },
+            ],
+          ],
+          height: 24,
+          widths: ['auto', '*', '*'],
+        },
+        layout: { vLineWidth: () => 0, hLineWidth: () => 1, hLineColor: () => '#E2ECF0' },
+        marginTop: 24,
+      },
+      { text: 'Il reste 1 créneau(x) à planifier.', style: 'notes' },
+    ];
+    const programInfo = {
+      columns: [
+        { image: 'src/data/pdf/tmp/doct-explication.png', width: 64, style: 'img' },
+        [{ text: 'Programme de la formation', style: 'infoTitle' }, { text: 'on va apprendre', style: 'infoContent' }],
+      ],
+      marginTop: 24,
+    };
+    const trainerAndContactInfo = {
+      columns: [
+        {
+          columns: [
+            { image: 'src/data/pdf/tmp/doct-explication.png', width: 64, style: 'img' },
+            [
+              { text: 'Intervenant(e)', style: 'infoTitle' },
+              { text: 'test OK', style: 'infoSubTitle' },
+              { text: 'Voici ma bio', style: 'infoContent' },
+            ],
+          ],
+        },
+        {
+          columns: [
+            { image: 'src/data/pdf/tmp/aux-perplexite.png', width: 64, style: 'img' },
+            [
+              { text: 'Votre contact pour la formation', style: 'infoTitle' },
+              { text: '09 87 65 43 21', style: 'infoSubTitle' },
+              { text: 'test@test.fr', style: 'infoSubTitle' },
+            ],
+          ],
+        },
+      ],
+      marginTop: 24,
+    };
+    const pdf = {
+      content: [header, table, programInfo, trainerAndContactInfo].flat(),
+      defaultStyle: { font: 'SourceSans', fontSize: 10 },
+      styles: {
+        title: { fontSize: 20, bold: true, alignment: 'left', color: '#1D7C8F', marginLeft: 24 },
+        surtitle: { fontSize: 12, bold: true, alignment: 'left', marginTop: 24, marginLeft: 24 },
+        tableHeader: { fontSize: 12, bold: true, alignment: 'center', marginTop: 4, marginBottom: 4 },
+        tableContent: { fontSize: 12, alignment: 'center', marginTop: 4, marginBottom: 4 },
+        notes: { italics: true, alignment: 'left', marginTop: 4 },
+        infoTitle: { fontSize: 14, bold: true, marginLeft: 12 },
+        infoSubTitle: { fontSize: 12, marginLeft: 12 },
+        infoContent: { fontSize: 10, italics: true, marginLeft: 12 },
+      },
+    };
+    expect(JSON.stringify(result)).toEqual(JSON.stringify(pdf));
+    const imageList = [
+      { url: 'https://storage.googleapis.com/compani-main/aux-pouce.png', name: 'aux-pouce.png' },
+      { url: 'https://storage.googleapis.com/compani-main/doct-explication.png', name: 'doct-explication.png' },
+      { url: 'https://storage.googleapis.com/compani-main/doct-quizz.png', name: 'doct-quizz.png' },
+      { url: 'https://storage.googleapis.com/compani-main/aux-perplexite.png', name: 'aux-perplexite.png' },
+    ];
+    sinon.assert.calledOnceWithExactly(downloadImages, imageList);
+  });
+
+  it('it should format and return pdf content with less infos', async () => {
+    const paths = [
+      'src/data/pdf/tmp/aux-pouce.png',
+      'src/data/pdf/tmp/doct-explication.png',
+      'src/data/pdf/tmp/doct-explication.png',
+      'src/data/pdf/tmp/aux-perplexite.png',
+    ];
+    downloadImages.returns(paths);
+
+    const data = {
+      subProgram: { program: { name: 'test' } },
+      slots: [
+        { date: '23/12/2020', hours: '12h - 14h', address: '' },
+        { date: '14/01/2020', hours: '12h - 14h', address: '24 avenue du test' },
+      ],
+      contact: { phone: '0987654321' },
+    };
+
+    const result = await CourseConvocation.getPdfContent(data);
+
+    const header = [
+      {
+        columns: [
+          { image: 'src/data/pdf/tmp/aux-pouce.png', width: 64, style: 'img' },
+          [
+            { text: 'Vous êtes convoqué(e) à la formation', style: 'surtitle' },
+            { text: 'test', style: 'title' },
+            { canvas: [{ type: 'line', x1: 20, y1: 10, x2: 400, y2: 10, lineWidth: 1, lineColor: '#E2ECF0' }] },
+          ],
+        ],
+      },
+    ];
+    const table = [
+      {
+        table: {
+          body: [
+            [
+              { text: 'Dates', style: 'tableHeader' },
+              { text: 'Heures', style: 'tableHeader' },
+              { text: 'Lieux', style: 'tableHeader' },
+            ],
+            [
+              { text: '23/12/2020', style: 'tableContent' },
+              { text: '12h - 14h', style: 'tableContent' },
+              { text: '', style: 'tableContent' },
+            ],
+            [
+              { text: '14/01/2020', style: 'tableContent' },
+              { text: '12h - 14h', style: 'tableContent' },
+              { text: '24 avenue du test', style: 'tableContent' },
+            ],
+          ],
+          height: 24,
+          widths: ['auto', '*', '*'],
+        },
+        layout: { vLineWidth: () => 0, hLineWidth: () => 1, hLineColor: () => '#E2ECF0' },
+        marginTop: 24,
+      },
+    ];
+    const programInfo = {
+      columns: [
+        { image: 'src/data/pdf/tmp/doct-explication.png', width: 64, style: 'img' },
+        [{ text: 'Programme de la formation', style: 'infoTitle' }, { text: '', style: 'infoContent' }],
+      ],
+      marginTop: 24,
+    };
+    const trainerAndContactInfo = {
+      columns: [
+        {
+          columns: [
+            { image: 'src/data/pdf/tmp/doct-explication.png', width: 64, style: 'img' },
+            [
+              { text: 'Intervenant(e)', style: 'infoTitle' },
+              { text: '', style: 'infoSubTitle' },
+              { text: '', style: 'infoContent' },
+            ],
+          ],
+        },
+        {
+          columns: [
+            { image: 'src/data/pdf/tmp/aux-perplexite.png', width: 64, style: 'img' },
+            [
+              { text: 'Votre contact pour la formation', style: 'infoTitle' },
+              { text: '09 87 65 43 21', style: 'infoSubTitle' },
+              { text: '', style: 'infoSubTitle' },
+            ],
+          ],
+        },
+      ],
+      marginTop: 24,
+    };
+    const pdf = {
+      content: [header, table, programInfo, trainerAndContactInfo].flat(),
+      defaultStyle: { font: 'SourceSans', fontSize: 10 },
+      styles: {
+        title: { fontSize: 20, bold: true, alignment: 'left', color: '#1D7C8F', marginLeft: 24 },
+        surtitle: { fontSize: 12, bold: true, alignment: 'left', marginTop: 24, marginLeft: 24 },
+        tableHeader: { fontSize: 12, bold: true, alignment: 'center', marginTop: 4, marginBottom: 4 },
+        tableContent: { fontSize: 12, alignment: 'center', marginTop: 4, marginBottom: 4 },
+        notes: { italics: true, alignment: 'left', marginTop: 4 },
+        infoTitle: { fontSize: 14, bold: true, marginLeft: 12 },
+        infoSubTitle: { fontSize: 12, marginLeft: 12 },
+        infoContent: { fontSize: 10, italics: true, marginLeft: 12 },
+      },
+    };
+    expect(JSON.stringify(result)).toEqual(JSON.stringify(pdf));
+    const imageList = [
+      { url: 'https://storage.googleapis.com/compani-main/aux-pouce.png', name: 'aux-pouce.png' },
+      { url: 'https://storage.googleapis.com/compani-main/doct-explication.png', name: 'doct-explication.png' },
+      { url: 'https://storage.googleapis.com/compani-main/doct-quizz.png', name: 'doct-quizz.png' },
+      { url: 'https://storage.googleapis.com/compani-main/aux-perplexite.png', name: 'aux-perplexite.png' },
+    ];
+    sinon.assert.calledOnceWithExactly(downloadImages, imageList);
+  });
+});
