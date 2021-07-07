12e8fe455b8845a748bae9b2ab14a13e9d7ab13d
Sophie Moustard <sophie.moustard@gmail.com>
2021-07-07 16:17:49 +0200

    COM-2180 update customer partners test

--- tests/integration/seed/customerPartnersSeed.js
+++ tests/integration/seed/customerPartnersSeed.js
@@ -1,10 +1,12 @@
 const { ObjectID } = require('mongodb');
+const { v4: uuidv4 } = require('uuid');
 const Customer = require('../../../src/models/Customer');
 const Partner = require('../../../src/models/Partner');
 const User = require('../../../src/models/User');
+const UserCompany = require('../../../src/models/UserCompany');
 const CustomerPartner = require('../../../src/models/CustomerPartner');
-const { populateDBForAuthentication, authCompany, otherCompany } = require('./authenticationSeed');
-const { auxiliaryFromOtherCompany } = require('./usersSeed');
+const { populateDBForAuthentication, authCompany, otherCompany, rolesList } = require('./authenticationSeed');
+const { WEBAPP } = require('../../../src/helpers/constants');
 
 const customersList = [
   {
@@ -36,6 +38,7 @@ const customersList = [
     },
   },
 ];
+
 const partnersList = [
   {
     _id: new ObjectID(),
@@ -58,28 +61,32 @@ const partnersList = [
 ];
 
 const customerPartnersList = [
-  {
-    _id: new ObjectID(),
-    partner: partnersList[1]._id,
-    customer: customersList[1],
-    company: otherCompany._id,
-  },
-  {
-    _id: new ObjectID(),
-    partner: partnersList[2]._id,
-    customer: customersList[0],
-    company: authCompany._id,
-  },
+  { _id: new ObjectID(), partner: partnersList[1]._id, customer: customersList[1], company: otherCompany._id },
+  { _id: new ObjectID(), partner: partnersList[2]._id, customer: customersList[0], company: authCompany._id },
 ];
 
+const auxiliaryFromOtherCompany = {
+  _id: new ObjectID(),
+  identity: { firstname: 'Philou', lastname: 'toto' },
+  local: { email: 'othercompanyauxiliary@alenvi.io', password: '123456!eR' },
+  role: { client: rolesList.find(role => role.name === 'auxiliary')._id },
+  refreshToken: uuidv4(),
+  inactivityDate: null,
+  origin: WEBAPP,
+};
+
+const userCompanies = [{ _id: new ObjectID(), user: auxiliaryFromOtherCompany._id, company: otherCompany._id }];
+
 const populateDB = async () => {
-  await User.deleteMany({});
-  await Customer.deleteMany({});
-  await Partner.deleteMany({});
-  await CustomerPartner.deleteMany({});
+  await User.deleteMany();
+  await Customer.deleteMany();
+  await Partner.deleteMany();
+  await CustomerPartner.deleteMany();
+  await UserCompany.deleteMany();
 
   await populateDBForAuthentication();
   await User.create(auxiliaryFromOtherCompany);
+  await UserCompany.insertMany(userCompanies);
   await Customer.insertMany(customersList);
   await Partner.insertMany(partnersList);
   await CustomerPartner.insertMany(customerPartnersList);
