87a8fbff8d54a992a06d7232afbe13b514e4ed1c
CorentinMartinot <corentin.martinot@gmail.com>
2021-09-09 12:21:07 +0200

    COM-2293 export event with kmDuringEven column

--- tests/unit/helpers/historyExport.test.js
+++ tests/unit/helpers/historyExport.test.js
@@ -182,6 +182,7 @@ describe('exportWorkingEventsHistory', () => {
     'Motif',
     'Durée',
     'Répétition',
+    'Déplacement véhiculé avec bénéficiaire',
     'Mode de transport spécifique',
     'Équipe',
     'Id Auxiliaire',
@@ -213,6 +214,7 @@ describe('exportWorkingEventsHistory', () => {
   const events = [
     {
       transportMode: 'public',
+      kmDuringEvent: 667,
       isCancelled: false,
       isBilled: true,
       type: 'intervention',
@@ -238,6 +240,7 @@ describe('exportWorkingEventsHistory', () => {
       ],
     },
     {
+      kmDuringEvent: 0,
       isCancelled: false,
       isBilled: true,
       type: 'intervention',
@@ -271,6 +274,7 @@ describe('exportWorkingEventsHistory', () => {
     },
     {
       transportMode: 'company_transport',
+      kmDuringEvent: 4124,
       isCancelled: true,
       cancel: { condition: 'invoiced_and_not_paid', reason: 'auxiliary_initiative' },
       isBilled: false,
@@ -334,15 +338,15 @@ describe('exportWorkingEventsHistory', () => {
     expect(exportArray).toEqual([
       header,
       ['Intervention', '', 'Lala', '20/05/2019 10:00:00', '20/05/2019 10:01:18', 'Manuel', 'QR Code manquant',
-        '20/05/2019 12:00:00', '', '', '', '2,00', 'Une fois par semaine', 'Transports en commun / À pied',
+        '20/05/2019 12:00:00', '', '', '', '2,00', 'Une fois par semaine', '667', 'Transports en commun / À pied',
         'Girafes - 75', expect.any(ObjectID), '', 'Jean-Claude', 'VAN DAMME', 'Non', expect.any(ObjectID), 'Mme',
         'MATHY', 'Mimi', '', 'Oui', 'Non', '', ''],
       ['Intervention', '', 'Lala', '20/05/2019 10:00:00', '20/05/2019 10:01:18', 'Manuel', 'QR Code manquant',
         '20/05/2019 12:00:00', '20/05/2019 12:03:24', 'Manuel', 'Problème de caméra', '2,00', 'Une fois par semaine',
-        '', 'Girafes - 75', '', '', '', '', 'Oui', expect.any(ObjectID), 'Mme', 'MATHY', 'Mimi', '',
+        '', '', 'Girafes - 75', '', '', '', '', 'Oui', expect.any(ObjectID), 'Mme', 'MATHY', 'Mimi', '',
         'Oui', 'Non', '', ''],
       ['Heure interne', 'Formation', '', '20/05/2019 10:00:00', '', '', '', '20/05/2019 12:00:00', '', '', '',
-        '2,00', '', 'Véhicule d\'entreprise', 'Etoiles - 75', '', '', '', '', 'Oui', expect.any(ObjectID), 'M.',
+        '2,00', '', '4124', 'Véhicule d\'entreprise', 'Etoiles - 75', '', '', '', '', 'Oui', expect.any(ObjectID), 'M.',
         'HORSEMAN', 'Bojack', 'brbr', 'Non', 'Oui', 'Facturée & non payée', 'Initiative de l\'intervenant(e)'],
     ]);
   });
