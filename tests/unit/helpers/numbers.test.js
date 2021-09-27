9411a54bbbd57959bc6b70e2e26b19a78372455f
Manon Palin <manon@compani.fr>
2021-09-27 16:24:12 +0200

    COM-2332: add unit tests

---
+++ tests/unit/helpers/numbers.test.js
@@ -0,0 +1,46 @@
+const expect = require('expect');
+const NumberHelper = require('../../../src/helpers/numbers');
+
+describe('multiply', () => {
+  it('should multiply numbers', async () => {
+    const a = 0.3;
+    const b = 0.2;
+
+    const result = NumberHelper.multiply(a, b);
+
+    expect(result).toEqual(0.06);
+  });
+});
+
+describe('dividedBy', () => {
+  it('should divid numbers', async () => {
+    const a = 0.3;
+    const b = 0.2;
+
+    const result = NumberHelper.dividedBy(a, b);
+
+    expect(result).toEqual(1.5);
+  });
+});
+
+describe('add', () => {
+  it('should add numbers', async () => {
+    const a = 0.6;
+    const b = 1.2;
+
+    const result = NumberHelper.add(a, b);
+
+    expect(result).toEqual(1.8);
+  });
+});
+
+describe('subsdtract', () => {
+  it('should substract numbers', async () => {
+    const a = 0.7;
+    const b = 0.2;
+
+    const result = NumberHelper.subsctract(a, b);
+
+    expect(result).toEqual(0.5);
+  });
+});
