1fa9c876f60f4a59628fc0ecabc32ffce4baeb98
Manon Palin <manon@compani.fr>
2021-09-27 12:35:38 +0200

    COM-2332: numbers helper

---
+++ src/helpers/numbers.js
@@ -0,0 +1,9 @@
+const { BigNumber } = require('bignumber.js');
+
+exports.multiply = (a, b) => Number(BigNumber(a).multipliedBy(b));
+
+exports.dividedBy = (a, b) => Number(BigNumber(a).dividedBy(b));
+
+exports.add = (a, b) => Number(BigNumber(a).plus(b));
+
+exports.subsctract = (a, b) => Number(BigNumber(a).minus(b));
