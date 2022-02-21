0be3eb9c40ddc2e3dde63088d9bc0fb5e4573912
CorentinMartinot <corentin.martinot@gmail.com>
2022-02-21 19:35:23 +0100

    COM-2674 mock in beforeEch and unmock in afterEach

--- tests/unit/helpers/dates/mockCurrentDate.test.js
+++ tests/unit/helpers/dates/mockCurrentDate.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const { _mockCurrentDate, _unmockCurrentDate, mockCurrentDateAndRunAsync } = require('../../mockCurrentDate');
+const { mockCurrentDate, unmockCurrentDate } = require('../../mockCurrentDate');
 const CompaniDatesHelper = require('../../../../src/helpers/dates/companiDates');
 
 describe('mockCurrentDate', () => {
@@ -9,52 +9,13 @@ describe('mockCurrentDate', () => {
     const expectedCurrentDate = '2021-12-20T07:00:00.000Z';
     const expectedCurrentDateInMillis = CompaniDatesHelper.CompaniDate(expectedCurrentDate)._getDate.toMillis();
 
-    _mockCurrentDate(expectedCurrentDate);
+    mockCurrentDate(expectedCurrentDate);
 
     const mockedCurrent = CompaniDatesHelper.CompaniDate()._getDate.toMillis();
     expect(mockedCurrent).toBeGreaterThanOrEqual(expectedCurrentDateInMillis);
     expect(mockedCurrent).toBeLessThan(expectedCurrentDateInMillis + 1000);
 
-    _unmockCurrentDate();
-
-    const unmockedCurrent = CompaniDatesHelper.CompaniDate()._getDate.toMillis();
-    expect(unmockedCurrent).toBeGreaterThanOrEqual(currentInMillis);
-    expect(unmockedCurrent).toBeLessThan(currentInMillis + 1000);
-  });
-
-  it('should embed a OK test execution with mocking and unmocking the current date', async () => {
-    const currentInMillis = CompaniDatesHelper.CompaniDate()._getDate.toMillis();
-
-    const expectedCurrentDate = '2021-12-20T07:00:00.000Z';
-    const testToRun = () => {
-      const mockedDate = CompaniDatesHelper.CompaniDate().toISO();
-      expect(mockedDate).toEqual(expectedCurrentDate);
-    };
-
-    await mockCurrentDateAndRunAsync(testToRun, expectedCurrentDate);
-    const unmockedCurrent = CompaniDatesHelper.CompaniDate()._getDate.toMillis();
-    expect(unmockedCurrent).toBeGreaterThanOrEqual(currentInMillis);
-    expect(unmockedCurrent).toBeLessThan(currentInMillis + 1000);
-  });
-
-  it('should embed a KO test execution with mocking and unmocking the current date', async () => {
-    const currentInMillis = CompaniDatesHelper.CompaniDate()._getDate.toMillis();
-
-    const expectedCurrentDate = '2021-12-20T07:00:00.000Z';
-    const testToRun = async () => {
-      const mockedDate = CompaniDatesHelper.CompaniDate().toISO();
-      expect(mockedDate).toEqual('shalom aleichem');
-    };
-
-    let errorMatcherResult;
-    try {
-      await mockCurrentDateAndRunAsync(testToRun, expectedCurrentDate);
-    } catch (e) {
-      errorMatcherResult = e.matcherResult;
-    }
-
-    expect(errorMatcherResult.actual).toEqual('2021-12-20T07:00:00.000Z');
-    expect(errorMatcherResult.expected).toEqual('shalom aleichem');
+    unmockCurrentDate();
 
     const unmockedCurrent = CompaniDatesHelper.CompaniDate()._getDate.toMillis();
     expect(unmockedCurrent).toBeGreaterThanOrEqual(currentInMillis);
--- tests/unit/mockCurrentDate.js
+++ tests/unit/mockCurrentDate.js
@@ -1,23 +1,14 @@
 const luxon = require('../../src/helpers/dates/luxon');
 
-const _mockCurrentDate = (mockedCurrentISO) => {
+const mockCurrentDate = (mockedCurrentISO) => {
   const luxonExpectedNow = luxon.DateTime.fromISO(mockedCurrentISO);
 
   luxon.Settings.now = () => luxonExpectedNow.toMillis();
 };
 
-const _unmockCurrentDate = () => {
+const unmockCurrentDate = () => {
   const currentDate = new Date();
   luxon.Settings.now = () => currentDate.getTime();
 };
 
-const mockCurrentDateAndRunAsync = async (runTest, mockedCurrentISO) => {
-  _mockCurrentDate(mockedCurrentISO);
-  try {
-    await runTest();
-  } finally {
-    _unmockCurrentDate();
-  }
-};
-
-module.exports = { _mockCurrentDate, _unmockCurrentDate, mockCurrentDateAndRunAsync };
+module.exports = { mockCurrentDate, unmockCurrentDate };
