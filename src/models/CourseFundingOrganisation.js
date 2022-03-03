d9801a0b963e61b78e5a43c83e30b1168b3dc24b
CorentinMartinot <corentin.martinot@gmail.com>
2022-03-03 12:47:48 +0100

    COM-2701 delete unique

--- src/models/CourseFundingOrganisation.js
+++ src/models/CourseFundingOrganisation.js
@@ -5,7 +5,7 @@ const addressSchemaDefinition = require('./schemaDefinitions/address');
 
 const CourseFundingOrganisationSchema = mongoose.Schema({
   // unique mongo index on 'name' (with case and diacritics insensitive collation) has been added manually in mep58
-  name: { type: String, unique: true, required: true },
+  name: { type: String, required: true },
   address: { type: mongoose.Schema(addressSchemaDefinition, { _id: false, id: false }), required: true },
 }, { timestamps: true });
 
