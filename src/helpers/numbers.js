cc98a1921386c098ac35090037e0a476f425c1ae
Manon Palin <manon@compani.fr>
2022-04-22 12:00:55 +0200

    COM-2807 - rename divide into oldDivide

--- src/helpers/numbers.js
+++ src/helpers/numbers.js
@@ -2,7 +2,7 @@ const { BigNumber } = require('bignumber.js');
 
 exports.oldMultiply = (...nums) => nums.reduce((acc, n) => BigNumber(acc).multipliedBy(n).toNumber(), 1);
 
-exports.divide = (a, b) => BigNumber(a).dividedBy(b).toNumber();
+exports.oldDivide = (a, b) => BigNumber(a).dividedBy(b).toNumber();
 
 exports.add = (...nums) => nums.reduce((acc, n) => BigNumber(acc).plus(n).toNumber(), 0);
 
