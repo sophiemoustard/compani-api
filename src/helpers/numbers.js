c2613cedc5cd9229d6aaf7c1971d0725f585276c
Manon Palin <manon@compani.fr>
2022-04-22 12:05:40 +0200

    COM-2807 - rename subtract into oldSubtract

--- src/helpers/numbers.js
+++ src/helpers/numbers.js
@@ -6,4 +6,4 @@ exports.oldDivide = (a, b) => BigNumber(a).dividedBy(b).toNumber();
 
 exports.oldAdd = (...nums) => nums.reduce((acc, n) => BigNumber(acc).plus(n).toNumber(), 0);
 
-exports.subtract = (a, b) => BigNumber(a).minus(b).toNumber();
+exports.oldSubtract = (a, b) => BigNumber(a).minus(b).toNumber();
