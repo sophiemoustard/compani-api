35b46a178d4e6e91a9f09663fb512858588fafb4
Sophie Moustard <sophiemoustard@sophies-macbook-pro.home>
2022-04-25 15:37:55 +0200

    COM-2805 - add methods

--- src/helpers/numbers.js
+++ src/helpers/numbers.js
@@ -1,9 +1,19 @@
 const { BigNumber } = require('bignumber.js');
 
+exports.toBN = a => BigNumber(a);
+
 exports.oldMultiply = (...nums) => nums.reduce((acc, n) => BigNumber(acc).multipliedBy(n).toNumber(), 1);
+
+exports.multiply = (...nums) => nums.reduce((acc, n) => BigNumber(acc).multipliedBy(n).toString(), 1);
 
 exports.oldDivide = (a, b) => BigNumber(a).dividedBy(b).toNumber();
 
+exports.divide = (a, b) => BigNumber(a).dividedBy(b).toString();
+
 exports.oldAdd = (...nums) => nums.reduce((acc, n) => BigNumber(acc).plus(n).toNumber(), 0);
 
+exports.add = (...nums) => nums.reduce((acc, n) => BigNumber(acc).plus(n).toString(), 0);
+
 exports.oldSubtract = (a, b) => BigNumber(a).minus(b).toNumber();
+
+exports.subtract = (a, b) => BigNumber(a).minus(b).toString();
