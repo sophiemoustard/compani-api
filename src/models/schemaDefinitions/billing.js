cf49519319001b470bfd85425ddc19bfa11ab55c
Manon Palin <manon@compani.fr>
2022-05-05 16:57:36 +0200

    COM-2829 - change billingItemList.unitInclTaxes from string to number

--- src/models/schemaDefinitions/billing.js
+++ src/models/schemaDefinitions/billing.js
@@ -9,7 +9,7 @@ const billEventSurchargesSchemaDefinition = [{
 
 const billingItemsDefinition = {
   billingItem: { type: mongoose.Schema.Types.ObjectId, ref: 'BillingItem', required: true },
-  unitInclTaxes: { type: String, required: true },
+  unitInclTaxes: { type: Number, required: true },
   name: { type: String, required: true },
   count: { type: Number, required: true },
   inclTaxes: { type: Number, required: true },
