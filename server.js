25afd35c7e5740f38904ca6f1757295044767ebb
Sophie Moustard <sophiemoustard@sophies-macbook-pro.home>
2022-05-06 09:39:22 +0200

    COM-2840 - add log on route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* eslint-disable no-console */
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const Boom = require('@hapi/boom');
@@ -7,9 +8,10 @@ const Boom = require('@hapi/boom');
 const { mongooseConnection } = require('./src/config/mongoose');
 const { routes } = require('./src/routes/index');
 const { plugins } = require('./src/plugins/index');
+const { DEVELOPMENT, TEST, PRODUCTION } = require('./src/helpers/constants');
 
 const server = Hapi.server({
-  port: process.env.NODE_ENV === 'test' ? 3001 : (process.env.PORT || 3000),
+  port: process.env.NODE_ENV === TEST ? 3001 : (process.env.PORT || 3000),
   routes: {
     log: {
       collect: true,
@@ -22,9 +24,7 @@ const server = Hapi.server({
     validate: {
       async failAction(request, h, err) {
         request.log('validationError', err.message);
-        if (process.env.NODE_ENV === 'production') {
-          throw Boom.badRequest();
-        }
+        if (process.env.NODE_ENV === PRODUCTION) throw Boom.badRequest();
         throw err;
       },
     },
@@ -34,23 +34,50 @@ const server = Hapi.server({
 const init = async () => {
   await server.register([...plugins]);
   await server.register([...routes]);
-  server.state('alenvi_token', {
-    isHttpOnly: true,
-    ttl: 24 * 60 * 60 * 1000,
-    isSecure: !['test', 'development'].includes(process.env.NODE_ENV),
-    isSameSite: 'Strict',
-    path: '/',
-    domain: process.env.TOKEN_DOMAIN,
-  });
 
-  server.state('refresh_token', {
-    isHttpOnly: true,
-    ttl: 365 * 24 * 60 * 60 * 1000,
-    isSecure: !['test', 'development'].includes(process.env.NODE_ENV),
-    isSameSite: 'Strict',
-    path: '/',
-    domain: process.env.TOKEN_DOMAIN,
-  });
+  const isDevelopment = process.env.NODE_ENV === DEVELOPMENT;
+  if (isDevelopment) {
+    server.ext('onPostStart', () => { console.log('memoryLog - onPostStart', JSON.stringify(process.memoryUsage())); });
+    server.ext(
+      'onPostResponse',
+      (req, h) => {
+        console.log('memoryLog - onPostResponse', req.path, JSON.stringify(process.memoryUsage()));
+        return h.continue;
+      }
+    );
+  }
+
+  server.ext(
+    'onRequest',
+    (req, h) => {
+      console.log('memoryLog - onRequest', req.path, isDevelopment ? JSON.stringify(process.memoryUsage()) : '');
+      return h.continue;
+    }
+  );
+
+  server.state(
+    'alenvi_token',
+    {
+      isHttpOnly: true,
+      ttl: 24 * 60 * 60 * 1000,
+      isSecure: !['test', 'development'].includes(process.env.NODE_ENV),
+      isSameSite: 'Strict',
+      path: '/',
+      domain: process.env.TOKEN_DOMAIN,
+    }
+  );
+
+  server.state(
+    'refresh_token',
+    {
+      isHttpOnly: true,
+      ttl: 365 * 24 * 60 * 60 * 1000,
+      isSecure: !['test', 'development'].includes(process.env.NODE_ENV),
+      isSameSite: 'Strict',
+      path: '/',
+      domain: process.env.TOKEN_DOMAIN,
+    }
+  );
 
   await mongooseConnection(server);
 
@@ -60,9 +87,9 @@ const init = async () => {
 
 module.exports = server;
 
-process.on('unhandledRejection', (err) => {
-  console.error(err);
-  process.exit(1);
-});
+process.on(
+  'unhandledRejection',
+  (err) => { console.error(err); process.exit(1); }
+);
 
 init();
