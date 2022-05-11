479ab7c031527a75ce25d970c43ced7775607530
Manon Palin <manon@compani.fr>
2022-05-11 15:17:52 +0200

    COM-2809 - fix eslint error

--- src/helpers/draftBills.js
+++ src/helpers/draftBills.js
@@ -1,5 +1,4 @@
 const get = require('lodash/get');
-const has = require('lodash/has');
 const pick = require('lodash/pick');
 const { ObjectId } = require('mongodb');
 const moment = require('../extensions/moment');
