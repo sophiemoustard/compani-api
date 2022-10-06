4e69978a244a591d34b93b5b4bf125e9c85b2151
Palin Manon <manon@compani.fr>
2022-10-06 15:05:11 +0200

    COM-3209 - fix unit tests after rebasing feature-refacto-course-company-link on dev

--- tests/unit/helpers/courses.test.js
+++ tests/unit/helpers/courses.test.js
@@ -1,6 +1,7 @@
 const sinon = require('sinon');
 const omit = require('lodash/omit');
 const expect = require('expect');
+const omit = require('lodash/omit');
 const { ObjectId } = require('mongodb');
 const fs = require('fs');
 const os = require('os');
