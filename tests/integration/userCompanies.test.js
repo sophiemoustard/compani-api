82f38b4fb31657df7a4aa8b201f1689265643d63
Palin Manon <manon@compani.fr>
2022-12-20 17:15:36 +0100

    COM-3335 - clarify tests

--- tests/integration/userCompanies.test.js
+++ tests/integration/userCompanies.test.js
@@ -20,7 +20,7 @@ describe('USER COMPANIES ROUTES - PUT /usercompanies/{id}', () => {
       authToken = await getTokenByCredentials(usersSeedList[1].local);
     });
 
-    it('should detach user Company', async () => {
+    it('should detach user company', async () => {
       const userCompanyId = userCompanies[0]._id.toHexString();
       const payload = { endDate: '2022-12-01T22:59:59.999Z' };
 
@@ -51,21 +51,7 @@ describe('USER COMPANIES ROUTES - PUT /usercompanies/{id}', () => {
       expect(res.statusCode).toBe(400);
     });
 
-    it('should return a 403 if user has a role', async () => {
-      const userCompanyId = userCompanies[7]._id;
-      const payload = { endDate: '2022-12-25T22:59:59.999Z' };
-
-      const res = await app.inject({
-        method: 'PUT',
-        url: `/usercompanies/${userCompanyId}`,
-        payload,
-        headers: { Cookie: `alenvi_token=${authToken}` },
-      });
-
-      expect(res.statusCode).toBe(403);
-    });
-
-    it('should return a 403 if user Company doesnt exist', async () => {
+    it('should return a 403 if user company doesnt exist', async () => {
       const userCompanyId = new ObjectId();
       const payload = { endDate: '2022-12-25T22:59:59.999Z' };
 
@@ -77,9 +63,25 @@ describe('USER COMPANIES ROUTES - PUT /usercompanies/{id}', () => {
       });
 
       expect(res.statusCode).toBe(403);
+      expect(res.result.message).toBe('Error while checking user company: userCompany not found.');
     });
 
-    it('should return a 403 if user is from an other company', async () => {
+    it('should return a 403 if user company startDate is in futur', async () => {
+      const userCompanyId = userCompanies[6]._id.toHexString();
+      const payload = { endDate: '2022-08-17T10:00:00.000Z' };
+
+      const res = await app.inject({
+        method: 'PUT',
+        url: `/usercompanies/${userCompanyId}`,
+        payload,
+        headers: { Cookie: `alenvi_token=${authToken}` },
+      });
+
+      expect(res.statusCode).toBe(403);
+      expect(res.result.message).toBe('Error while checking user company: userCompany not found.');
+    });
+
+    it('should return 403 if company is not allowed to detach its learners', async () => {
       const userCompanyId = userCompanies[2]._id.toHexString();
       const payload = { endDate: '2022-12-25T22:59:59.999Z' };
 
@@ -91,6 +93,22 @@ describe('USER COMPANIES ROUTES - PUT /usercompanies/{id}', () => {
       });
 
       expect(res.statusCode).toBe(403);
+      expect(res.result.message).toBe('Error while checking user company: userCompany not found.');
+    });
+
+    it('should return 403 if user is already detached', async () => {
+      const userCompanyId = userCompanies[8]._id.toHexString();
+      const payload = { endDate: '2022-12-25T22:59:59.999Z' };
+
+      const res = await app.inject({
+        method: 'PUT',
+        url: `/usercompanies/${userCompanyId}`,
+        payload,
+        headers: { Cookie: `alenvi_token=${authToken}` },
+      });
+
+      expect(res.statusCode).toBe(403);
+      expect(res.result.message).toBe('Error while checking user company: userCompany not found.');
     });
 
     it('should return a 403 if user doesnt exist', async () => {
@@ -105,6 +123,22 @@ describe('USER COMPANIES ROUTES - PUT /usercompanies/{id}', () => {
       });
 
       expect(res.statusCode).toBe(403);
+      expect(res.result.message).toBe('Error while checking user: user not found.');
+    });
+
+    it('should return a 403 if user has a role', async () => {
+      const userCompanyId = userCompanies[7]._id;
+      const payload = { endDate: '2022-12-25T22:59:59.999Z' };
+
+      const res = await app.inject({
+        method: 'PUT',
+        url: `/usercompanies/${userCompanyId}`,
+        payload,
+        headers: { Cookie: `alenvi_token=${authToken}` },
+      });
+
+      expect(res.statusCode).toBe(403);
+      expect(res.result.message).toBe('Error while checking user: user not found.');
     });
 
     it('should return a 403 if detachment date is before user company startdate', async () => {
@@ -120,20 +154,6 @@ describe('USER COMPANIES ROUTES - PUT /usercompanies/{id}', () => {
 
       expect(res.statusCode).toBe(403);
       expect(res.result.message).toBe('Impossible: la date de fin doit être postérieure à la date de début.');
-    });
-
-    it('should return a 403 if user company startDate is in futur', async () => {
-      const userCompanyId = userCompanies[6]._id.toHexString();
-      const payload = { endDate: '2022-08-17T10:00:00.000Z' };
-
-      const res = await app.inject({
-        method: 'PUT',
-        url: `/usercompanies/${userCompanyId}`,
-        payload,
-        headers: { Cookie: `alenvi_token=${authToken}` },
-      });
-
-      expect(res.statusCode).toBe(403);
     });
 
     it('should return a 403 if detachment date is before first trainee\'s addition in course history', async () => {
@@ -152,12 +172,12 @@ describe('USER COMPANIES ROUTES - PUT /usercompanies/{id}', () => {
     });
   });
 
-  describe('TRAINING_ORGANISATION_MANAGER', () => {
+  describe('TRAINING_ORGANISATION_MANAGER #tag', () => {
     beforeEach(async () => {
       authToken = await getTokenByCredentials(usersSeedList[3].local);
     });
 
-    it('should detach user Company', async () => {
+    it('should detach user company', async () => {
       const userCompanyId = userCompanies[5]._id.toHexString();
       const payload = { endDate: '2022-12-01T22:59:59.999Z' };
 
@@ -188,21 +208,7 @@ describe('USER COMPANIES ROUTES - PUT /usercompanies/{id}', () => {
       expect(res.statusCode).toBe(400);
     });
 
-    it('should return a 403 if user has a role', async () => {
-      const userCompanyId = userCompanies[7]._id;
-      const payload = { endDate: '2022-12-25T22:59:59.999Z' };
-
-      const res = await app.inject({
-        method: 'PUT',
-        url: `/usercompanies/${userCompanyId}`,
-        payload,
-        headers: { Cookie: `alenvi_token=${authToken}` },
-      });
-
-      expect(res.statusCode).toBe(403);
-    });
-
-    it('should return a 403 if user Company doesnt exist', async () => {
+    it('should return a 403 if user company doesnt exist', async () => {
       const userCompanyId = new ObjectId();
       const payload = { endDate: '2022-12-25T22:59:59.999Z' };
 
@@ -214,9 +220,25 @@ describe('USER COMPANIES ROUTES - PUT /usercompanies/{id}', () => {
       });
 
       expect(res.statusCode).toBe(403);
+      expect(res.result.message).toBe('Error while checking user company: userCompany not found.');
     });
 
-    it('should return a 403 if user is from an other company', async () => {
+    it('should return a 403 if user company startDate is in futur', async () => {
+      const userCompanyId = userCompanies[6]._id.toHexString();
+      const payload = { endDate: '2022-08-17T10:00:00.000Z' };
+
+      const res = await app.inject({
+        method: 'PUT',
+        url: `/usercompanies/${userCompanyId}`,
+        payload,
+        headers: { Cookie: `alenvi_token=${authToken}` },
+      });
+
+      expect(res.statusCode).toBe(403);
+      expect(res.result.message).toBe('Error while checking user company: userCompany not found.');
+    });
+
+    it('should return 403 if company is not allowed to detach its learners', async () => {
       const userCompanyId = userCompanies[2]._id.toHexString();
       const payload = { endDate: '2022-12-25T22:59:59.999Z' };
 
@@ -228,6 +250,22 @@ describe('USER COMPANIES ROUTES - PUT /usercompanies/{id}', () => {
       });
 
       expect(res.statusCode).toBe(403);
+      expect(res.result.message).toBe('Error while checking user company: userCompany not found.');
+    });
+
+    it('should return 403 if user is already detached', async () => {
+      const userCompanyId = userCompanies[8]._id.toHexString();
+      const payload = { endDate: '2022-12-25T22:59:59.999Z' };
+
+      const res = await app.inject({
+        method: 'PUT',
+        url: `/usercompanies/${userCompanyId}`,
+        payload,
+        headers: { Cookie: `alenvi_token=${authToken}` },
+      });
+
+      expect(res.statusCode).toBe(403);
+      expect(res.result.message).toBe('Error while checking user company: userCompany not found.');
     });
 
     it('should return a 403 if user doesnt exist', async () => {
@@ -242,6 +280,22 @@ describe('USER COMPANIES ROUTES - PUT /usercompanies/{id}', () => {
       });
 
       expect(res.statusCode).toBe(403);
+      expect(res.result.message).toBe('Error while checking user: user not found.');
+    });
+
+    it('should return a 403 if user has a role', async () => {
+      const userCompanyId = userCompanies[7]._id;
+      const payload = { endDate: '2022-12-25T22:59:59.999Z' };
+
+      const res = await app.inject({
+        method: 'PUT',
+        url: `/usercompanies/${userCompanyId}`,
+        payload,
+        headers: { Cookie: `alenvi_token=${authToken}` },
+      });
+
+      expect(res.statusCode).toBe(403);
+      expect(res.result.message).toBe('Error while checking user: user not found.');
     });
 
     it('should return a 403 if detachment date is before user company startdate', async () => {
@@ -257,20 +311,6 @@ describe('USER COMPANIES ROUTES - PUT /usercompanies/{id}', () => {
 
       expect(res.statusCode).toBe(403);
       expect(res.result.message).toBe('Impossible: la date de fin doit être postérieure à la date de début.');
-    });
-
-    it('should return a 403 if user company startDate is in futur', async () => {
-      const userCompanyId = userCompanies[6]._id.toHexString();
-      const payload = { endDate: '2022-08-17T10:00:00.000Z' };
-
-      const res = await app.inject({
-        method: 'PUT',
-        url: `/usercompanies/${userCompanyId}`,
-        payload,
-        headers: { Cookie: `alenvi_token=${authToken}` },
-      });
-
-      expect(res.statusCode).toBe(403);
     });
 
     it('should return a 403 if detachment date is before first trainee\'s addition in course history', async () => {
@@ -291,9 +331,9 @@ describe('USER COMPANIES ROUTES - PUT /usercompanies/{id}', () => {
 
   describe('Other roles', () => {
     const roles = [
-      { name: 'helper', expectedCode: 403, erp: true },
-      { name: 'planning_referent', expectedCode: 403, erp: true },
-      { name: 'trainer', expectedCode: 403, erp: false },
+      { name: 'helper', expectedCode: 403, erp: true, message: 'Insufficient scope' },
+      { name: 'planning_referent', expectedCode: 403, erp: true, message: 'Insufficient scope' },
+      { name: 'trainer', expectedCode: 403, erp: false, message: 'Error: user\'s role does\'nt allow this action.' },
     ];
 
     const userCompanyId = userCompanies[0]._id.toHexString();
@@ -310,6 +350,7 @@ describe('USER COMPANIES ROUTES - PUT /usercompanies/{id}', () => {
           headers: { Cookie: `alenvi_token=${authToken}` },
         });
         expect(res.statusCode).toBe(role.expectedCode);
+        expect(res.result.message).toBe(role.message);
       });
     });
   });
