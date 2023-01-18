b0416ced62581ae7ec2c74dd952cb895fa9d22ce
ulysse <ulysse@alenvi.io>
2023-01-18 11:11:58 +0100

    COM-3353: manon's review

--- src/helpers/users.js
+++ src/helpers/users.js
@@ -83,28 +83,24 @@ exports.getUsersListWithSectorHistories = async (query, credentials) => {
 exports.getLearnerList = async (query, credentials) => {
   let userQuery = {};
   if (query.companies) {
-    const userCompanyQuery = { company: { $in: query.companies } };
+    let userCompanyQuery = { company: { $in: query.companies } };
     if (query.startDate && query.endDate) {
-      Object.assign(
-        userCompanyQuery,
-        {
-          startDate: { $lte: CompaniDate(query.endDate).toISO() },
-          $or: [{ endDate: { $gt: CompaniDate(query.startDate).toISO() } }, { endDate: { $exists: false } }],
-        }
-      );
+      userCompanyQuery = {
+        ...userCompanyQuery,
+        startDate: { $lte: CompaniDate(query.endDate).toISO() },
+        $or: [{ endDate: { $gt: CompaniDate(query.startDate).toISO() } }, { endDate: { $exists: false } }],
+      };
     } else if (query.endDate) {
-      Object.assign(
-        userCompanyQuery,
-        { $or: [{ endDate: { $exists: false } }, { endDate: { $gt: CompaniDate(query.endDate).toISO() } }] }
-      );
+      userCompanyQuery = {
+        ...userCompanyQuery,
+        $or: [{ endDate: { $exists: false } }, { endDate: { $gt: CompaniDate(query.endDate).toISO() } }],
+      };
     } else {
-      Object.assign(
-        userCompanyQuery,
-        {
-          startDate: { $lte: CompaniDate().toISO() },
-          $or: [{ endDate: { $exists: false } }, { endDate: { $gt: CompaniDate().toISO() } }],
-        }
-      );
+      userCompanyQuery = {
+        ...userCompanyQuery,
+        startDate: { $lte: CompaniDate().toISO() },
+        $or: [{ endDate: { $exists: false } }, { endDate: { $gt: CompaniDate().toISO() } }],
+      };
     }
     const rolesToExclude = await Role.find({ name: { $in: [HELPER, AUXILIARY_WITHOUT_COMPANY] } }).lean();
     const usersCompany = await UserCompany.find(userCompanyQuery, { user: 1 }).lean();
