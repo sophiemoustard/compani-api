5a7859de7ebd744f38bdc6de793df232f0c19ce1
ulysse <ulysse@alenvi.io>
2023-02-16 14:54:55 +0100

    COM-3401: user userCompanyList rather than company

--- src/routes/preHandlers/email.js
+++ src/routes/preHandlers/email.js
@@ -4,6 +4,7 @@ const User = require('../../models/User');
 const translate = require('../../helpers/translate');
 const { TRAINER, COACH, CLIENT_ADMIN, TRAINEE } = require('../../helpers/constants');
 const UtilsHelper = require('../../helpers/utils');
+const UserCompaniesHelper = require('../../helpers/userCompanies');
 
 const { language } = translate;
 
@@ -12,7 +13,7 @@ exports.authorizeSendEmail = async (req) => {
   const isVendorUser = get(req, 'auth.credentials.role.vendor') || false;
 
   const receiver = await User.findOne({ 'local.email': req.payload.email })
-    .populate({ path: 'company' })
+    .populate({ path: 'userCompanyList' })
     .populate({ path: 'role.vendor', select: 'name' })
     .populate({ path: 'role.client', select: 'name' })
     .lean();
@@ -23,7 +24,8 @@ exports.authorizeSendEmail = async (req) => {
   const receiverIsCoachOrAdmin = [COACH, CLIENT_ADMIN].includes(get(receiver, 'role.client.name'));
   const vendorIsSendingToAuthorizedType = isVendorUser &&
     (receiverIsTrainer || req.payload.type === TRAINEE || receiverIsCoachOrAdmin);
-  const sameCompany = UtilsHelper.areObjectIdsEquals(receiver.company, companyId);
+  const getCurrentAndFutureCompanies = UserCompaniesHelper.getCurrentAndFutureCompanies(receiver.userCompanyList);
+  const sameCompany = UtilsHelper.doesArrayIncludeId(getCurrentAndFutureCompanies, companyId);
   if (!vendorIsSendingToAuthorizedType && !sameCompany) throw Boom.notFound();
 
   return null;
