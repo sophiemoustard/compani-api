0e51c8f3987c96f5ab32bed1f00637da870dc0a6
Sophie Moustard <sophiemoustard@sophies-macbook-pro.home>
2023-05-24 16:42:45 +0200

    COM-3450 - limit number of connections

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -5,8 +5,10 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 exports.mongooseConnection = async (server) => {
-  const uri = process.env.NODE_ENV === 'test' ? 'mongodb://localhost:27017/hapitest' : process.env.MONGODB_URI;
-  await mongoose.connect(uri, { retryWrites: false }, (err) => { if (err) throw err; });
+  const isTest = process.env.NODE_ENV === 'test';
+  const uri = isTest ? 'mongodb://localhost:27017/hapitest' : process.env.MONGODB_URI;
+  const options = { retryWrites: false, ...(isTest && { maxPoolSize: 50 }) };
+  await mongoose.connect(uri, options, (err) => { if (err) throw err; });
 
   // When successfully connected
   mongoose.connection.once('connected', () => server.log(['info', 'db'], 'Successfully connected to MongoDB'));
