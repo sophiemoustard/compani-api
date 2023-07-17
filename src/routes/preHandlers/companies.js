3b08d45acec1d8c71d98831d5538a9f12914419d
Palin Manon <manon@compani.fr>
2023-07-17 15:47:00 +0200

    COM-3505 - add billingRepresentative in bdd

--- src/routes/preHandlers/companies.js
+++ src/routes/preHandlers/companies.js
@@ -3,7 +3,8 @@ const get = require('lodash/get');
 const Company = require('../../models/Company');
 const translate = require('../../helpers/translate');
 const UtilsHelper = require('../../helpers/utils');
-const { TRAINING_ORGANISATION_MANAGER, VENDOR_ADMIN } = require('../../helpers/constants');
+const { TRAINING_ORGANISATION_MANAGER, VENDOR_ADMIN, CLIENT_ADMIN } = require('../../helpers/constants');
+const User = require('../../models/User');
 
 const { language } = translate;
 
@@ -22,15 +23,29 @@ exports.companyExists = async (req) => {
 exports.authorizeCompanyUpdate = async (req) => {
   const companyId = get(req, 'auth.credentials.company._id', null);
   const vendorRole = get(req, 'auth.credentials.role.vendor.name') || null;
+  const { params, payload } = req;
+
   const isVendorAdmin = !!vendorRole && [TRAINING_ORGANISATION_MANAGER, VENDOR_ADMIN].includes(vendorRole);
-  if (!isVendorAdmin && (!companyId || !UtilsHelper.areObjectIdsEquals(req.params._id, companyId))) {
+  if (!isVendorAdmin && (!companyId || !UtilsHelper.areObjectIdsEquals(params._id, companyId))) {
     throw Boom.forbidden();
   }
 
   const nameAlreadyExists = await Company
-    .countDocuments({ _id: { $ne: req.params._id }, name: req.payload.name }, { limit: 1 })
+    .countDocuments({ _id: { $ne: params._id }, name: payload.name }, { limit: 1 })
     .collation({ locale: 'fr', strength: 1 });
   if (nameAlreadyExists) throw Boom.conflict(translate[language].companyExists);
+
+  if (payload.billingRepresentative) {
+    const billingRepresentative = await User
+      .findOne({ _id: payload.billingRepresentative }, { role: 1 })
+      .populate({ path: 'company' })
+      .lean({ autopopulate: true });
+
+    const billingRepresentativeExistsAndIsClientAdmin = billingRepresentative &&
+      UtilsHelper.areObjectIdsEquals(billingRepresentative.company, companyId) &&
+      get(billingRepresentative, 'role.client.name') === CLIENT_ADMIN;
+    if (!billingRepresentativeExistsAndIsClientAdmin) throw Boom.notFound();
+  }
 
   return null;
 };
