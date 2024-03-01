7051432537cbdbdcc2233ac6ac32772eed758714
Sophie Moustard <sophiemoustard@sophies-macbook-pro.home>
2024-03-01 15:18:25 +0100

    COM-3664 - remove pay routes

--- src/controllers/payController.js
+++ src/controllers/payController.js
@@ -1,7 +1,6 @@
 const Boom = require('@hapi/boom');
 const get = require('lodash/get');
 const translate = require('../helpers/translate');
-const DraftPayHelper = require('../helpers/draftPay');
 const Absences123PayHelper = require('../helpers/123paie/absences');
 const Contracts123PayHelper = require('../helpers/123paie/contracts');
 const Identification123PayHelper = require('../helpers/123paie/identification');
@@ -10,34 +9,6 @@ const PayHelper = require('../helpers/pay');
 const { IDENTIFICATION, CONTRACT_VERSION, ABSENCE, CONTRACT_END, PAY } = require('../helpers/constants');
 
 const { language } = translate;
-
-const draftPayList = async (req) => {
-  req.log('payController - draftPayList - query', req.query);
-  req.log('payController - draftPayList - company', get(req, 'auth.credentials.company._id'));
-
-  try {
-    const draftPay = await DraftPayHelper.getDraftPay(req.query, req.auth.credentials);
-
-    return {
-      message: translate[language].draftPay,
-      data: { draftPay },
-    };
-  } catch (e) {
-    req.log('error', e);
-    return Boom.isBoom(e) ? e : Boom.badImplementation(e);
-  }
-};
-
-const createList = async (req) => {
-  try {
-    await PayHelper.createPayList(req.payload, req.auth.credentials);
-
-    return { message: translate[language].payListCreated };
-  } catch (e) {
-    req.log('error', e);
-    return Boom.isBoom(e) ? e : Boom.badImplementation(e);
-  }
-};
 
 const getHoursBalanceDetails = async (req) => {
   try {
@@ -49,23 +20,6 @@ const getHoursBalanceDetails = async (req) => {
     return {
       message: translate[language].hoursBalanceDetail,
       data: { hoursBalanceDetail: detail },
-    };
-  } catch (e) {
-    req.log('error', e);
-    return Boom.isBoom(e) ? e : Boom.badImplementation(e);
-  }
-};
-
-const getHoursToWork = async (req) => {
-  try {
-    req.log('payController - getHoursToWork - query', req.query);
-    req.log('payController - getHoursToWork - company', get(req, 'auth.credentials.company._id'));
-
-    const hoursToWork = await PayHelper.getHoursToWorkBySector(req.query, req.auth.credentials);
-
-    return {
-      message: translate[language].hoursToWorkFound,
-      data: { hoursToWork },
     };
   } catch (e) {
     req.log('error', e);
@@ -102,9 +56,6 @@ const exportDsnInfo = async (req, h) => {
 };
 
 module.exports = {
-  draftPayList,
-  createList,
   getHoursBalanceDetails,
-  getHoursToWork,
   exportDsnInfo,
 };
--- src/routes/preHandlers/pay.js
+++ src/routes/preHandlers/pay.js
@@ -4,15 +4,6 @@ const Sector = require('../../models/Sector');
 const UserCompany = require('../../models/UserCompany');
 const UtilsHelper = require('../../helpers/utils');
 
-exports.authorizePayCreation = async (req) => {
-  const companyId = get(req, 'auth.credentials.company._id', null);
-  const ids = req.payload.map(pay => pay.auxiliary);
-  const usersCount = await UserCompany.countDocuments({ company: companyId, user: { $in: ids } });
-  if (usersCount !== ids.length) throw Boom.notFound();
-
-  return null;
-};
-
 exports.authorizeGetDetails = async (req) => {
   const companyId = get(req, 'auth.credentials.company._id', null);
   if (req.query.auxiliary) {
@@ -25,15 +16,6 @@ exports.authorizeGetDetails = async (req) => {
     const sectorsCount = await Sector.countDocuments({ _id: { $in: sectors }, company: companyId });
     if (sectorsCount !== sectors.length) throw Boom.notFound();
   }
-
-  return null;
-};
-
-exports.authorizeGetHoursToWork = async (req) => {
-  const companyId = get(req, 'auth.credentials.company._id', null);
-  const sectors = UtilsHelper.formatIdsArray(req.query.sector);
-  const sectorsCount = await Sector.countDocuments({ _id: { $in: sectors }, company: companyId });
-  if (sectorsCount !== sectors.length) throw Boom.notFound();
 
   return null;
 };
