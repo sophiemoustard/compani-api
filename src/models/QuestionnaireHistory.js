d5149400966eff1f7a6128694d104771315928a6
Manon Palin <manonpalin@macbook-pro-de-manon.home>
2024-06-14 09:53:55 +0200

    COM-3719 - set undefined default value for trainerAnswerList in QuestionnaireHistory model

--- src/models/QuestionnaireHistory.js
+++ src/models/QuestionnaireHistory.js
@@ -9,7 +9,7 @@ const QuestionnaireHistorySchema = mongoose.Schema({
   questionnaireAnswersList: [{
     card: { type: mongoose.Schema.Types.ObjectId, ref: 'Card' },
     answerList: { type: [String] },
-    trainerAnswerList: { type: [String] },
+    trainerAnswerList: { type: [String], default: undefined },
   }],
   company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
   origin: { type: String, enum: ORIGIN_OPTIONS, required: true, immutable: true, default: MOBILE },
