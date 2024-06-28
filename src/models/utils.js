0e6dccb076260205498443bee6c36b4f2113aa28
ulysse <ulysse@alenvi.io>
2024-06-28 18:04:43 +0200

    COM-3736: add validations

--- src/models/utils.js
+++ src/models/utils.js
@@ -1,9 +1,12 @@
 const MONTH_VALIDATION = /^([0]{1}[1-9]{1}|[1]{1}[0-2]{1})-[2]{1}[0]{1}[0-9]{2}$/;
 const PHONE_VALIDATION = /^[0]{1}[1-9]{1}[0-9]{8}$/;
 const SIRET_VALIDATION = /^\d{14}$/;
-
+const IBAN_VALIDATION = /^FR\d{12}[0-9A-Z]{11}\d{2}$/;
+const BIC_VALIDATION = /^[A-Z]{6}[0-9A-Z]{2}([0-9A-Z]{3})?$/;
 module.exports = {
   MONTH_VALIDATION,
   PHONE_VALIDATION,
   SIRET_VALIDATION,
+  IBAN_VALIDATION,
+  BIC_VALIDATION,
 };
