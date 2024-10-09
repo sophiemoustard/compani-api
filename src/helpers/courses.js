9f655e73435fab2299730e21b1700369148650e0
Manon Palin <manonpalin@mbp-de-manon.home>
2024-10-09 10:25:32 +0200

    COM-3799 : create PUT /courses/{_id}/trainers

--- src/helpers/courses.js
+++ src/helpers/courses.js
@@ -1246,3 +1246,6 @@ exports.composeCourseName = (course) => {
 
   return companyName + course.subProgram.program.name + misc;
 };
+
+exports.addTrainer = async (courseId, payload) => Course
+  .updateOne({ _id: courseId }, { $addToSet: { trainers: payload.trainer } });
