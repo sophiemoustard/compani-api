39cf910e00b2619edde1dfee166469ae350ff8b1
Aude <aude@compani.fr>
2024-11-07 12:41:20 +0100

    COM-3810: fix unit tests in course

--- tests/unit/helpers/courses.test.js
+++ tests/unit/helpers/courses.test.js
@@ -6207,8 +6207,8 @@ describe('generateConvocationPdf', () => {
       _id: courseId,
       subProgram: { program: { name: 'Comment attraper des Pokemons' } },
       trainers: [
-        { identity: { firstname: 'Ash', lastname: 'Ketchum' }, biography: 'Bio' },
-        { identity: { firstname: 'Toto', lastname: 'Tata' } },
+        { identity: { firstname: 'Ash', lastname: 'Ketchum' }, formattedIdentity: 'Ash KETCHUM', biography: 'Bio' },
+        { identity: { firstname: 'Toto', lastname: 'Tata' }, formattedIdentity: 'Toto TATA' },
       ],
       contact: { phone: '0123456789' },
       contactPhoneNumber: '01 23 45 67 89',
@@ -6268,8 +6268,8 @@ describe('generateConvocationPdf', () => {
         _id: courseId,
         subProgram: { program: { name: 'Comment attraper des Pokemons' } },
         trainers: [
-          { identity: { firstname: 'Ash', lastname: 'Ketchum' }, biography: 'Bio' },
-          { identity: { firstname: 'Toto', lastname: 'Tata' } },
+          { identity: { firstname: 'Ash', lastname: 'Ketchum' }, formattedIdentity: 'Ash KETCHUM', biography: 'Bio' },
+          { identity: { firstname: 'Toto', lastname: 'Tata' }, formattedIdentity: 'Toto TATA' },
         ],
         contact: { phone: '0123456789' },
         contactPhoneNumber: '01 23 45 67 89',
