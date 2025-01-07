5abfae1e66f529eef32d079684832d94d71999b7
Aude <aude@compani.fr>
2025-01-07 11:32:25 +0100

    COM-3863: fix review + add integration test for wrong quantity

--- src/routes/courseSlots.js
+++ src/routes/courseSlots.js
@@ -17,7 +17,7 @@ exports.plugin = {
           payload: Joi.object({
             course: Joi.objectId().required(),
             step: Joi.objectId().required(),
-            quantity: Joi.number().positive().required(),
+            quantity: Joi.number().integer().positive().required(),
           }),
         },
         pre: [{ method: authorizeCreate }],
--- tests/integration/courseSlots.test.js
+++ tests/integration/courseSlots.test.js
@@ -86,6 +86,22 @@ describe('COURSE SLOTS ROUTES - POST /courseslots', () => {
         expect(response.statusCode).toBe(400);
       });
     });
+
+    const wrongQuantity = [0, -2, 1.5];
+    wrongQuantity.forEach((q) => {
+      it('should return 400 if quantity is not an integer or negatif number', async () => {
+        const payload = { course: courseSlotsList[0].course, step: stepsList[0]._id, quantity: q };
+
+        const response = await app.inject({
+          method: 'POST',
+          url: '/courseslots',
+          headers: { Cookie: `alenvi_token=${authToken}` },
+          payload,
+        });
+
+        expect(response.statusCode).toBe(400);
+      });
+    });
   });
 
   describe('Other roles', () => {
